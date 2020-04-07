5a793439138a4449b31bf363cfd8dd0bc986fe9d
Ryan Fitzpatrick <rmfitzpatrick@signalfx.com>
2020-04-07 09:21:59 -0400

    Correct typo in ToUnixTimeMicroseconds comment

--- src/Datadog.Trace/ExtensionMethods/TimeExtensions.cs
+++ src/Datadog.Trace/ExtensionMethods/TimeExtensions.cs
@@ -18,8 +18,8 @@ namespace Datadog.Trace.ExtensionMethods
         /// <summary>
         /// Returns the number of microseconds that have elapsed since 1970-01-01T00:00:00.000Z.
         /// </summary>
-        /// <param name="dateTimeOffset">The value to get the number of elapsed nanoseconds for.</param>
-        /// <returns>The number of nanoseconds that have elapsed since 1970-01-01T00:00:00.000Z.</returns>
+        /// <param name="dateTimeOffset">The value to get the number of elapsed microseconds for.</param>
+        /// <returns>The number of microseconds that have elapsed since 1970-01-01T00:00:00.000Z.</returns>
         public static long ToUnixTimeMicroseconds(this DateTimeOffset dateTimeOffset)
         {
             const long microsecondsPerMillisecond = 1000;
