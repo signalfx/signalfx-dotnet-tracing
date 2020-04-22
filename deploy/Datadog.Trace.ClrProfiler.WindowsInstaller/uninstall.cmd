2cfbc5e5da48bc536c267415b16d8b7e9e9832ee
Paulo Janotti <pjanotti@splunk.com>
2020-04-21 17:30:11 -0700

    Few more fit and finish: rebrand and add NOTICE

--- deploy/Datadog.Trace.ClrProfiler.WindowsInstaller/uninstall.cmd
+++ deploy/Datadog.Trace.ClrProfiler.WindowsInstaller/uninstall.cmd
@@ -1,15 +1,16 @@
+@rem Modified by SignalFx
 @echo off
 setlocal
 
 echo Executing uninstall.cmd at %date% %time%
 
-set DATADOG_APPCMD_CMDLINE=%systemroot%\system32\inetsrv\appcmd.exe set config /section:system.webServer/modules /-[name='DatadogTracingModule']
+set DATADOG_APPCMD_CMDLINE=%systemroot%\system32\inetsrv\appcmd.exe set config /section:system.webServer/modules /-[name='SignalFxTracingModule']
 
 IF EXIST %systemroot%\system32\inetsrv\appcmd.exe (
-    echo Attempting to uninstall the Datadog ASP.NET HttpModule with %systemroot%\system32\inetsrv\appcmd.exe
+    echo Attempting to uninstall the SignalFx ASP.NET HttpModule with %systemroot%\system32\inetsrv\appcmd.exe
     %DATADOG_APPCMD_CMDLINE% 2>&1
 ) ELSE (
-    echo "%systemroot%\system32\inetsrv\appcmd.exe" doesn't exist. The Datadog ASP.NET HttpModule will not be uninstalled by this installer.
+    echo "%systemroot%\system32\inetsrv\appcmd.exe" doesn't exist. The SignalFx ASP.NET HttpModule will not be uninstalled by this installer.
 )
 
 REM Always report success
