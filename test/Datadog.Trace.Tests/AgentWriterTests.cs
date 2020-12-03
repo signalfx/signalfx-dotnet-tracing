5f8cccaf7087e3c5c37ffc00eea380f27f8ab9ae
Paulo Janotti <pjanotti@splunk.com>
2020-12-03 11:56:28 -0800

    Add tests

--- test/Datadog.Trace.Tests/AgentWriterTests.cs
+++ test/Datadog.Trace.Tests/AgentWriterTests.cs
@@ -10,42 +10,48 @@ namespace Datadog.Trace.Tests
 {
     public class AgentWriterTests
     {
-        private readonly AgentWriter _agentWriter;
-        private readonly Mock<IApi> _api;
-        private readonly SpanContext _spanContext;
-
-        public AgentWriterTests()
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task WriteTrace_2Traces_SendToApi(bool synchronousSend)
         {
             var tracer = new Mock<IDatadogTracer>();
             tracer.Setup(x => x.DefaultServiceName).Returns("Default");
 
-            _api = new Mock<IApi>();
-            _agentWriter = new AgentWriter(_api.Object, statsd: null);
+            var api = new Mock<IApi>();
+            var agentWriter = new AgentWriter(api.Object, statsd: null, synchronousSend);
 
             var parentSpanContext = new Mock<ISpanContext>();
             var traceContext = new Mock<ITraceContext>();
-            _spanContext = new SpanContext(parentSpanContext.Object, traceContext.Object, serviceName: null);
-        }
+            var spanContext = new SpanContext(parentSpanContext.Object, traceContext.Object, serviceName: null);
 
-        [Fact]
-        public async Task WriteTrace_2Traces_SendToApi()
-        {
             // TODO:bertrand it is too complicated to setup such a simple test
-            var trace = new[] { new Span(_spanContext, start: null) };
-            _agentWriter.WriteTrace(trace);
-            await Task.Delay(TimeSpan.FromSeconds(3));
-            _api.Verify(x => x.SendTracesAsync(It.Is<Span[][]>(y => y.Single().Equals(trace))), Times.Once);
+            var trace = new[] { new Span(spanContext, start: null) };
+            agentWriter.WriteTrace(trace);
+            if (!synchronousSend)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(3));
+            }
 
-            trace = new[] { new Span(_spanContext, start: null) };
-            _agentWriter.WriteTrace(trace);
-            await Task.Delay(TimeSpan.FromSeconds(3));
-            _api.Verify(x => x.SendTracesAsync(It.Is<Span[][]>(y => y.Single().Equals(trace))), Times.Once);
+            api.Verify(x => x.SendTracesAsync(It.Is<Span[][]>(y => y.Single().Equals(trace))), Times.Once);
+
+            trace = new[] { new Span(spanContext, start: null) };
+            agentWriter.WriteTrace(trace);
+            if (!synchronousSend)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(3));
+            }
+
+            api.Verify(x => x.SendTracesAsync(It.Is<Span[][]>(y => y.Single().Equals(trace))), Times.Once);
         }
 
-        [Fact]
-        public async Task FlushTwice()
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task FlushTwice(bool synchronousSend)
         {
-            var w = new AgentWriter(_api.Object, statsd: null);
+            var api = new Mock<IApi>();
+            var w = new AgentWriter(api.Object, statsd: null, synchronousSend);
             await w.FlushAndCloseAsync();
             await w.FlushAndCloseAsync();
         }
