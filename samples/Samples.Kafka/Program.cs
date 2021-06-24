71c04b6353e8f6c0fdb6f2ee5dc016be85a18947
Robert Pająk <rpajak@splunk.com>
2021-06-24 11:14:04 +0200

    Kafka instrumentation part 1 - Sample application (#123)

---
+++ samples/Samples.Kafka/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace Samples.Kafka
+{
+    // Run following command to prepare the Kafka cluster:
+    //   docker-compose up kafka
+    // Based mostly on:
+    // - https://github.com/confluentinc/confluent-kafka-dotnet/blob/v1.7.0/examples/ConfluentCloud/Program.cs
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kafkaUrl = Environment.GetEnvironmentVariable("KAFKA_HOST") ?? "localhost:9092";
+            string topic = "dotnet-test-topic";
+
+            var pConfig = new ProducerConfig { BootstrapServers = kafkaUrl };
+            using (var producer = new ProducerBuilder<Null, string>(pConfig).Build())
+            {
+                producer.Produce(topic, new Message<Null, string> { Value = "test value" });
+                producer.Flush(TimeSpan.FromSeconds(10));
+            }
+
+            var cConfig = new ConsumerConfig
+            {
+                BootstrapServers = kafkaUrl,
+                GroupId = Guid.NewGuid().ToString(),
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            using (var consumer = new ConsumerBuilder<Null, string>(cConfig).Build())
+            {
+                consumer.Subscribe(topic);
+                try
+                {
+                    var consumeResult = consumer.Consume();
+                    Console.WriteLine($"consumed: {consumeResult.Message.Value}");
+                }
+                catch (ConsumeException ex)
+                {
+                    Console.WriteLine($"consume error: {ex.Error.Reason}");
+                }
+            }
+        }
+    }
+}
