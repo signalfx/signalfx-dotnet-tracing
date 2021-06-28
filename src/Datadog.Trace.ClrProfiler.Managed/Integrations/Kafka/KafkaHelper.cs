7cc74489fa2fa5f301a15104a8e60a608eec5598
dszmigielski <dszmigielski@splunk.com>
2021-06-28 15:04:14 +0200

    Add Kafka integration for Consume method

---
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/Kafka/KafkaHelper.cs
@@ -0,0 +1,21 @@
+using SignalFx.Tracing.Logging;
+using SignalFx.Tracing.Vendors.Serilog;
+
+namespace Datadog.Trace.ClrProfiler.Integrations.Kafka
+{
+    internal static class KafkaHelper
+    {
+        private static readonly ILogger Log = SignalFxLogging.GetLogger(typeof(ConsumeKafkaIntegration));
+
+        internal static T GetProperty<T>(object obj, string propertyName)
+        {
+            if (obj.TryGetPropertyValue("propertyName", out T property))
+            {
+                property = default;
+                Log.Warning($"Unable to access {propertyName} property.");
+            }
+
+            return property;
+        }
+    }
+}
