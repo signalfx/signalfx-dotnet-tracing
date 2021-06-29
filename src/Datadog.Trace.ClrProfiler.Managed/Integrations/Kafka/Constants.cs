11af65409b7f9c3e61b8e14b39f8fd676ed0e287
dszmigielski <dszmigielski@splunk.com>
2021-06-29 09:29:42 +0200

    Fix type names

--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/Kafka/Constants.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/Kafka/Constants.cs
@@ -9,9 +9,9 @@ namespace Datadog.Trace.ClrProfiler.Integrations.Kafka
         internal const string MinimumVersion = "1.4.0";
         internal const string MaximumVersion = "1.7.0";
 
-        internal const string ConsumeResultTypeName = "Confluent.Kafka.ConsumeResult`2[!0,!1]";
+        internal const string ConsumeResultTypeName = "Confluent.Kafka.ConsumeResult`2<TKey,TValue>";
         internal const string TopicPartitionTypeName = "Confluent.Kafka.TopicPartition";
-        internal const string MessageTypeName = "Confluent.Kafka.Message`2[!0,!1]";
-        internal const string ActionOfDeliveryReportTypeName = "System.Action`1[Confluent.Kafka.DeliveryReport`2[!0,!1]]";
+        internal const string MessageTypeName = "Confluent.Kafka.Message`2<TKey,TValue>";
+        internal const string ActionOfDeliveryReportTypeName = "System.Action`1<Confluent.Kafka.DeliveryReport`2<TKey,TValue>>";
     }
 }
