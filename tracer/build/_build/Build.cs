DataDog/dd-trace-dotnet

Store benchmark results as an artifact (#1799)
* Add json exporter to benchmarks

* Upload benchmark results as an artifact for later processing

* Ignore the build_data folder (used in CI)

* Add default exporters

* Remove manual exporters

* Fix exporter

tracer/build/_build/Build.cs
@@ -287,25 +287,41 @@ partial class Build : NukeBuild
         .Description("Runs the Benchmarks project")
         .Executes(() =>
         {
-            DotNetBuild(s => s
-                .SetProjectFile(Solution.GetProject(Projects.BenchmarksTrace))
-                .SetConfiguration(BuildConfiguration)
-                .SetFramework(TargetFramework.NETCOREAPP3_1)
-                .EnableNoDependencies()
-                .When(!string.IsNullOrEmpty(NugetPackageDirectory), o => o.SetPackageDirectory(NugetPackageDirectory))
-            );
+            var benchmarksProject = Solution.GetProject(Projects.BenchmarksTrace);
+            var resultsDirectory = benchmarksProject.Directory / "BenchmarkDotNet.Artifacts" / "results";
+            EnsureCleanDirectory(resultsDirectory);
 
-            DotNetRun(s => s
-                .SetProjectFile(Solution.GetProject(Projects.BenchmarksTrace))
-                .SetConfiguration(BuildConfiguration)
-                .SetFramework(TargetFramework.NETCOREAPP3_1)
-                .EnableNoRestore()
-                .EnableNoBuild()
-                .SetApplicationArguments("-r net472 netcoreapp3.1 -m -f * --iterationTime 2000")
-                .SetProcessEnvironmentVariable("DD_SERVICE", "dd-trace-dotnet")
-                .SetProcessEnvironmentVariable("DD_ENV", "CI")
-                .When(!string.IsNullOrEmpty(NugetPackageDirectory), o => o.SetPackageDirectory(NugetPackageDirectory))
-            );
+            try
+            {
+                DotNetBuild(s => s
+                    .SetProjectFile(benchmarksProject)
+                    .SetConfiguration(BuildConfiguration)
+                    .SetFramework(TargetFramework.NETCOREAPP3_1)
+                    .EnableNoDependencies()
+                    .When(!string.IsNullOrEmpty(NugetPackageDirectory), o => o.SetPackageDirectory(NugetPackageDirectory))
+                );
+
+                DotNetRun(s => s
+                    .SetProjectFile(benchmarksProject)
+                    .SetConfiguration(BuildConfiguration)
+                    .SetFramework(TargetFramework.NETCOREAPP3_1)
+                    .EnableNoRestore()
+                    .EnableNoBuild()
+                    .SetApplicationArguments("-r net472 netcoreapp3.1 -m -f * --iterationTime 2000")
+                    .SetProcessEnvironmentVariable("DD_SERVICE", "dd-trace-dotnet")
+                    .SetProcessEnvironmentVariable("DD_ENV", "CI")
+                    .When(!string.IsNullOrEmpty(NugetPackageDirectory), o => o.SetPackageDirectory(NugetPackageDirectory))
+                );
+            }
+            finally
+            {
+                MoveLogsToBuildData();
+                if (Directory.Exists(resultsDirectory))
+                {
+                    CopyDirectoryRecursively(resultsDirectory, BuildDataDirectory / "benchmarks",
+                                             DirectoryExistsPolicy.Merge, FileExistsPolicy.Overwrite);
+                }
+            }
         });
 
     /// <summary>

signalfx/signalfx-dotnet-tracing

tracer/build/_build/Build.cs
@@ -287,25 +287,41 @@ partial class Build : NukeBuild
         .Description("Runs the Benchmarks project")
         .Executes(() =>
         {
-            DotNetBuild(s => s
-                .SetProjectFile(Solution.GetProject(Projects.BenchmarksTrace))
-                .SetConfiguration(BuildConfiguration)
-                .SetFramework(TargetFramework.NETCOREAPP3_1)
-                .EnableNoDependencies()
-                .When(!string.IsNullOrEmpty(NugetPackageDirectory), o => o.SetPackageDirectory(NugetPackageDirectory))
-            );
+            var benchmarksProject = Solution.GetProject(Projects.BenchmarksTrace);
+            var resultsDirectory = benchmarksProject.Directory / "BenchmarkDotNet.Artifacts" / "results";
+            EnsureCleanDirectory(resultsDirectory);
 
-            DotNetRun(s => s
-                .SetProjectFile(Solution.GetProject(Projects.BenchmarksTrace))
-                .SetConfiguration(BuildConfiguration)
-                .SetFramework(TargetFramework.NETCOREAPP3_1)
-                .EnableNoRestore()
-                .EnableNoBuild()
-                .SetApplicationArguments("-r net472 netcoreapp3.1 -m -f * --iterationTime 2000")
-                .SetProcessEnvironmentVariable("SIGNALFX_SERVICE", "otel-trace-dotnet")
-                .SetProcessEnvironmentVariable("SIGNALFX_ENV", "CI")
-                .When(!string.IsNullOrEmpty(NugetPackageDirectory), o => o.SetPackageDirectory(NugetPackageDirectory))
-            );
+            try
+            {
+                DotNetBuild(s => s
+                    .SetProjectFile(benchmarksProject)
+                    .SetConfiguration(BuildConfiguration)
+                    .SetFramework(TargetFramework.NETCOREAPP3_1)
+                    .EnableNoDependencies()
+                    .When(!string.IsNullOrEmpty(NugetPackageDirectory), o => o.SetPackageDirectory(NugetPackageDirectory))
+                );
+
+                DotNetRun(s => s
+                    .SetProjectFile(benchmarksProject)
+                    .SetConfiguration(BuildConfiguration)
+                    .SetFramework(TargetFramework.NETCOREAPP3_1)
+                    .EnableNoRestore()
+                    .EnableNoBuild()
+                    .SetApplicationArguments("-r net472 netcoreapp3.1 -m -f * --iterationTime 2000")
+                    .SetProcessEnvironmentVariable("SIGNALFX_SERVICE", "otel-trace-dotnet")
+                    .SetProcessEnvironmentVariable("SIGNALFX_ENV", "CI")
+                    .When(!string.IsNullOrEmpty(NugetPackageDirectory), o => o.SetPackageDirectory(NugetPackageDirectory))
+                );
+            }
+            finally
+            {
+                MoveLogsToBuildData();
+                if (Directory.Exists(resultsDirectory))
+                {
+                    CopyDirectoryRecursively(resultsDirectory, BuildDataDirectory / "benchmarks",
+                                             DirectoryExistsPolicy.Merge, FileExistsPolicy.Overwrite);
+                }
+            }
         });
 
     /// <summary>