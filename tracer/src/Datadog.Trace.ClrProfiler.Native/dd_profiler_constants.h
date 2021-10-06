DataDog/dd-trace-dotnet

Add PInvoke Map rewriting in all platforms to support native library renaming (#1809)
* Add PInvoke Map rewriting in all platforms to support native library renaming.

* Fallback to the fixed name when we can't extract the path (Installed in the GAC).

* Fix native filename per platform.

* Adds the filename checker project. (Test is still missing for next commit)

* Changes.

* Fix GetCLRProfilerPath function.
Adds the NativeProfilerChecks test.

* Fix non Windows build

* Revert unnecessary changes.

* Add try/catch on the Checker path.
Change GetCLRPRofilerPath to detect the runtime type.

* try to fix tests.

* Adds conditional for the check

* setup the test project for linux.

* commenting to do some checks.

* Build passing. Uncomment, initialization block.

* Add ToString to check and simplify catch scenario.

* comments

* change

* change std::filepath with a normal rfind.

* changing algorithm to avoid std::filesystem

* Remove validation based on the process name, all samples are running through the dotnet.exe process

tracer/src/Datadog.Trace.ClrProfiler.Native/dd_profiler_constants.h
@@ -92,9 +92,20 @@ const WSTRING managed_profiler_full_assembly_version =
 const WSTRING managed_profiler_name = WStr("Datadog.Trace");
 
 const WSTRING nonwindows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+NonWindows");
+const WSTRING windows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+Windows");
+
 const WSTRING appsec_nonwindows_nativemethods_type = WStr("Datadog.Trace.AppSec.Waf.NativeBindings.NativeLibrary+NonWindows");
+const WSTRING appsec_windows_nativemethods_type = WStr("Datadog.Trace.AppSec.Waf.NativeBindings.NativeLibrary+Windows");
 
 const WSTRING calltarget_modification_action = WStr("CallTargetModification");
+
+#ifdef _WIN32
+const WSTRING native_dll_filename = WStr("DATADOG.TRACE.CLRPROFILER.NATIVE.DLL");
+#elif MACOS
+const WSTRING native_dll_filename = WStr("Datadog.Trace.ClrProfiler.Native.dylib");
+#else
+const WSTRING native_dll_filename = WStr("Datadog.Trace.ClrProfiler.Native.so");
+#endif
 
 } // namespace trace
 

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace.ClrProfiler.Native/dd_profiler_constants.h
@@ -91,9 +91,20 @@ const WSTRING managed_profiler_full_assembly_version =
 const WSTRING managed_profiler_name = WStr("OpenTelemetry.AutoInstrumentation");
 
 const WSTRING nonwindows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+NonWindows");
+const WSTRING windows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+Windows");
+
 const WSTRING appsec_nonwindows_nativemethods_type = WStr("Datadog.Trace.AppSec.Waf.NativeBindings.NativeLibrary+NonWindows");
+const WSTRING appsec_windows_nativemethods_type = WStr("Datadog.Trace.AppSec.Waf.NativeBindings.NativeLibrary+Windows");
 
 const WSTRING calltarget_modification_action = WStr("CallTargetModification");
+
+#ifdef _WIN32
+const WSTRING native_dll_filename = WStr("OPENTELEMETRY.AUTOINSTRUMENTATION.CLRPROFILER.NATIVE.DLL");
+#elif MACOS
+const WSTRING native_dll_filename = WStr("OpenTelemetry.AutoInstrumentation.ClrProfiler.Native.dylib");
+#else
+const WSTRING native_dll_filename = WStr("OpenTelemetry.AutoInstrumentation.ClrProfiler.Native.so");
+#endif
 
 } // namespace trace
 