42ec0d85603f37e1d9fa4aacba2206b5234e79d4
Rasmus Kuusmann <rkuusmann@splunk.com>
2021-11-01 16:47:40 +0200

    Update rabbitmq semantics (#172)
    * Covert rabbitmq tags
    
    * restore default constructor
    
    * fix tests
    
    * fix wrong operation name
    
    * Update rabbitmq tests
    
    * revert span kind case
    
    * PR feedbakc
    
    * Restore
    
    * Refactor based on PR comments
    
    * fix expected service name
    
    * fix spans filtering by service name

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/RabbitMQ/BasicDeliverIntegration.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/RabbitMQ/BasicDeliverIntegration.cs
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+// Modified by Splunk Inc.
+
 using System;
 using System.ComponentModel;
 using Datadog.Trace.ClrProfiler.CallTarget;
@@ -28,7 +30,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.RabbitMQ
     [EditorBrowsable(EditorBrowsableState.Never)]
     public class BasicDeliverIntegration
     {
-        private const string Command = "basic.deliver";
+        private const string Command = RabbitMQIntegration.DeliverCommand;
         private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(BasicDeliverIntegration));
 
         /// <summary>
@@ -70,6 +72,8 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.RabbitMQ
             if (tags != null)
             {
                 tags.MessageSize = body?.Length.ToString() ?? "0";
+
+                RabbitMQIntegration.SetTagsFromBasicProperties(tags, basicProperties);
             }
 
             return new CallTargetState(scope);
---
+++ tracer/src/Datadog.Trace/Tagging/MessagingTags.cs
@@ -0,0 +1,54 @@
+// Modified by Splunk Inc.
+
+using System.Linq;
+using Datadog.Trace.ExtensionMethods;
+
+namespace Datadog.Trace.Tagging
+{
+    internal abstract class MessagingTags : InstrumentationTags
+    {
+        protected static readonly IProperty<string>[] MessagingTagsProperties =
+            InstrumentationTagsProperties.Concat(
+                new Property<MessagingTags, string>(Tags.Messaging.System, t => t.System, (t, v) => t.System = v),
+                new Property<MessagingTags, string>(Tags.Messaging.Destination, t => t.Destination, (t, v) => t.Destination = v),
+                new Property<MessagingTags, string>(Tags.Messaging.DestinationKind, t => t.DestinationKind, (t, v) => t.DestinationKind = v),
+                new Property<MessagingTags, string>(Tags.Messaging.TempDestination, t => t.TempDestination, (t, v) => t.TempDestination = v),
+                new Property<MessagingTags, string>(Tags.Messaging.Protocol, t => t.Protocol, (t, v) => t.Protocol = v),
+                new Property<MessagingTags, string>(Tags.Messaging.ProtocolVersion, t => t.ProtocolVersion, (t, v) => t.ProtocolVersion = v),
+                new Property<MessagingTags, string>(Tags.Messaging.Url, t => t.Url, (t, v) => t.Url = v),
+                new Property<MessagingTags, string>(Tags.Messaging.Operation, t => t.Operation, (t, v) => t.Operation = v),
+                new Property<MessagingTags, string>(Tags.Messaging.MessageId, t => t.MessageId, (t, v) => t.MessageId = v),
+                new Property<MessagingTags, string>(Tags.Messaging.ConversationId, t => t.ConversationId, (t, v) => t.ConversationId = v),
+                new Property<MessagingTags, string>(Tags.Messaging.ConsumerId, t => t.ConsumerId, (t, v) => t.ConsumerId = v),
+                new Property<MessagingTags, string>(Tags.Messaging.MessageSize, t => t.MessageSize, (t, v) => t.MessageSize = v),
+                new Property<MessagingTags, string>(Tags.Messaging.MessageSizeCompressed, t => t.MessageSizeCompressed, (t, v) => t.MessageSizeCompressed = v));
+
+        public string System { get; set; }
+
+        public string Destination { get; set; }
+
+        public string DestinationKind { get; set; }
+
+        public string TempDestination { get; set; }
+
+        public string Protocol { get; set; }
+
+        public string ProtocolVersion { get; set; }
+
+        public string Url { get; set; }
+
+        public string Operation { get; set; }
+
+        public string MessageId { get; set; }
+
+        public string ConversationId { get; set; }
+
+        public string ConsumerId { get; set; }
+
+        public string MessageSize { get; set; }
+
+        public string MessageSizeCompressed { get; set; }
+
+        protected override IProperty<string>[] GetAdditionalTags() => MessagingTagsProperties;
+    }
+}
