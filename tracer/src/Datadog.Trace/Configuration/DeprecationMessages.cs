DataDog/dd-trace-dotnet

[2.0] Mark deprecated settings obsolete (#1974)
* Mark DD_TRACE_ANALYTICS_ENABLED obsolete

App Analytics was replaced by Tracing without Limits last year.

* Mark DD_TRACE_LOG_PATH as deprecated

Prefer DD_TRACE_LOG_DIRECTORY instead

* Update public api

tracer/src/Datadog.Trace/Configuration/DeprecationMessages.cs
@@ -1,0 +1,13 @@
+﻿// <copyright file="DeprecationMessages.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+namespace Datadog.Trace.Configuration
+{
+    internal static class DeprecationMessages
+    {
+        public const string AppAnalytics = "App Analytics has been replaced by Tracing without Limits. For more information see https://docs.datadoghq.com/tracing/legacy_app_analytics/";
+        public const string LogPath = "DD_TRACE_LOG_PATH is deprecated. Use DD_TRACE_LOG_DIRECTORY instead";
+    }
+}

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/Configuration/DeprecationMessages.cs
@@ -1,0 +1,13 @@
+﻿// <copyright file="DeprecationMessages.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+namespace Datadog.Trace.Configuration
+{
+    internal static class DeprecationMessages
+    {
+        public const string AppAnalytics = "App Analytics has been replaced by Tracing without Limits. For more information see https://docs.datadoghq.com/tracing/legacy_app_analytics/";
+        public const string LogPath = "SIGNALFX_TRACE_LOG_PATH is deprecated. Use SIGNALFX_TRACE_LOG_DIRECTORY instead";
+    }
+}