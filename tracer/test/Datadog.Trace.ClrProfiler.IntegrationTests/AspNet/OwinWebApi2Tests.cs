DataDog/dd-trace-dotnet

Use the non truncated UserAgent property in .netfx webapi (#2128)
* Fix Typo

* Use the non truncated UserAgent property in .netfx webapi

- Change the code to pass the useragent when needed
- Impact webapi integration tests

* Update tracer/src/Datadog.Trace/SpanContextPropagator.cs

Removing useless log line

Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

* Update tracer/src/Datadog.Trace/SpanContextPropagator.cs

Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

* Implement one comment

* Fix Public Api Tests and add a UTest

Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/OwinWebApi2Tests.cs
@@ -12,7 +12,6 @@ using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Datadog.Trace.Configuration;
@@ -53,6 +52,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
         {
             SetServiceVersion("1.0.0");
             SetEnvironmentVariable(ConfigurationKeys.FeatureFlags.RouteTemplateResourceNamesEnabled, enableRouteTemplateResourceNames.ToString());
+            SetEnvironmentVariable(ConfigurationKeys.HeaderTags, HttpHeaderNames.UserAgent + ":http_useragent");
 
             _fixture = fixture;
             _output = output;
@@ -126,6 +126,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             {
                 _httpClient = new HttpClient();
                 _httpClient.DefaultRequestHeaders.Add(HttpHeaderNames.TracingEnabled, "false");
+                _httpClient.DefaultRequestHeaders.Add(HttpHeaderNames.UserAgent, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36");
             }
 
             public MockTracerAgent Agent { get; private set; }

signalfx/signalfx-dotnet-tracing

tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/OwinWebApi2Tests.cs
@@ -12,7 +12,6 @@ using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Datadog.Trace.Configuration;
@@ -54,6 +53,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
         {
             SetServiceVersion("1.0.0");
             SetEnvironmentVariable(ConfigurationKeys.FeatureFlags.RouteTemplateResourceNamesEnabled, enableRouteTemplateResourceNames.ToString());
+            SetEnvironmentVariable(ConfigurationKeys.HeaderTags, CommonHttpHeaderNames.UserAgent + ":http_useragent");
 
             _fixture = fixture;
             _output = output;
@@ -127,6 +127,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             {
                 _httpClient = new HttpClient();
                 _httpClient.DefaultRequestHeaders.Add(CommonHttpHeaderNames.TracingEnabled, "false");
+                _httpClient.DefaultRequestHeaders.Add(CommonHttpHeaderNames.UserAgent, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36");
             }
 
             public MockTracerAgent Agent { get; private set; }