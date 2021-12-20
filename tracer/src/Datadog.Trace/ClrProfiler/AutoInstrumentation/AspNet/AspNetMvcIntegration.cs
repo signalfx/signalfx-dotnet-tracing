DataDog/dd-trace-dotnet

Change the public API to only use ISpan / IScope (#2090)
Goal:
Modify the public API to only work with interfaces `IScope` and `ISpan` instead of the implementation types `Scope` and `Span`. This is a required step for our current approach to solve the mismatching version issue.

Summary of changes:
- Introduced new public interfaces: `IScope`, `ISpan`, `ITracer`, `IHasTags`
- In public API's, replace implementation types with the new interfaces
- Renamed the internal `StartActiveWithTags` methods to `StartActiveInternal`
- Refactored internal usages of `Tracer.ActiveScope` (now returns `IScope`) to `Tracer.InternalActiveScope`
- In testing code, add lots of typecasts from `ISpan` to `Span` so we can access internal members like Duration, TraceContext, etc.

Internal API changes:
- `Span`: The Span object is always initialized with a `SpanContext` object, so the property `internal SpanContext Context` can always be accessed by our code to get internal properties like TraceContext and Origin.
- `Tracer`: The Tracer object always creates Scope objects from this assembly, so the property `internal Scope InternalActiveScope` can always be accessed by our code to get internal properties like Parent and Root.

Additional Changes:
- Add a public interface `ITracer` that the `Tracer` implements. This will allow for users to register the Tracer via DI, though we may need to make some more changes to the `ITracer` API before 2.0
- Add a public API test to `Datadog.Trace.OpenTracing.Tests`
- Add an internal interface called `IDatadogOpenTracingTracer` which has the absolute minimum API that we will maintain for communication from Datadog.Trace.OpenTracing => Datadog.Trace
- Add `"ReSharperTestRunner"` to the list of known TestRunners

tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AspNet/AspNetMvcIntegration.cs
@@ -144,7 +144,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AspNet
                     var tracer = Tracer.Instance;
                     var tagsFromHeaders = Enumerable.Empty<KeyValuePair<string, string>>();
 
-                    if (tracer.ActiveScope == null)
+                    if (tracer.InternalActiveScope == null)
                     {
                         try
                         {
@@ -160,7 +160,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AspNet
                     }
 
                     var tags = new AspNetTags();
-                    scope = Tracer.Instance.StartActiveWithTags(OperationName, propagatedContext, tags: tags);
+                    scope = Tracer.Instance.StartActiveInternal(OperationName, propagatedContext, tags: tags);
                     span = scope.Span;
 
                     span.DecorateWebServerSpan(

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AspNet/AspNetMvcIntegration.cs
@@ -138,7 +138,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AspNet
                     var tracer = Tracer.Instance;
                     var tagsFromHeaders = Enumerable.Empty<KeyValuePair<string, string>>();
 
-                    if (tracer.ActiveScope == null)
+                    if (tracer.InternalActiveScope == null)
                     {
                         try
                         {
@@ -155,7 +155,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AspNet
                     }
 
                     var tags = new AspNetTags();
-                    scope = Tracer.Instance.StartActiveWithTags(resourceName, propagatedContext, tags: tags);
+                    scope = Tracer.Instance.StartActiveInternal(resourceName, propagatedContext, tags: tags);
                     span = scope.Span;
 
                     span.DecorateWebServerSpan(