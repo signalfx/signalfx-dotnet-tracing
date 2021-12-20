DataDog/dd-trace-dotnet

[AppSec] Fix tag prefix bug (#2123)
Fix tag prefix bug that meant request headers where incorrectly labeled response headers.

tracer/src/Datadog.Trace/AppSec/Security.cs
@@ -177,9 +177,9 @@ namespace Datadog.Trace.AppSec
             }
         }
 
-        private static void AddHeaderTags(Span span, IHeadersCollection headers, Dictionary<string, string> headersToCollect)
+        private static void AddHeaderTags(Span span, IHeadersCollection headers, Dictionary<string, string> headersToCollect, string prefix)
         {
-            var tags = SpanContextPropagator.Instance.ExtractHeaderTags(headers, headersToCollect, defaultTagPrefix: SpanContextPropagator.HttpResponseHeadersTagPrefix);
+            var tags = SpanContextPropagator.Instance.ExtractHeaderTags(headers, headersToCollect, defaultTagPrefix: prefix);
             foreach (var tag in tags)
             {
                 span.SetTag(tag.Key, tag.Value);
@@ -227,14 +227,14 @@ namespace Datadog.Trace.AppSec
             span.SetTag(Tags.ActorIp, ipInfo.IpAddress);
 
             var headers = transport.GetRequestHeaders();
-            AddHeaderTags(span, headers, RequestHeaders);
+            AddHeaderTags(span, headers, RequestHeaders, SpanContextPropagator.HttpRequestHeadersTagPrefix);
 
             transport.OnCompleted(() =>
             {
                 TryAddEndPoint(span);
 
                 var headers = transport.GetResponseHeaders();
-                AddHeaderTags(span, headers, ResponseHeaders);
+                AddHeaderTags(span, headers, ResponseHeaders, SpanContextPropagator.HttpResponseHeadersTagPrefix);
             });
         }
 

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/AppSec/Security.cs
@@ -180,9 +180,9 @@ namespace Datadog.Trace.AppSec
             }
         }
 
-        private static void AddHeaderTags(Span span, IHeadersCollection headers, Dictionary<string, string> headersToCollect)
+        private static void AddHeaderTags(Span span, IHeadersCollection headers, Dictionary<string, string> headersToCollect, string prefix)
         {
-            var tags = PropagationExtensions.ExtractHeaderTags(headers, headersToCollect, defaultTagPrefix: PropagationExtensions.HttpResponseHeadersTagPrefix);
+            var tags = PropagationExtensions.ExtractHeaderTags(headers, headersToCollect, defaultTagPrefix: prefix);
             foreach (var tag in tags)
             {
                 span.SetTag(tag.Key, tag.Value);
@@ -230,14 +230,14 @@ namespace Datadog.Trace.AppSec
             span.SetTag(Tags.ActorIp, ipInfo.IpAddress);
 
             var headers = transport.GetRequestHeaders();
-            AddHeaderTags(span, headers, RequestHeaders);
+            AddHeaderTags(span, headers, RequestHeaders, PropagationExtensions.HttpRequestHeadersTagPrefix);
 
             transport.OnCompleted(() =>
             {
                 TryAddEndPoint(span);
 
                 var headers = transport.GetResponseHeaders();
-                AddHeaderTags(span, headers, ResponseHeaders);
+                AddHeaderTags(span, headers, ResponseHeaders, PropagationExtensions.HttpResponseHeadersTagPrefix);
             });
         }
 