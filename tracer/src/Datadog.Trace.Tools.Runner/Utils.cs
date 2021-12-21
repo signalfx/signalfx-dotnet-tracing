DataDog/dd-trace-dotnet

Isolate transport settings (#2166)

tracer/src/Datadog.Trace.Tools.Runner/Utils.cs
@@ -303,13 +303,13 @@ namespace Datadog.Trace.Tools.Runner
             {
                 if (!await agentWriter.Ping().ConfigureAwait(false))
                 {
-                    Console.WriteLine($"Error connecting to the Datadog Agent at {tracerSettings.AgentUri}.");
+                    Console.WriteLine($"Error connecting to the Datadog Agent at {tracerSettings.Exporter.AgentUri}.");
                     return false;
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error connecting to the Datadog Agent at {tracerSettings.AgentUri}.");
+                Console.WriteLine($"Error connecting to the Datadog Agent at {tracerSettings.Exporter.AgentUri}.");
                 Console.WriteLine(ex);
                 return false;
             }

tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.Exporter.cs
@@ -1,0 +1,71 @@
+// <copyright file="ConfigurationKeys.Exporter.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+namespace Datadog.Trace.Configuration
+{
+    /// <summary>
+    /// String constants for standard Datadog configuration keys.
+    /// </summary>
+    public static partial class ConfigurationKeys
+    {
+        /// <summary>
+        /// Configuration key for the Agent host where the Tracer can send traces.
+        /// Overridden by <see cref="AgentUri"/> if present.
+        /// Default value is "localhost".
+        /// </summary>
+        /// <seealso cref="ExporterSettings.AgentUri"/>
+        public const string AgentHost = "DD_AGENT_HOST";
+
+        /// <summary>
+        /// Configuration key for the Agent port where the Tracer can send traces.
+        /// Default value is 8126.
+        /// </summary>
+        /// <seealso cref="ExporterSettings.AgentUri"/>
+        public const string AgentPort = "DD_TRACE_AGENT_PORT";
+
+        /// <summary>
+        /// Configuration key for the named pipe where the Tracer can send traces.
+        /// Default value is <c>null</c>.
+        /// </summary>
+        /// <seealso cref="ExporterSettings.TracesPipeName"/>
+        public const string TracesPipeName = "DD_TRACE_PIPE_NAME";
+
+        /// <summary>
+        /// Configuration key for setting the timeout in milliseconds for named pipes communication.
+        /// Default value is <c>0</c>.
+        /// </summary>
+        /// <seealso cref="ExporterSettings.TracesPipeTimeoutMs"/>
+        public const string TracesPipeTimeoutMs = "DD_TRACE_PIPE_TIMEOUT_MS";
+
+        /// <summary>
+        /// Configuration key for the named pipe that DogStatsD binds to.
+        /// Default value is <c>null</c>.
+        /// </summary>
+        /// <seealso cref="ExporterSettings.MetricsPipeName"/>
+        public const string MetricsPipeName = "DD_DOGSTATSD_PIPE_NAME";
+
+        /// <summary>
+        /// Sibling setting for <see cref="AgentPort"/>.
+        /// Used to force a specific port binding for the Trace Agent.
+        /// Default value is 8126.
+        /// </summary>
+        /// <seealso cref="ExporterSettings.AgentUri"/>
+        public const string TraceAgentPortKey = "DD_APM_RECEIVER_PORT";
+
+        /// <summary>
+        /// Configuration key for the Agent URL where the Tracer can send traces.
+        /// Overrides values in <see cref="AgentHost"/> and <see cref="AgentPort"/> if present.
+        /// Default value is "http://localhost:8126".
+        /// </summary>
+        /// <seealso cref="ExporterSettings.AgentUri"/>
+        public const string AgentUri = "DD_TRACE_AGENT_URL";
+
+        /// <summary>
+        /// Configuration key for the DogStatsd port where the Tracer can send metrics.
+        /// Default value is 8125.
+        /// </summary>
+        public const string DogStatsdPort = "DD_DOGSTATSD_PORT";
+    }
+}

tracer/test/Datadog.Trace.Tests/TracerInstanceTest.cs
@@ -72,7 +72,10 @@ namespace Datadog.Trace.Tests
             {
                 var oldSettings = new TracerSettings
                 {
-                    AgentUri = new Uri($"http://127.0.0.1:{agent.Port}"),
+                    Exporter = new ExporterSettings()
+                    {
+                        AgentUri = new Uri($"http://127.0.0.1:{agent.Port}"),
+                    },
                     TracerMetricsEnabled = false,
                     StartupDiagnosticLogEnabled = false,
                     GlobalTags = new Dictionary<string, string> { { "test-tag", "original-value" } },
@@ -83,7 +86,10 @@ namespace Datadog.Trace.Tests
 
                 var newSettings = new TracerSettings
                 {
-                    AgentUri = new Uri($"http://127.0.0.1:{agent.Port}"),
+                    Exporter = new ExporterSettings()
+                    {
+                        AgentUri = new Uri($"http://127.0.0.1:{agent.Port}"),
+                    },
                     TracerMetricsEnabled = false,
                     StartupDiagnosticLogEnabled = false,
                     GlobalTags = new Dictionary<string, string> { { "test-tag", "new-value" } },

tracer/test/benchmarks/Benchmarks.Trace/AgentWriterBenchmark.cs
@@ -27,7 +27,7 @@ namespace Benchmarks.Trace
             settings.StartupDiagnosticLogEnabled = false;
             settings.TraceEnabled = false;
 
-            var api = new Api(settings.AgentUri, new FakeApiRequestFactory(), statsd: null, updateSampleRates: null, isPartialFlushEnabled: false);
+            var api = new Api(settings.Exporter.AgentUri, new FakeApiRequestFactory(), statsd: null, updateSampleRates: null, isPartialFlushEnabled: false);
 
             AgentWriter = new AgentWriter(api, statsd: null, automaticFlush: false);
 

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace.Tools.Runner/Utils.cs
@@ -315,13 +315,13 @@ namespace Datadog.Trace.Tools.Runner
             {
                 if (!await agentWriter.Ping().ConfigureAwait(false))
                 {
-                    Console.WriteLine($"Error connecting to the Datadog Agent at {tracerSettings.AgentUri}.");
+                    Console.WriteLine($"Error connecting to the Datadog Agent at {tracerSettings.ExporterSettings.AgentUri}.");
                     return false;
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error connecting to the Datadog Agent at {tracerSettings.AgentUri}.");
+                Console.WriteLine($"Error connecting to the Datadog Agent at {tracerSettings.ExporterSettings.AgentUri}.");
                 Console.WriteLine(ex);
                 return false;
             }

tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.Exporter.cs
@@ -1,0 +1,78 @@
+// <copyright file="ConfigurationKeys.Exporter.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+namespace Datadog.Trace.Configuration
+{
+    /// <summary>
+    /// String constants for standard Datadog configuration keys.
+    /// </summary>
+    public static partial class ConfigurationKeys
+    {
+        /// <summary>
+        /// Configuration key for the Agent host where the Tracer can send traces.
+        /// Overridden by <see cref="AgentUri"/> if present.
+        /// Default value is "localhost".
+        /// </summary>
+        /// <seealso cref="ExporterSettings.AgentUri"/>
+        public const string AgentHost = "SINGALFX_AGENT_HOST";
+
+        /// <summary>
+        /// Configuration key for the Agent port where the Tracer can send traces.
+        /// Default value is 8126.
+        /// </summary>
+        /// <seealso cref="ExporterSettings.AgentUri"/>
+        public const string AgentPort = "SINGALFX_TRACE_AGENT_PORT";
+
+        /// <summary>
+        /// Configuration key for the named pipe where the Tracer can send traces.
+        /// Default value is <c>null</c>.
+        /// </summary>
+        /// <seealso cref="ExporterSettings.TracesPipeName"/>
+        public const string TracesPipeName = "SINGALFX_TRACE_PIPE_NAME";
+
+        /// <summary>
+        /// Configuration key for setting the timeout in milliseconds for named pipes communication.
+        /// Default value is <c>0</c>.
+        /// </summary>
+        /// <seealso cref="ExporterSettings.TracesPipeTimeoutMs"/>
+        public const string TracesPipeTimeoutMs = "SINGALFX_TRACE_PIPE_TIMEOUT_MS";
+
+        /// <summary>
+        /// Configuration key for the named pipe that DogStatsD binds to.
+        /// Default value is <c>null</c>.
+        /// </summary>
+        /// <seealso cref="ExporterSettings.MetricsPipeName"/>
+        public const string MetricsPipeName = "SINGALFX_DOGSTATSD_PIPE_NAME";
+
+        /// <summary>
+        /// Sibling setting for <see cref="AgentPort"/>.
+        /// Used to force a specific port binding for the Trace Agent.
+        /// Default value is 8126.
+        /// </summary>
+        /// <seealso cref="ExporterSettings.AgentUri"/>
+        public const string TraceAgentPortKey = "SINGALFX_APM_RECEIVER_PORT";
+
+        /// <summary>
+        /// Configuration key for the Agent URL where the Tracer can send traces.
+        /// Overrides values in <see cref="AgentHost"/> and <see cref="AgentPort"/> if present.
+        /// Default value is "http://localhost:8126".
+        /// </summary>
+        /// <seealso cref="ExporterSettings.AgentUri"/>
+        public const string AgentUri = "SINGALFX_TRACE_AGENT_URL";
+
+        /// <summary>
+        /// Configuration key for the trace endpoint. Same as <see creg="AgentUri"/> created
+        /// for compatibility of previous version of SignalFx .NET Tracing.
+        /// </summary>
+        /// <seealso cref="AgentUri"/>
+        public const string EndpointUrl = "SIGNALFX_ENDPOINT_URL";
+
+        /// <summary>
+        /// Configuration key for the DogStatsd port where the Tracer can send metrics.
+        /// Default value is 8125.
+        /// </summary>
+        public const string DogStatsdPort = "SINGALFX_DOGSTATSD_PORT";
+    }
+}

tracer/test/Datadog.Trace.Tests/TracerInstanceTest.cs
@@ -75,7 +75,10 @@ namespace Datadog.Trace.Tests
                 var oldSettings = new TracerSettings
                 {
                     Exporter = ExporterType.DatadogAgent,
-                    AgentUri = new Uri($"http://127.0.0.1:{agent.Port}"),
+                    ExporterSettings = new ExporterSettings()
+                    {
+                        AgentUri = new Uri($"http://127.0.0.1:{agent.Port}"),
+                    },
                     TracerMetricsEnabled = false,
                     StartupDiagnosticLogEnabled = false,
                     GlobalTags = new Dictionary<string, string> { { "test-tag", "original-value" } },
@@ -87,7 +90,10 @@ namespace Datadog.Trace.Tests
                 var newSettings = new TracerSettings
                 {
                     Exporter = ExporterType.DatadogAgent,
-                    AgentUri = new Uri($"http://127.0.0.1:{agent.Port}"),
+                    ExporterSettings = new ExporterSettings()
+                    {
+                        AgentUri = new Uri($"http://127.0.0.1:{agent.Port}"),
+                    },
                     TracerMetricsEnabled = false,
                     StartupDiagnosticLogEnabled = false,
                     GlobalTags = new Dictionary<string, string> { { "test-tag", "new-value" } },

tracer/test/benchmarks/Benchmarks.Trace/AgentWriterBenchmark.cs
@@ -28,7 +28,7 @@ namespace Benchmarks.Trace
             settings.StartupDiagnosticLogEnabled = false;
             settings.TraceEnabled = false;
 
-            var api = new Api(settings.AgentUri, new FakeApiRequestFactory(), statsd: null, updateSampleRates: null, isPartialFlushEnabled: false);
+            var api = new Api(settings.ExporterSettings.AgentUri, new FakeApiRequestFactory(), statsd: null, updateSampleRates: null, isPartialFlushEnabled: false);
 
             AgentWriter = new AgentWriter(api, new NullMetrics(), automaticFlush: false);
 