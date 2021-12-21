feb31b7399d2bf87fad876fedc50804960f5cafb
Zach Montoya <zach.montoya@datadoghq.com>
2021-12-21 15:15:52 -0800

    Remove all LibLog code and simplify ScopeManager code (#2184)
    In Tracer 2.0, all automatic logs injection is done via automatic instrumentation. This means we no longer have a need for the LibLog library and the additional complexity it introduced in the Tracer code. This removes all of that, including the ScopeManager event handlers that the LibLogScopeEventSubscriber relied on to perform correct logs correlation.

--- tracer/src/Datadog.Trace/Ci/CITracerManagerFactory.cs
+++ tracer/src/Datadog.Trace/Ci/CITracerManagerFactory.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="CITracerManagerFactory.cs" company="Datadog">
+// <copyright file="CITracerManagerFactory.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
@@ -10,7 +10,6 @@ using Datadog.Trace.Ci.Agent;
 using Datadog.Trace.Ci.Sampling;
 using Datadog.Trace.Configuration;
 using Datadog.Trace.Conventions;
-using Datadog.Trace.Logging;
 using Datadog.Trace.Propagation;
 using Datadog.Trace.RuntimeMetrics;
 using Datadog.Trace.Sampling;
@@ -28,11 +27,10 @@ namespace Datadog.Trace.Ci
             IScopeManager scopeManager,
             IDogStatsd statsd,
             RuntimeMetricsWriter runtimeMetrics,
-            LibLogScopeEventSubscriber libLogSubscriber,
             ITraceIdConvention traceIdConvention,
             string defaultServiceName)
         {
-            return new CITracerManager(settings, agentWriter, sampler, propagator, scopeManager, statsd, runtimeMetrics, libLogSubscriber, traceIdConvention, defaultServiceName);
+            return new CITracerManager(settings, agentWriter, sampler, propagator, scopeManager, statsd, runtimeMetrics, traceIdConvention, defaultServiceName);
         }
 
         protected override ISampler GetSampler(ImmutableTracerSettings settings)
