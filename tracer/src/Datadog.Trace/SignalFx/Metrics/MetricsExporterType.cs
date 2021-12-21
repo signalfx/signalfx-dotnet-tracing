9ac5b1675aeb35129de47af85bbbf7d86b26ce17
Mateusz Łach <mateusza@splunk.com>
2021-12-21 10:26:40 +0100

    initial metrics exporter (#254)

---
+++ tracer/src/Datadog.Trace/SignalFx/Metrics/MetricsExporterType.cs
@@ -0,0 +1,23 @@
+﻿namespace Datadog.Trace.SignalFx.Metrics
+{
+    /// <summary>
+    /// Enumeration for the available metric exporter types.
+    /// </summary>
+    public enum MetricsExporterType
+    {
+        /// <summary>
+        /// The default exporter.
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// StatsD based exporter.
+        /// </summary>
+        StatsD,
+
+        /// <summary>
+        /// SignalFx exporter.
+        /// </summary>
+        SignalFx = Default
+    }
+}
