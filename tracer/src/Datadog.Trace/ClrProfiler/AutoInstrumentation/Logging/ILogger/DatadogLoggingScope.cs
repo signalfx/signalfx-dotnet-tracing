DataDog/dd-trace-dotnet

[Redo] Fix logs injection to work correctly when there's a version mismatch (#2161)
- Enables logs injection to work with the new DistributedTracer when version conflicts occur by using the `IReadOnlyDictionary<string, string> IDistributedTracer.GetSpanContext()` API to get the active TraceId / SpanId instead of the local active scope. We only need to fetch the TraceId and SpanId, so we access the dictionary with the appropriate keys.
- Lowers the minimum assembly version for NLog from 2.1.0 to 1.0.0.505, which passes both the DistributedTrace case and the cross-AppDomain scenarios

tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Logging/ILogger/DatadogLoggingScope.cs
@@ -48,8 +48,8 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Logging.ILogger
                     0 => new KeyValuePair<string, object>("dd_service", _service),
                     1 => new KeyValuePair<string, object>("dd_env", _env),
                     2 => new KeyValuePair<string, object>("dd_version", _version),
-                    3 => new KeyValuePair<string, object>("dd_trace_id", (_tracer.ActiveScope?.Span.TraceId ?? 0).ToString()),
-                    4 => new KeyValuePair<string, object>("dd_span_id", (_tracer.ActiveScope?.Span.SpanId ?? 0).ToString()),
+                    3 => new KeyValuePair<string, object>("dd_trace_id", _tracer.DistributedSpanContext?[HttpHeaderNames.TraceId] ?? "0"),
+                    4 => new KeyValuePair<string, object>("dd_span_id", _tracer.DistributedSpanContext?[HttpHeaderNames.ParentId] ?? "0"),
                     _ => throw new ArgumentOutOfRangeException(nameof(index))
                 };
             }
@@ -57,8 +57,10 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Logging.ILogger
 
         public override string ToString()
         {
-            var span = _tracer.ActiveScope?.Span;
-            if (span is null)
+            var spanContext = _tracer.DistributedSpanContext;
+            if (spanContext is null
+                || !spanContext.TryGetValue(HttpHeaderNames.TraceId, out string traceId)
+                || !spanContext.TryGetValue(HttpHeaderNames.ParentId, out string spanId))
             {
                 return _cachedFormat;
             }
@@ -67,21 +69,23 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Logging.ILogger
                 CultureInfo.InvariantCulture,
                 "{0}, dd_trace_id:\"{1}\", dd_span_id:\"{2}\"",
                 _cachedFormat,
-                span.TraceId,
-                span.SpanId);
+                traceId,
+                spanId);
         }
 
         public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
         {
-            var span = _tracer.ActiveScope?.Span;
+            var spanContext = _tracer.DistributedSpanContext;
             yield return new KeyValuePair<string, object>("dd_service", _service);
             yield return new KeyValuePair<string, object>("dd_env", _env);
             yield return new KeyValuePair<string, object>("dd_version", _version);
 
-            if (span is not null)
+            if (spanContext is not null
+                && spanContext.TryGetValue(HttpHeaderNames.TraceId, out string traceId)
+                && spanContext.TryGetValue(HttpHeaderNames.ParentId, out string spanId))
             {
-                yield return new KeyValuePair<string, object>("dd_trace_id", span.TraceId.ToString());
-                yield return new KeyValuePair<string, object>("dd_span_id", span.SpanId.ToString());
+                yield return new KeyValuePair<string, object>("dd_trace_id", traceId);
+                yield return new KeyValuePair<string, object>("dd_span_id", spanId);
             }
         }
 

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Logging/ILogger/DatadogLoggingScope.cs
@@ -59,8 +59,10 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Logging.ILogger
 
         public override string ToString()
         {
-            var span = _tracer.ActiveScope?.Span;
-            if (span is null)
+            var spanContext = _tracer.DistributedSpanContext;
+            if (spanContext is null
+                || !spanContext.TryGetValue(HttpHeaderNames.TraceId, out string traceId)
+                || !spanContext.TryGetValue(HttpHeaderNames.ParentId, out string spanId))
             {
                 return _cachedFormat;
             }
@@ -69,21 +71,23 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Logging.ILogger
                 CultureInfo.InvariantCulture,
                 "{0}, trace_id:\"{1}\", span_id:\"{2}\"",
                 _cachedFormat,
-                span.TraceId.ToString(),
-                span.SpanId);
+                traceId,
+                spanId);
         }
 
         public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
         {
-            var span = _tracer.ActiveScope?.Span;
-            yield return new KeyValuePair<string, object>(CorrelationIdentifier.ServiceKey, _service);
-            yield return new KeyValuePair<string, object>(CorrelationIdentifier.EnvKey, _env);
-            yield return new KeyValuePair<string, object>(CorrelationIdentifier.VersionKey, _version);
+            var spanContext = _tracer.DistributedSpanContext;
+            yield return new KeyValuePair<string, object>("dd_service", _service);
+            yield return new KeyValuePair<string, object>("dd_env", _env);
+            yield return new KeyValuePair<string, object>("dd_version", _version);
 
-            if (span is not null)
+            if (spanContext is not null
+                && spanContext.TryGetValue(HttpHeaderNames.TraceId, out string traceId)
+                && spanContext.TryGetValue(HttpHeaderNames.ParentId, out string spanId))
             {
-                yield return new KeyValuePair<string, object>(CorrelationIdentifier.TraceIdKey, span.TraceId.ToString());
-                yield return new KeyValuePair<string, object>(CorrelationIdentifier.SpanIdKey, span.SpanId.ToString());
+                yield return new KeyValuePair<string, object>(CorrelationIdentifier.TraceIdKey, traceId);
+                yield return new KeyValuePair<string, object>(CorrelationIdentifier.SpanIdKey, spanId);
             }
         }
 