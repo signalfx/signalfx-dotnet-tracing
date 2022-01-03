DataDog/dd-trace-dotnet

Support child actions in aspnet (#2139)
* Add Child Action in tests to check it breaks

* Handle Child actions use case

Having a child action would overwrite the scope stored in items.
We thus failed to close all spans leading to missing spans.

ResourceName needs a special handling in that case to make sure we don't overwrite the resource name with the child action one.

* Apply suggestions from code review

Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

* fix trailing whitespaces from github commit

* RouteTemplateResourceNamesEnabled works with child actions

* implement comments

Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

tracer/src/Datadog.Trace/AspNet/SharedItems.cs
@@ -1,0 +1,59 @@
+// <copyright file="SharedItems.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#if NETFRAMEWORK
+
+using System.Collections.Generic;
+using System.Web;
+
+namespace Datadog.Trace.AspNet
+{
+    internal static class SharedItems
+    {
+        public const string HttpContextPropagatedResourceNameKey = "__Datadog.Trace.ClrProfiler.Managed.AspNetMvcIntegration-aspnet.resourcename";
+
+        internal static void PushScope(HttpContext context, string key, Scope item)
+        {
+            if (context == null)
+            {
+                return;
+            }
+
+            // Storing only the scope by default to avoid allocating a stack if no inner calls are done
+            var existingItem = context.Items[key];
+            if (existingItem is null)
+            {
+                context.Items[key] = item;
+            }
+            else if (existingItem is Stack<Scope> stack)
+            {
+                stack.Push(item);
+            }
+            else if (existingItem is Scope previousScope)
+            {
+                var newStack = new Stack<Scope>();
+                newStack.Push(previousScope);
+                newStack.Push(item);
+                context.Items[key] = newStack;
+            }
+        }
+
+        internal static Scope TryPopScope(HttpContext context, string key)
+        {
+            var item = context?.Items[key];
+            if (item is Scope storedScope)
+            {
+                return storedScope;
+            }
+            else if (item is Stack<Scope> stack && stack.Count > 0)
+            {
+                return stack.Pop();
+            }
+
+            return default(Scope);
+        }
+    }
+}
+#endif

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/AspNet/SharedItems.cs
@@ -1,0 +1,61 @@
+// <copyright file="SharedItems.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+// Modified by Splunk Inc.
+
+#if NETFRAMEWORK
+
+using System.Collections.Generic;
+using System.Web;
+
+namespace Datadog.Trace.AspNet
+{
+    internal static class SharedItems
+    {
+        public const string HttpContextPropagatedResourceNameKey = "__SignalFx.Tracing.ClrProfiler.Managed.AspNetMvcIntegration-aspnet.resourcename";
+
+        internal static void PushScope(HttpContext context, string key, Scope item)
+        {
+            if (context == null)
+            {
+                return;
+            }
+
+            // Storing only the scope by default to avoid allocating a stack if no inner calls are done
+            var existingItem = context.Items[key];
+            if (existingItem is null)
+            {
+                context.Items[key] = item;
+            }
+            else if (existingItem is Stack<Scope> stack)
+            {
+                stack.Push(item);
+            }
+            else if (existingItem is Scope previousScope)
+            {
+                var newStack = new Stack<Scope>();
+                newStack.Push(previousScope);
+                newStack.Push(item);
+                context.Items[key] = newStack;
+            }
+        }
+
+        internal static Scope TryPopScope(HttpContext context, string key)
+        {
+            var item = context?.Items[key];
+            if (item is Scope storedScope)
+            {
+                return storedScope;
+            }
+            else if (item is Stack<Scope> stack && stack.Count > 0)
+            {
+                return stack.Pop();
+            }
+
+            return default(Scope);
+        }
+    }
+}
+#endif