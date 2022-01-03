DataDog/dd-trace-dotnet

CallTarget abstract methods support (#2120)
* Initial commit

* Changes

* changes.

* Changes

* Changes

* Finish implementation

* Add support for abstract calltarget definitions.

* Add Abstract ADO.NET integration.

* ADO.NET abstract integration.

* Restore the dependabot project.

* Restore launchSettings.json

* Adds another test case.

* Changes

* Fix tests

* Add support for abstract methods in System.Data version 2.0.0

* Add abstract instrumentation for netstandard version of System.Data.Common.DbCommand

* Add DBCommand smoke tests for DB2 and Devart DBCommands.

* Fix tests.

* fix tests.

* change the logic to support inheritance depth level

* Revert DBScopeFactory logic and tests.

* Changes from review.

* fix IntegrationId

* Fixes rebase from master

* Fix merge

tracer/src/Datadog.Trace/ClrProfiler/NativeMethods.cs
@@ -25,6 +25,11 @@ namespace Datadog.Trace.ClrProfiler
 
         public static void InitializeProfiler(string id, NativeCallTargetDefinition[] methodArrays)
         {
+            if (methodArrays is null || methodArrays.Length == 0)
+            {
+                return;
+            }
+
             if (IsWindows)
             {
                 Windows.InitializeProfiler(id, methodArrays, methodArrays.Length);
@@ -47,6 +52,23 @@ namespace Datadog.Trace.ClrProfiler
             }
         }
 
+        public static void AddDerivedInstrumentations(string id, NativeCallTargetDefinition[] methodArrays)
+        {
+            if (methodArrays is null || methodArrays.Length == 0)
+            {
+                return;
+            }
+
+            if (IsWindows)
+            {
+                Windows.AddDerivedInstrumentations(id, methodArrays, methodArrays.Length);
+            }
+            else
+            {
+                NonWindows.AddDerivedInstrumentations(id, methodArrays, methodArrays.Length);
+            }
+        }
+
         // the "dll" extension is required on .NET Framework
         // and optional on .NET Core
         private static class Windows
@@ -59,6 +81,9 @@ namespace Datadog.Trace.ClrProfiler
 
             [DllImport("Datadog.Trace.ClrProfiler.Native.dll")]
             public static extern void EnableByRefInstrumentation();
+
+            [DllImport("Datadog.Trace.ClrProfiler.Native.dll")]
+            public static extern void AddDerivedInstrumentations([MarshalAs(UnmanagedType.LPWStr)] string id, [In] NativeCallTargetDefinition[] methodArrays, int size);
         }
 
         // assume .NET Core if not running on Windows
@@ -72,6 +97,9 @@ namespace Datadog.Trace.ClrProfiler
 
             [DllImport("Datadog.Trace.ClrProfiler.Native")]
             public static extern void EnableByRefInstrumentation();
+
+            [DllImport("Datadog.Trace.ClrProfiler.Native")]
+            public static extern void AddDerivedInstrumentations([MarshalAs(UnmanagedType.LPWStr)] string id, [In] NativeCallTargetDefinition[] methodArrays, int size);
         }
     }
 }

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/ClrProfiler/NativeMethods.cs
@@ -25,6 +25,11 @@ namespace Datadog.Trace.ClrProfiler
 
         public static void InitializeProfiler(string id, NativeCallTargetDefinition[] methodArrays)
         {
+            if (methodArrays is null || methodArrays.Length == 0)
+            {
+                return;
+            }
+
             if (IsWindows)
             {
                 Windows.InitializeProfiler(id, methodArrays, methodArrays.Length);
@@ -44,6 +49,23 @@ namespace Datadog.Trace.ClrProfiler
             else
             {
                 NonWindows.EnableByRefInstrumentation();
+            }
+        }
+
+        public static void AddDerivedInstrumentations(string id, NativeCallTargetDefinition[] methodArrays)
+        {
+            if (methodArrays is null || methodArrays.Length == 0)
+            {
+                return;
+            }
+
+            if (IsWindows)
+            {
+                Windows.AddDerivedInstrumentations(id, methodArrays, methodArrays.Length);
+            }
+            else
+            {
+                NonWindows.AddDerivedInstrumentations(id, methodArrays, methodArrays.Length);
             }
         }
 
@@ -77,6 +99,9 @@ namespace Datadog.Trace.ClrProfiler
             [DllImport("SignalFx.Trace.ClrProfiler.Native.dll")]
             public static extern void EnableByRefInstrumentation();
 
+            [DllImport("SignalFx.Trace.ClrProfiler.Native.dll")]
+            public static extern void AddDerivedInstrumentations([MarshalAs(UnmanagedType.LPWStr)] string id, [In] NativeCallTargetDefinition[] methodArrays, int size);
+
             [DllImport("SignalFx.Tracing.ClrProfiler.Native.dll")]
             public static extern int SignalFxReadThreadSamples(int len, byte[] buf);
 
@@ -95,6 +120,9 @@ namespace Datadog.Trace.ClrProfiler
 
             [DllImport("SignalFx.Trace.ClrProfiler.Native")]
             public static extern void EnableByRefInstrumentation();
+
+            [DllImport("SignalFx.Trace.ClrProfiler.Native")]
+            public static extern void AddDerivedInstrumentations([MarshalAs(UnmanagedType.LPWStr)] string id, [In] NativeCallTargetDefinition[] methodArrays, int size);
 
             [DllImport("SignalFx.Tracing.ClrProfiler.Native")]
             public static extern int SignalFxReadThreadSamples(int len, byte[] buf);