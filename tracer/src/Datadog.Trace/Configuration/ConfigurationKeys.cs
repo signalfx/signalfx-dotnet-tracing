DataDog/dd-trace-dotnet

Do not normalize periods in header tags if specified by the customer (#2205)
* Fix normalization following Anna's spec

* Add Feature Flag

* change feature flag name

tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -349,6 +349,13 @@ namespace Datadog.Trace.Configuration
             /// </summary>
             /// <seealso cref="TracerSettings.DelayWcfInstrumentationEnabled"/>
             public const string DelayWcfInstrumentationEnabled = "DD_TRACE_DELAY_WCF_INSTRUMENTATION_ENABLED";
+
+            /// <summary>
+            /// Enables a fix around header tags normalization.
+            /// We used to normalize periods even if a tag was provided for a header, whereas we should not.
+            /// This flag defaults to true and is here in case customers need retrocompatibility only
+            /// </summary>
+            public const string HeaderTagsNormalizationFixEnabled = "DD_TRACE_HEADER_TAG_NORMALIZATION_FIX_ENABLED";
         }
     }
 }

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -437,6 +437,13 @@ namespace Datadog.Trace.Configuration
             /// </summary>
             /// <seealso cref="TracerSettings.DelayWcfInstrumentationEnabled"/>
             public const string DelayWcfInstrumentationEnabled = "SIGNALFX_TRACE_DELAY_WCF_INSTRUMENTATION_ENABLED";
+
+            /// <summary>
+            /// Enables a fix around header tags normalization.
+            /// We used to normalize periods even if a tag was provided for a header, whereas we should not.
+            /// This flag defaults to true and is here in case customers need retrocompatibility only
+            /// </summary>
+            public const string HeaderTagsNormalizationFixEnabled = "SIGNALFX_TRACE_HEADER_TAG_NORMALIZATION_FIX_ENABLED";
         }
 
         internal static class ThreadSampling