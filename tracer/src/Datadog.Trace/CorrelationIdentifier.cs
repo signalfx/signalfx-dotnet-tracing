DataDog/dd-trace-dotnet

Move some static readonly fields to const (#2292)

tracer/src/Datadog.Trace/CorrelationIdentifier.cs
@@ -12,18 +12,18 @@ namespace Datadog.Trace
     /// </summary>
     public static class CorrelationIdentifier
     {
-        internal static readonly string ServiceKey = "dd.service";
-        internal static readonly string VersionKey = "dd.version";
-        internal static readonly string EnvKey = "dd.env";
-        internal static readonly string TraceIdKey = "dd.trace_id";
-        internal static readonly string SpanIdKey = "dd.span_id";
+        internal const string ServiceKey = "dd.service";
+        internal const string VersionKey = "dd.version";
+        internal const string EnvKey = "dd.env";
+        internal const string TraceIdKey = "dd.trace_id";
+        internal const string SpanIdKey = "dd.span_id";
 
         // Serilog property names require valid C# identifiers
-        internal static readonly string SerilogServiceKey = "dd_service";
-        internal static readonly string SerilogVersionKey = "dd_version";
-        internal static readonly string SerilogEnvKey = "dd_env";
-        internal static readonly string SerilogTraceIdKey = "dd_trace_id";
-        internal static readonly string SerilogSpanIdKey = "dd_span_id";
+        internal const string SerilogServiceKey = "dd_service";
+        internal const string SerilogVersionKey = "dd_version";
+        internal const string SerilogEnvKey = "dd_env";
+        internal const string SerilogTraceIdKey = "dd_trace_id";
+        internal const string SerilogSpanIdKey = "dd_span_id";
 
         /// <summary>
         /// Gets the name of the service

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/CorrelationIdentifier.cs
@@ -16,16 +16,16 @@ namespace Datadog.Trace
     /// </summary>
     public static class CorrelationIdentifier
     {
-        internal static readonly string ServiceKey = "service.name";
-        internal static readonly string VersionKey = "service.version";
-        internal static readonly string EnvKey = "deployment.environment";
-        internal static readonly string TraceIdKey = "trace_id";
-        internal static readonly string SpanIdKey = "span_id";
+        internal const string ServiceKey = "service.name";
+        internal const string VersionKey = "service.version";
+        internal const string EnvKey = "deployment.environment";
+        internal const string TraceIdKey = "trace_id";
+        internal const string SpanIdKey = "span_id";
 
         // Serilog property names require valid C# identifiers
-        internal static readonly string SerilogServiceKey = "service_name";
-        internal static readonly string SerilogVersionKey = "service_version";
-        internal static readonly string SerilogEnvKey = "deployment_environment";
+        internal const string SerilogServiceKey = "service_name";
+        internal const string SerilogVersionKey = "service_version";
+        internal const string SerilogEnvKey = "deployment_environment";
 
         /// <summary>
         /// Gets the name of the service