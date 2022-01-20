49b3038afc254fa6cedc4a98dffbda896fac78c8
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-01-20 14:11:18 +0100

    smoke thread sampling test

---
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/ThreadSamplingTests.cs
@@ -0,0 +1,38 @@
+// Modified by Splunk Inc.
+
+#if !NETFRAMEWORK
+
+using Datadog.Trace.TestHelpers;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Datadog.Trace.ClrProfiler.IntegrationTests
+{
+    public class ThreadSamplingTests : TestHelper
+    {
+        public ThreadSamplingTests(ITestOutputHelper output)
+            : base("ThreadSampling", output)
+        {
+            SetServiceVersion("1.0.0");
+        }
+
+        [SkippableFact]
+        [Trait("Category", "EndToEnd")]
+        [Trait("RunOnWindows", "True")]
+        public void SubmitTheadSamples()
+        {
+            SetEnvironmentVariable($"SIGNALFX_THREAD_SAMPLING_ENABLED", "true");
+            SetEnvironmentVariable($"SIGNALFX_THREAD_SAMPLING_PERIOD", "1000");
+
+            using (var agent = EnvironmentHelper.GetMockAgent())
+            using (var processResult = RunSampleAndWaitForExit(agent.Port))
+            {
+                // at application works 5 seconds, we should expect at least 3 attempts of thread sampling
+                processResult.StandardOutput.Should().Contain("thread samples captured at", AtLeast.Times(expected: 3));
+            }
+        }
+    }
+}
+
+#endif
