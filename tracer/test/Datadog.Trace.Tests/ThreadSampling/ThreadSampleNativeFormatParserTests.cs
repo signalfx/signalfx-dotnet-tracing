1efb3b61890adc42f188e11a63d6c8a872498e6a
Paulo Janotti <pjanotti@splunk.com>
2022-01-21 06:35:53 -0800

    Add tests to thread sampling batch parser

---
+++ tracer/test/Datadog.Trace.Tests/ThreadSampling/ThreadSampleNativeFormatParserTests.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Datadog.Trace.ThreadSampling;
+using VerifyTests;
+using VerifyXunit;
+using Xunit;
+
+namespace Datadog.Trace.Tests.ThreadSample
+{
+    [UsesVerify]
+    public class ThreadSampleNativeFormatParserTests
+    {
+        static ThreadSampleNativeFormatParserTests()
+        {
+            VerifierSettings.DerivePathInfo(
+                (sourceFile, projectDirectory, type, method) =>
+                {
+                    return new(directory: Path.Combine(projectDirectory, "..", "snapshots"));
+                });
+        }
+
+        public static IEnumerable<object[]> GetBufferFiles =>
+            Enumerable.Range(0, 3).Select(n => new object[] { $"Buffer{n:D6}" });
+
+        [Theory]
+        [MemberData(nameof(GetBufferFiles))]
+        public async Task ParseSampleBuffer(string fileName)
+        {
+            var buf = File.ReadAllBytes($"../../../ThreadSample/Buffers/{fileName}.bin");
+            var parser = new ThreadSampleNativeFormatParser(buf, buf.Length);
+            var samples = parser.Parse();
+
+            VerifySettings settings = new();
+            settings.UseParameters(fileName);
+            await Verifier.Verify(samples, settings);
+        }
+    }
+}
