6b7efe072f8fa26f0928ececf741618f6fde90ac
Rasmus Kuusmann <rkuusmann@splunk.com>
2022-01-26 12:34:49 +0200

    Fix log injection samples (#347)
    * fix log injection samples
    
    * update docs

---
+++ tracer/samples/AutomaticTraceIdInjection/NLog40Example/run.ps1
@@ -0,0 +1,12 @@
+﻿$Env:COR_ENABLE_PROFILING="1"
+$Env:COR_PROFILER="{B4C89B0F-9908-4F73-9F59-0D77C5A06874}"
+$Env:COR_PROFILER_PATH=(Join-Path $PWD "\..\..\..\bin\tracer-home\win-x64\SignalFx.Tracing.ClrProfiler.Native.dll" | Resolve-Path)
+
+$Env:SIGNALFX_DOTNET_TRACER_HOME=(Join-Path $PWD "\..\..\..\bin\tracer-home\" | Resolve-Path)
+
+$Env:SIGNALFX_ENV="dev"
+$Env:SIGNALFX_SERVICE_NAME="NLog40Example"
+$Env:SIGNALFX_VERSION="1.0.0"
+$Env:SIGNALFX_LOGS_INJECTION="true"
+
+dotnet run
--- tracer/samples/AutomaticTraceIdInjection/NLog45Example/Program.cs
+++ tracer/samples/AutomaticTraceIdInjection/NLog45Example/Program.cs
@@ -1,19 +1,22 @@
-using Datadog.Trace;
 using NLog;
+using OpenTracing.Util;
 
 namespace NLog45Example
 {
-    class Program
+    public class Program
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            // Obtain the automatically registered OpenTracing.Util.GlobalTracer instance
+            var tracer = GlobalTracer.Instance;
+
             using (MappedDiagnosticsContext.SetScoped("order-number", 1024))
             {
                 Logger.Info("Message before a trace.");
 
-                using (var scope = Tracer.Instance.StartActive("NLog45Example - Main()"))
+                using (var scope = tracer.BuildSpan("NLog45Example - Main()").StartActive(finishSpanOnDispose: true))
                 {
                     Logger.Info("Message during a trace.");
                 }
