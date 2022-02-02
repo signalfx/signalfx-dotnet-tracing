2b3c5528ce6477247d577c7194bf9bbcb6ec3d18
Andrew Lock <andrew.lock@datadoghq.com>
2022-02-02 15:36:46 +0000

    Don't write to the console.WriteLine (#2379)
    Just ran into this while testing something unrelated. Not sure exactly why it happened, but we shouldn't write to the console unprovoked, as it could break people's logs etc. Especially when we're running in CI etc, and people may be grepping logs for other things

--- tracer/src/Datadog.Trace.ClrProfiler.Managed.Loader/StartupLogger.cs
+++ tracer/src/Datadog.Trace.ClrProfiler.Managed.Loader/StartupLogger.cs
@@ -39,8 +39,6 @@ namespace Datadog.Trace.ClrProfiler.Managed.Loader
                         // ignore
                     }
                 }
-
-                Console.Error.WriteLine(message, args);
             }
             catch
             {
