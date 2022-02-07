e6fd34c5d6f2571b0fe89be207834e72024ee93c
Mateusz Lach <mateusza@splunk.com>
2022-02-07 12:59:03 +0100

    metric exporter retries

--- tracer/src/Datadog.Trace/SignalFx/Metrics/SignalFxMetricExporter.cs
+++ tracer/src/Datadog.Trace/SignalFx/Metrics/SignalFxMetricExporter.cs
@@ -1,7 +1,9 @@
-﻿// Modified by Splunk Inc.
+// Modified by Splunk Inc.
 
 using System;
+using System.Collections.Generic;
 using System.Net;
+using System.Threading;
 using Datadog.Trace.Logging;
 using Datadog.Trace.Propagation;
 using Datadog.Trace.Vendors.ProtoBuf;
@@ -14,10 +16,31 @@ namespace Datadog.Trace.SignalFx.Metrics
     /// </summary>
     internal class SignalFxMetricExporter : ISignalFxMetricExporter
     {
+        private const int MaxAttempts = 3;
+
+        private static readonly HashSet<HttpStatusCode> TransientErrorStatusCodes = new HashSet<HttpStatusCode>(new[]
+        {
+            HttpStatusCode.RequestTimeout,
+#if NETCOREAPP
+            HttpStatusCode.TooManyRequests
+#endif
+        });
+
+        private static readonly HashSet<WebExceptionStatus> TransientExceptionStatuses = new HashSet<WebExceptionStatus>(new[]
+        {
+            WebExceptionStatus.ConnectionClosed,
+            WebExceptionStatus.Timeout,
+            WebExceptionStatus.RequestCanceled
+        });
+
         private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(SignalFxMetricExporter));
+
         private readonly Uri _metricsEndpointAddress;
+
         private readonly string _apiToken;
+
         private readonly int _webRequestTimeoutMs;
+        private readonly TimeSpan _waitBeforeRetries;
 
         public SignalFxMetricExporter(Uri metricsEndpointAddress, string apiToken)
         {
@@ -29,6 +52,7 @@ namespace Datadog.Trace.SignalFx.Metrics
             _metricsEndpointAddress = metricsEndpointAddress;
             _apiToken = apiToken;
             _webRequestTimeoutMs = 500;
+            _waitBeforeRetries = TimeSpan.FromMilliseconds(200);
         }
 
         public void Send(DataPointUploadMessage msg)
@@ -38,45 +62,69 @@ namespace Datadog.Trace.SignalFx.Metrics
                 return;
             }
 
-            try
+            var attemptNumber = 1;
+
+            while (true)
             {
-                var webRequest = WebRequest.CreateHttp(_metricsEndpointAddress);
-                webRequest.ContentType = "application/x-protobuf";
-                webRequest.Method = "POST";
-                webRequest.Headers.Add(CommonHttpHeaderNames.TracingEnabled, "false");
-
-                if (!string.IsNullOrWhiteSpace(_apiToken))
+                try
                 {
-                    webRequest.Headers.Add("X-Sf-Token", _apiToken);
-                }
+                    var webRequest = WebRequest.CreateHttp(_metricsEndpointAddress);
+                    webRequest.ContentType = "application/x-protobuf";
+                    webRequest.Method = "POST";
+                    webRequest.Headers.Add(CommonHttpHeaderNames.TracingEnabled, "false");
 
-                webRequest.Timeout = _webRequestTimeoutMs;
+                    if (!string.IsNullOrWhiteSpace(_apiToken))
+                    {
+                        webRequest.Headers.Add("X-Sf-Token", _apiToken);
+                    }
 
-                using (var requestStream = webRequest.GetRequestStream())
-                {
-                    Serializer.Serialize(requestStream, msg);
-                    requestStream.Flush();
-                }
+                    webRequest.Timeout = _webRequestTimeoutMs;
 
-                using var webResponse = (HttpWebResponse)webRequest.GetResponse();
-                if (IndicatesSuccess(webResponse.StatusCode))
-                {
+                    using (var requestStream = webRequest.GetRequestStream())
+                    {
+                        Serializer.Serialize(requestStream, msg);
+                        requestStream.Flush();
+                    }
+
+                    using var webResponse = (HttpWebResponse)webRequest.GetResponse();
+                    // if request failed (statusCode < 200 || statusCode >= 300), exception will be thrown
                     Log.Debug($"Sent {msg.datapoints.Count} metric data points to: {_metricsEndpointAddress}.");
+                    return;
                 }
-                else
+                catch (Exception e)
                 {
-                    Log.Error($"HTTP error sending metric data to {_metricsEndpointAddress}: {webResponse.StatusCode}.");
+                    if (attemptNumber >= MaxAttempts || !IsTransient(e))
+                    {
+                        Log.Error(e, $"Dropping metrics after {attemptNumber} unsuccessful attempts sending to: {_metricsEndpointAddress}.");
+                        return;
+                    }
+
+                    Log.Debug(e, "Transient exception encountered. Retrying sending metric data.");
                 }
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, $"Metric data could not be sent to {_metricsEndpointAddress}.");
+
+                attemptNumber++;
+                // short wait before retrying
+                Thread.Sleep(_waitBeforeRetries);
             }
         }
 
-        private static bool IndicatesSuccess(HttpStatusCode webResponseStatusCode)
+        private static bool IsTransient(Exception ex)
         {
-            return webResponseStatusCode >= HttpStatusCode.OK && webResponseStatusCode < HttpStatusCode.MultipleChoices;
+            var webException = ex as WebException;
+            if (webException != null)
+            {
+                var webExceptionStatus = webException.Status;
+                if (webExceptionStatus != WebExceptionStatus.ProtocolError)
+                {
+                    // these are considered to be transient
+                    return TransientExceptionStatuses.Contains(webExceptionStatus);
+                }
+
+                using var response = (HttpWebResponse)webException.Response;
+                return TransientErrorStatusCodes.Contains(response.StatusCode);
+            }
+
+            return false;
         }
     }
 }
