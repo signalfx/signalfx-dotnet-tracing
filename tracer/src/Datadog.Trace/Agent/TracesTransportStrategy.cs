551d4e6a7a5569d73a74d863f4b47f61c192b1b7
Andrew Lock <andrew.lock@datadoghq.com>
2022-02-11 09:29:10 +0000

    Add warning when attempting to use UDS on < .NET Core 3.1 (#2412)
    * Add warning when attempting to use UDS on < .NET Core 3.1
    
    * Update tracer/src/Datadog.Trace/Agent/TracesTransportStrategy.cs
    
    Co-authored-by: Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
    
    Co-authored-by: Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>

--- tracer/src/Datadog.Trace/Agent/TracesTransportStrategy.cs
+++ tracer/src/Datadog.Trace/Agent/TracesTransportStrategy.cs
@@ -29,6 +29,14 @@ namespace Datadog.Trace.Agent
                 case TracesTransportType.WindowsNamedPipe:
                     Log.Information<string, string, int>("Using {FactoryType} for trace transport, with pipe name {PipeName} and timeout {Timeout}ms.", nameof(NamedPipeClientStreamFactory), settings.TracesPipeName, settings.TracesPipeTimeoutMs);
                     return new HttpStreamRequestFactory(new NamedPipeClientStreamFactory(settings.TracesPipeName, settings.TracesPipeTimeoutMs), new DatadogHttpClient());
+                case TracesTransportType.UnixDomainSocket:
+#if NETCOREAPP3_1_OR_GREATER
+                    Log.Information<string, string, int>("Using {FactoryType} for trace transport, with Unix Domain Sockets path {Path} and timeout {Timeout}ms.", nameof(UnixDomainSocketStreamFactory), settings.TracesUnixDomainSocketPath, settings.TracesPipeTimeoutMs);
+                    return new HttpStreamRequestFactory(new UnixDomainSocketStreamFactory(settings.TracesUnixDomainSocketPath), new DatadogHttpClient());
+#else
+                    Log.Error("Using Unix Domain Sockets for trace transport is only supported on .NET Core 3.1 and greater. Falling back to default transport.");
+                    goto case TracesTransportType.Default;
+#endif
                 case TracesTransportType.Default:
                 default:
 #if NETCOREAPP
