f24dc9cc52f96b3a9f7777f04b961a07a6250b79
Andrew Lock <andrew.lock@datadoghq.com>
2022-02-13 18:55:38 +0000

    Generalise `HttpClientRequestFactory`, `ApiWebRequestFactory`, and `DatadogHttpHeaderHelper` (#2410)
    * Pass default headers in HttpClientRequestFactory constructor
    
    * Pass default headers in ApiWebRequestFactory constructor
    
    * Add unit tests for DatadogHttpHeaderHelper
    
    * Make DatadogHttpHeader abstract and create derived TraceAgentHttpHeaderHelper
    
    We need to be able to add different headers/content types for other use cases when using the DatadogHttpClient. This creates the required base class and the first implementation, ensuring the existing behaviour does not change
    
    * DirectLogSubmission sink doesn't need to add agent-specific headers
    
    * Rename DatadogHttpHeaderHelper to HttpHeaderHelperBase
    
    * Create a static factory for DatadogHttpClient

--- tracer/src/Datadog.Trace/Agent/TracesTransportStrategy.cs
+++ tracer/src/Datadog.Trace/Agent/TracesTransportStrategy.cs
@@ -25,18 +25,18 @@ namespace Datadog.Trace.Agent
             {
                 case TracesTransportType.CustomTcpProvider:
                     Log.Information("Using {FactoryType} for trace transport.", nameof(TcpStreamFactory));
-                    return new HttpStreamRequestFactory(new TcpStreamFactory(settings.AgentUri.Host, settings.AgentUri.Port), new DatadogHttpClient());
+                    return new HttpStreamRequestFactory(new TcpStreamFactory(settings.AgentUri.Host, settings.AgentUri.Port), DatadogHttpClient.CreateTraceAgentClient());
                 case TracesTransportType.WindowsNamedPipe:
                     Log.Information<string, string, int>("Using {FactoryType} for trace transport, with pipe name {PipeName} and timeout {Timeout}ms.", nameof(NamedPipeClientStreamFactory), settings.TracesPipeName, settings.TracesPipeTimeoutMs);
-                    return new HttpStreamRequestFactory(new NamedPipeClientStreamFactory(settings.TracesPipeName, settings.TracesPipeTimeoutMs), new DatadogHttpClient());
+                    return new HttpStreamRequestFactory(new NamedPipeClientStreamFactory(settings.TracesPipeName, settings.TracesPipeTimeoutMs), DatadogHttpClient.CreateTraceAgentClient());
                 case TracesTransportType.Default:
                 default:
 #if NETCOREAPP
                     Log.Information("Using {FactoryType} for trace transport.", nameof(HttpClientRequestFactory));
-                    return new HttpClientRequestFactory();
+                    return new HttpClientRequestFactory(AgentHttpHeaderNames.DefaultHeaders);
 #else
                     Log.Information("Using {FactoryType} for trace transport.", nameof(ApiWebRequestFactory));
-                    return new ApiWebRequestFactory();
+                    return new ApiWebRequestFactory(AgentHttpHeaderNames.DefaultHeaders);
 #endif
             }
         }
