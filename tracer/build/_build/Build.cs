e444fb16fe74490dbc15e0aa62b18ef17248909a
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-02-14 06:56:31 +0100

    Release 0.2.2 (#382)
    * release 0.2.2
    
    * Update docs/CHANGELOG.md
    
    Co-authored-by: Robert Pająk <pellared@hotmail.com>
    
    * Update docs/CHANGELOG.md
    
    Co-authored-by: Robert Pająk <pellared@hotmail.com>

--- tracer/build/_build/Build.cs
+++ tracer/build/_build/Build.cs
@@ -69,7 +69,7 @@ partial class Build : NukeBuild
     readonly bool IsAlpine = false;
 
     [Parameter("The build version. Default is latest")]
-    readonly string Version = "0.2.1";
+    readonly string Version = "0.2.2";
 
     [Parameter("Whether the build version is a prerelease(for packaging purposes). Default is latest")]
     readonly bool IsPrerelease = false;
