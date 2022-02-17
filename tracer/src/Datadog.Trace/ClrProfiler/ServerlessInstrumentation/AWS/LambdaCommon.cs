DataDog/dd-trace-dotnet

[Serverless] interact with extension (#2352)

tracer/src/Datadog.Trace/ClrProfiler/ServerlessInstrumentation/AWS/LambdaCommon.cs
@@ -4,47 +4,149 @@
 // </copyright>
 
 using System;
+using System.IO;
 using System.Net;
+using System.Text;
 
-using Datadog.Trace.Logging;
-using Datadog.Trace.Util;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.Vendors.Newtonsoft.Json;
 
 namespace Datadog.Trace.ClrProfiler.ServerlessInstrumentation.AWS
 {
     internal class LambdaCommon
     {
-        private const string TraceContextEndpointEnvName = "_DD_TRACE_CONTEXT_ENDPOINT";
-        private const string TraceContextEndpoint = "http://127.0.0.1:8124/lambda/trace-context";
         private const string PlaceholderServiceName = "placeholder-service";
         private const string PlaceholderOperationName = "placeholder-operation";
+        private const string DefaultJson = "{}";
+        private const double ServerlessMaxWaitingFlushTime = 3;
 
-        private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(LambdaCommon));
+        internal static CallTargetState StartInvocation<TArg>(TArg payload, ILambdaExtensionRequest requestBuilder)
+        {
+            var json = DefaultJson;
+            try
+            {
+                json = JsonConvert.SerializeObject(payload);
+            }
+            catch (Exception)
+            {
+                Serverless.Debug("Could not serialize input");
+            }
 
-        internal static Scope CreatePlaceholderScope(Tracer tracer)
+            NotifyExtensionStart(requestBuilder, json);
+            return new CallTargetState(CreatePlaceholderScope(Tracer.Instance, requestBuilder));
+        }
+
+        internal static CallTargetState StartInvocationWithoutEvent(ILambdaExtensionRequest requestBuilder)
+        {
+            return StartInvocation(DefaultJson, requestBuilder);
+        }
+
+        internal static CallTargetReturn<TReturn> EndInvocationSync<TReturn>(TReturn returnValue, Exception exception, Scope scope, ILambdaExtensionRequest requestBuilder)
+        {
+            scope?.Dispose();
+            Flush();
+            NotifyExtensionEnd(requestBuilder, exception != null);
+            return new CallTargetReturn<TReturn>(returnValue);
+        }
+
+        internal static TReturn EndInvocationAsync<TReturn>(TReturn returnValue, Exception exception, Scope scope, ILambdaExtensionRequest requestBuilder)
+        {
+            scope?.Dispose();
+            Flush();
+            NotifyExtensionEnd(requestBuilder, exception != null);
+            return returnValue;
+        }
+
+        internal static Scope CreatePlaceholderScope(Tracer tracer, ILambdaExtensionRequest requestBuilder)
         {
             Scope scope = null;
             try
             {
-                string endpoint = EnvironmentHelpers.GetEnvironmentVariable(TraceContextEndpointEnvName) ?? TraceContextEndpoint;
-                WebRequest request = WebRequest.Create(endpoint);
-                request.Credentials = CredentialCache.DefaultCredentials;
-                request.Headers.Set(HttpHeaderNames.TracingEnabled, "false");
-                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                var request = requestBuilder.GetTraceContextRequest();
+                using (var response = (HttpWebResponse)request.GetResponse())
                 {
-                    string traceId = response.Headers.Get(HttpHeaderNames.TraceId);
+                    var traceId = response.Headers.Get(HttpHeaderNames.TraceId);
                     // need to set the exact same spanId so nested spans (auto-instrumentation or manual) will have the correct parent-id
-                    string spanId = response.Headers.Get(HttpHeaderNames.SpanId);
+                    var spanId = response.Headers.Get(HttpHeaderNames.SpanId);
                     Serverless.Debug($"received traceId = {traceId} and spanId = {spanId}");
-                    var span = tracer.StartSpan(PlaceholderOperationName, null, serviceName: PlaceholderServiceName, traceId: Convert.ToUInt64(traceId), spanId: Convert.ToUInt64(spanId));
-                    scope = tracer.TracerManager.ScopeManager.Activate(span, true);
+                    var span = tracer.StartSpan(PlaceholderOperationName, null, serviceName: PlaceholderServiceName, traceId: Convert.ToUInt64(traceId), spanId: Convert.ToUInt64(spanId), addToTraceContext: false);
+                    scope = tracer.TracerManager.ScopeManager.Activate(span, false);
                 }
             }
             catch (Exception ex)
             {
-                Serverless.Error("Error creating the placeholder scope." + ex);
+                Serverless.Error("Error creating the placeholder scope", ex);
             }
 
             return scope;
+        }
+
+        internal static bool SendStartInvocation(ILambdaExtensionRequest requestBuilder, string data)
+        {
+            var request = requestBuilder.GetStartInvocationRequest();
+            var byteArray = Encoding.UTF8.GetBytes(data ?? DefaultJson);
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            return ValidateOKStatus((HttpWebResponse)request.GetResponse());
+        }
+
+        internal static bool SendEndInvocation(ILambdaExtensionRequest requestBuilder, bool isError)
+        {
+            var request = requestBuilder.GetEndInvocationRequest(isError);
+            return ValidateOKStatus((HttpWebResponse)request.GetResponse());
+        }
+
+        internal static bool ValidateOKStatus(HttpWebResponse response)
+        {
+            var statusCode = response.StatusCode;
+            Serverless.Debug("The extension responds with statusCode = " + statusCode);
+            return statusCode == HttpStatusCode.OK;
+        }
+
+        internal static void NotifyExtensionStart(ILambdaExtensionRequest requestBuilder, string json)
+        {
+            try
+            {
+                if (!SendStartInvocation(requestBuilder, json))
+                {
+                    Serverless.Debug("Extension does not send a status 200 OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                Serverless.Error("Could not send payload to the extension", ex);
+            }
+        }
+
+        internal static void NotifyExtensionEnd(ILambdaExtensionRequest requestBuilder, bool isError)
+        {
+            try
+            {
+                if (!SendEndInvocation(requestBuilder, isError))
+                {
+                    Serverless.Debug("Extension does not send a status 200 OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                Serverless.Error("Could not send payload to the extension", ex);
+            }
+        }
+
+        internal static void Flush()
+        {
+            try
+            {
+                // here we need a sync flush, since the lambda environment can be destroy after each invocation
+                // 3 seconds is enough to send payload to the extension (via localhost)
+                Tracer.Instance.TracerManager.AgentWriter.FlushTracesAsync().Wait(TimeSpan.FromSeconds(ServerlessMaxWaitingFlushTime));
+            }
+            catch (Exception ex)
+            {
+                Serverless.Error("Could not flush to the extension", ex);
+            }
         }
     }
 }

tracer/src/Datadog.Trace/ClrProfiler/ServerlessInstrumentation/AWS/LambdaRequestBuilder.cs
@@ -1,0 +1,57 @@
+// <copyright file="LambdaRequestBuilder.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System.Net;
+
+using Datadog.Trace.Agent.Transports;
+using Datadog.Trace.Util;
+
+namespace Datadog.Trace.ClrProfiler.ServerlessInstrumentation.AWS
+{
+    internal class LambdaRequestBuilder : ILambdaExtensionRequest
+    {
+        private const string EndInvocationPath = "/lambda/end-invocation";
+        private const string StartInvocationPath = "/lambda/start-invocation";
+        private const string TraceContextPath = "/trace-context";
+        private const string TraceContextUri = "http://127.0.0.1:8124";
+        private const string TraceContextUriEnvName = "_DD_TRACE_CONTEXT_ENDPOINT";
+
+        internal LambdaRequestBuilder()
+        {
+            Uri = EnvironmentHelpers.GetEnvironmentVariable(TraceContextUriEnvName) ?? TraceContextUri;
+        }
+
+        internal string Uri { get; }
+
+        WebRequest ILambdaExtensionRequest.GetTraceContextRequest()
+        {
+            var request = WebRequest.Create(Uri + TraceContextPath);
+            request.Headers.Set(HttpHeaderNames.TracingEnabled, "false");
+            return request;
+        }
+
+        WebRequest ILambdaExtensionRequest.GetStartInvocationRequest()
+        {
+            var request = WebRequest.Create(Uri + StartInvocationPath);
+            request.Method = "POST";
+            request.Headers.Set(HttpHeaderNames.TracingEnabled, "false");
+            request.ContentType = MimeTypes.Json;
+            return request;
+        }
+
+        WebRequest ILambdaExtensionRequest.GetEndInvocationRequest(bool isError)
+        {
+            var request = WebRequest.Create(Uri + EndInvocationPath);
+            request.Method = "POST";
+            request.Headers.Set(HttpHeaderNames.TracingEnabled, "false");
+            if (isError)
+            {
+                request.Headers.Set(HttpHeaderNames.InvocationError, "true");
+            }
+
+            return request;
+        }
+    }
+}

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/ClrProfiler/ServerlessInstrumentation/AWS/LambdaCommon.cs
@@ -4,48 +4,149 @@
 // </copyright>
 
 using System;
+using System.IO;
 using System.Net;
-
-using Datadog.Trace.Logging;
+using System.Text;
+using Datadog.Trace.ClrProfiler.CallTarget;
 using Datadog.Trace.Propagation;
-using Datadog.Trace.Util;
+using Datadog.Trace.Vendors.Newtonsoft.Json;
 
 namespace Datadog.Trace.ClrProfiler.ServerlessInstrumentation.AWS
 {
     internal class LambdaCommon
     {
-        private const string TraceContextEndpointEnvName = "_DD_TRACE_CONTEXT_ENDPOINT";
-        private const string TraceContextEndpoint = "http://127.0.0.1:8124/lambda/trace-context";
         private const string PlaceholderServiceName = "placeholder-service";
         private const string PlaceholderOperationName = "placeholder-operation";
+        private const string DefaultJson = "{}";
+        private const double ServerlessMaxWaitingFlushTime = 3;
 
-        private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(LambdaCommon));
+        internal static CallTargetState StartInvocation<TArg>(TArg payload, ILambdaExtensionRequest requestBuilder)
+        {
+            var json = DefaultJson;
+            try
+            {
+                json = JsonConvert.SerializeObject(payload);
+            }
+            catch (Exception)
+            {
+                Serverless.Debug("Could not serialize input");
+            }
 
-        internal static Scope CreatePlaceholderScope(Tracer tracer)
+            NotifyExtensionStart(requestBuilder, json);
+            return new CallTargetState(CreatePlaceholderScope(Tracer.Instance, requestBuilder));
+        }
+
+        internal static CallTargetState StartInvocationWithoutEvent(ILambdaExtensionRequest requestBuilder)
+        {
+            return StartInvocation(DefaultJson, requestBuilder);
+        }
+
+        internal static CallTargetReturn<TReturn> EndInvocationSync<TReturn>(TReturn returnValue, Exception exception, Scope scope, ILambdaExtensionRequest requestBuilder)
+        {
+            scope?.Dispose();
+            Flush();
+            NotifyExtensionEnd(requestBuilder, exception != null);
+            return new CallTargetReturn<TReturn>(returnValue);
+        }
+
+        internal static TReturn EndInvocationAsync<TReturn>(TReturn returnValue, Exception exception, Scope scope, ILambdaExtensionRequest requestBuilder)
+        {
+            scope?.Dispose();
+            Flush();
+            NotifyExtensionEnd(requestBuilder, exception != null);
+            return returnValue;
+        }
+
+        internal static Scope CreatePlaceholderScope(Tracer tracer, ILambdaExtensionRequest requestBuilder)
         {
             Scope scope = null;
             try
             {
-                string endpoint = EnvironmentHelpers.GetEnvironmentVariable(TraceContextEndpointEnvName) ?? TraceContextEndpoint;
-                WebRequest request = WebRequest.Create(endpoint);
-                request.Credentials = CredentialCache.DefaultCredentials;
-                request.Headers.Set(DDHttpHeaderNames.TracingEnabled, "false");
-                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                var request = requestBuilder.GetTraceContextRequest();
+                using (var response = (HttpWebResponse)request.GetResponse())
                 {
-                    string traceId = response.Headers.Get(DDHttpHeaderNames.TraceId);
+                    var traceId = response.Headers.Get(DDHttpHeaderNames.TraceId);
                     // need to set the exact same spanId so nested spans (auto-instrumentation or manual) will have the correct parent-id
-                    string spanId = response.Headers.Get(DDHttpHeaderNames.SpanId);
+                    var spanId = response.Headers.Get(DDHttpHeaderNames.SpanId);
                     Serverless.Debug($"received traceId = {traceId} and spanId = {spanId}");
-                    var span = tracer.StartSpan(PlaceholderOperationName, null, serviceName: PlaceholderServiceName, traceId: TraceId.CreateFromUlong(Convert.ToUInt64(traceId)), spanId: Convert.ToUInt64(spanId));
-                    scope = tracer.TracerManager.ScopeManager.Activate(span, true);
+                    var span = tracer.StartSpan(PlaceholderOperationName, null, serviceName: PlaceholderServiceName, traceId: TraceId.CreateFromUlong(Convert.ToUInt64(traceId)), spanId: Convert.ToUInt64(spanId), addToTraceContext: false);
+                    scope = tracer.TracerManager.ScopeManager.Activate(span, false);
                 }
             }
             catch (Exception ex)
             {
-                Serverless.Error("Error creating the placeholder scope." + ex);
+                Serverless.Error("Error creating the placeholder scope", ex);
             }
 
             return scope;
+        }
+
+        internal static bool SendStartInvocation(ILambdaExtensionRequest requestBuilder, string data)
+        {
+            var request = requestBuilder.GetStartInvocationRequest();
+            var byteArray = Encoding.UTF8.GetBytes(data ?? DefaultJson);
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            return ValidateOKStatus((HttpWebResponse)request.GetResponse());
+        }
+
+        internal static bool SendEndInvocation(ILambdaExtensionRequest requestBuilder, bool isError)
+        {
+            var request = requestBuilder.GetEndInvocationRequest(isError);
+            return ValidateOKStatus((HttpWebResponse)request.GetResponse());
+        }
+
+        internal static bool ValidateOKStatus(HttpWebResponse response)
+        {
+            var statusCode = response.StatusCode;
+            Serverless.Debug("The extension responds with statusCode = " + statusCode);
+            return statusCode == HttpStatusCode.OK;
+        }
+
+        internal static void NotifyExtensionStart(ILambdaExtensionRequest requestBuilder, string json)
+        {
+            try
+            {
+                if (!SendStartInvocation(requestBuilder, json))
+                {
+                    Serverless.Debug("Extension does not send a status 200 OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                Serverless.Error("Could not send payload to the extension", ex);
+            }
+        }
+
+        internal static void NotifyExtensionEnd(ILambdaExtensionRequest requestBuilder, bool isError)
+        {
+            try
+            {
+                if (!SendEndInvocation(requestBuilder, isError))
+                {
+                    Serverless.Debug("Extension does not send a status 200 OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                Serverless.Error("Could not send payload to the extension", ex);
+            }
+        }
+
+        internal static void Flush()
+        {
+            try
+            {
+                // here we need a sync flush, since the lambda environment can be destroy after each invocation
+                // 3 seconds is enough to send payload to the extension (via localhost)
+                Tracer.Instance.TracerManager.AgentWriter.FlushTracesAsync().Wait(TimeSpan.FromSeconds(ServerlessMaxWaitingFlushTime));
+            }
+            catch (Exception ex)
+            {
+                Serverless.Error("Could not flush to the extension", ex);
+            }
         }
     }
 }

tracer/src/Datadog.Trace/ClrProfiler/ServerlessInstrumentation/AWS/LambdaRequestBuilder.cs
@@ -1,0 +1,58 @@
+// <copyright file="LambdaRequestBuilder.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System.Net;
+
+using Datadog.Trace.Agent.Transports;
+using Datadog.Trace.Propagation;
+using Datadog.Trace.Util;
+
+namespace Datadog.Trace.ClrProfiler.ServerlessInstrumentation.AWS
+{
+    internal class LambdaRequestBuilder : ILambdaExtensionRequest
+    {
+        private const string EndInvocationPath = "/lambda/end-invocation";
+        private const string StartInvocationPath = "/lambda/start-invocation";
+        private const string TraceContextPath = "/trace-context";
+        private const string TraceContextUri = "http://127.0.0.1:8124";
+        private const string TraceContextUriEnvName = "_DD_TRACE_CONTEXT_ENDPOINT";
+
+        internal LambdaRequestBuilder()
+        {
+            Uri = EnvironmentHelpers.GetEnvironmentVariable(TraceContextUriEnvName) ?? TraceContextUri;
+        }
+
+        internal string Uri { get; }
+
+        WebRequest ILambdaExtensionRequest.GetTraceContextRequest()
+        {
+            var request = WebRequest.Create(Uri + TraceContextPath);
+            request.Headers.Set(DDHttpHeaderNames.TracingEnabled, "false");
+            return request;
+        }
+
+        WebRequest ILambdaExtensionRequest.GetStartInvocationRequest()
+        {
+            var request = WebRequest.Create(Uri + StartInvocationPath);
+            request.Method = "POST";
+            request.Headers.Set(DDHttpHeaderNames.TracingEnabled, "false");
+            request.ContentType = MimeTypes.Json;
+            return request;
+        }
+
+        WebRequest ILambdaExtensionRequest.GetEndInvocationRequest(bool isError)
+        {
+            var request = WebRequest.Create(Uri + EndInvocationPath);
+            request.Method = "POST";
+            request.Headers.Set(DDHttpHeaderNames.TracingEnabled, "false");
+            if (isError)
+            {
+                request.Headers.Set(DDHttpHeaderNames.InvocationError, "true");
+            }
+
+            return request;
+        }
+    }
+}