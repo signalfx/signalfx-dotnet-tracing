DataDog/dd-trace-dotnet

Decouple starting processes from Tracer Initialization (#2186)
* Decouple starting processes from Tracer Initilizattion

The loader is now in charge of starting processes if need be.
In AAS, even if DD_TRACE_ENABLED is set to false, the profiler will stay attached. It won't rejit any method though.

* Change variable names and improve the process start logic

* Implement changes asked in review

* Last few modifications after real tests on AAS

* Move Config keys to AzureAppServices and allow custom tracing when DD_TRACE_ENABLED is false

tracer/src/Datadog.Trace.ClrProfiler.Managed.Loader/Startup.cs
@@ -14,6 +14,10 @@ namespace Datadog.Trace.ClrProfiler.Managed.Loader
     public partial class Startup
     {
         private const string AssemblyName = "Datadog.Trace, Version=2.2.0.0, Culture=neutral, PublicKeyToken=def86d061d0d2eeb";
+        private const string AzureAppServicesKey = "DD_AZURE_APP_SERVICES";
+        private const string AasCustomTracingKey = "DD_AAS_ENABLE_CUSTOM_TRACING";
+        private const string AasCustomMetricsKey = "DD_AAS_ENABLE_CUSTOM_METRICS";
+        private const string TraceEnabledKey = "DD_TRACE_ENABLED";
 
         /// <summary>
         /// Initializes static members of the <see cref="Startup"/> class.
@@ -32,12 +36,35 @@ namespace Datadog.Trace.ClrProfiler.Managed.Loader
                 StartupLogger.Log(ex, "Unable to register a callback to the CurrentDomain.AssemblyResolve event.");
             }
 
-            TryLoadManagedAssembly();
+            var runInAas = ReadBooleanEnvironmentVariable(AzureAppServicesKey, false);
+            if (!runInAas)
+            {
+                TryInvokeManagedMethod("Datadog.Trace.ClrProfiler.Instrumentation", "Initialize");
+                return;
+            }
+
+            // In AAS, the loader can be used to load the tracer, the traceagent only (if only custom tracing is enabled),
+            // dogstatsd or all of them.
+            var customTracingEnabled = ReadBooleanEnvironmentVariable(AasCustomTracingKey, false);
+            var needsDogStatsD = ReadBooleanEnvironmentVariable(AasCustomMetricsKey, false);
+            var automaticTraceEnabled = ReadBooleanEnvironmentVariable(TraceEnabledKey, true);
+
+            if (automaticTraceEnabled || customTracingEnabled || needsDogStatsD)
+            {
+                StartupLogger.Log("Invoking managed method to start external processes.");
+                TryInvokeManagedMethod("Datadog.Trace.AgentProcessManager", "Initialize");
+            }
+
+            if (automaticTraceEnabled)
+            {
+                StartupLogger.Log("Invoking managed tracer.");
+                TryInvokeManagedMethod("Datadog.Trace.ClrProfiler.Instrumentation", "Initialize");
+            }
         }
 
         internal static string ManagedProfilerDirectory { get; }
 
-        private static void TryLoadManagedAssembly()
+        private static void TryInvokeManagedMethod(string typeName, string methodName)
         {
             try
             {
@@ -45,15 +72,14 @@ namespace Datadog.Trace.ClrProfiler.Managed.Loader
 
                 if (assembly != null)
                 {
-                    // call method Datadog.Trace.ClrProfiler.Instrumentation.Initialize()
-                    var type = assembly.GetType("Datadog.Trace.ClrProfiler.Instrumentation", throwOnError: false);
-                    var method = type?.GetRuntimeMethod("Initialize", parameters: Type.EmptyTypes);
+                    var type = assembly.GetType(typeName, throwOnError: false);
+                    var method = type?.GetRuntimeMethod(methodName, parameters: Type.EmptyTypes);
                     method?.Invoke(obj: null, parameters: null);
                 }
             }
             catch (Exception ex)
             {
-                StartupLogger.Log(ex, "Error when loading managed assemblies.");
+                StartupLogger.Log(ex, "Error when invoking managed method: {0}.{1}", typeName, methodName);
             }
         }
 
@@ -69,6 +95,17 @@ namespace Datadog.Trace.ClrProfiler.Managed.Loader
             }
 
             return null;
+        }
+
+        private static bool ReadBooleanEnvironmentVariable(string key, bool defaultValue)
+        {
+            var value = ReadEnvironmentVariable(key);
+            return value switch
+            {
+                "1" or "true" or "True" or "TRUE" or "t" or "T" => true,
+                "0" or "false" or "False" or "FALSE" or "f" or "F" => false,
+                _ => defaultValue
+            };
         }
     }
 }

tracer/src/Datadog.Trace.ClrProfiler.Native/environment_variables.h
@@ -75,6 +75,14 @@ namespace environment
     // of Azure App Services
     const WSTRING azure_app_services = WStr("DD_AZURE_APP_SERVICES");
 
+    // In AAS, Indicates whether the customer will need the trace agent even thoug
+    // automatic tracing is disabled
+    const WSTRING aas_needs_agent = WStr("DD_AAS_ENABLE_CUSTOM_TRACING");
+
+    // In AAS, Indicates whether the customer will need dogstatsd even thoug
+    // automatic tracing is disabled
+    const WSTRING aas_needs_dogstatsd = WStr("DD_AAS_ENABLE_CUSTOM_METRICS");
+
     // The app_pool_id in the context of azure app services
     const WSTRING azure_app_services_app_pool_id = WStr("APP_POOL_ID");
 

tracer/src/Datadog.Trace/PlatformHelpers/AzureAppServices.cs
@@ -77,6 +77,16 @@ namespace Datadog.Trace.PlatformHelpers
         /// </summary>
         internal const string OperatingSystemKey = "WEBSITE_OS";
 
+        /// <summary>
+        /// Used to force the loader to start the tracer agent (in case automatic instrumentation is disabled)
+        /// </summary>
+        public const string AasEnableCustomTracing = "DD_AAS_ENABLE_CUSTOM_TRACING";
+
+        /// <summary>
+        /// Used to force the loader to start dogstatsd (in case automatic instrumentation is disabled)
+        /// </summary>
+        public const string AasEnableCustomMetrics = "DD_AAS_ENABLE_CUSTOM_METRICS";
+
         private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(AzureAppServices));
 
         static AzureAppServices()
@@ -141,6 +151,8 @@ namespace Datadog.Trace.PlatformHelpers
                     Runtime = FrameworkDescription.Instance.Name;
 
                     DebugModeEnabled = GetVariableIfExists(Configuration.ConfigurationKeys.DebugEnabled, environmentVariables)?.ToBoolean() ?? false;
+                    CustomTracingEnabled = GetVariableIfExists(AasEnableCustomTracing, environmentVariables)?.ToBoolean() ?? false;
+                    NeedsDogStatsD = GetVariableIfExists(AasEnableCustomMetrics, environmentVariables)?.ToBoolean() ?? false;
                 }
             }
             catch (Exception ex)
@@ -153,6 +165,10 @@ namespace Datadog.Trace.PlatformHelpers
         public static AzureAppServices Metadata { get; set; }
 
         public bool DebugModeEnabled { get; }
+
+        public bool CustomTracingEnabled { get; }
+
+        public bool NeedsDogStatsD { get; }
 
         public bool IsRelevant { get; }
 

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace.ClrProfiler.Managed.Loader/Startup.cs
@@ -16,6 +16,10 @@ namespace Datadog.Trace.ClrProfiler.Managed.Loader
     public partial class Startup
     {
         private const string AssemblyName = "SignalFx.Tracing, Version=0.2.2.0, Culture=neutral, PublicKeyToken=e43a27c2023d388a";
+        private const string AzureAppServicesKey = "SIGNALFX_AZURE_APP_SERVICES";
+        private const string AasCustomTracingKey = "SIGNALFX_AAS_ENABLE_CUSTOM_TRACING";
+        private const string AasCustomMetricsKey = "SIGNALFX_AAS_ENABLE_CUSTOM_METRICS";
+        private const string TraceEnabledKey = "SIGNALFX_TRACE_ENABLED";
 
         /// <summary>
         /// Initializes static members of the <see cref="Startup"/> class.
@@ -34,12 +38,35 @@ namespace Datadog.Trace.ClrProfiler.Managed.Loader
                 StartupLogger.Log(ex, "Unable to register a callback to the CurrentDomain.AssemblyResolve event.");
             }
 
-            TryLoadManagedAssembly();
+            var runInAas = ReadBooleanEnvironmentVariable(AzureAppServicesKey, false);
+            if (!runInAas)
+            {
+                TryInvokeManagedMethod("Datadog.Trace.ClrProfiler.Instrumentation", "Initialize");
+                return;
+            }
+
+            // In AAS, the loader can be used to load the tracer, the traceagent only (if only custom tracing is enabled),
+            // dogstatsd or all of them.
+            var customTracingEnabled = ReadBooleanEnvironmentVariable(AasCustomTracingKey, false);
+            var needsDogStatsD = ReadBooleanEnvironmentVariable(AasCustomMetricsKey, false);
+            var automaticTraceEnabled = ReadBooleanEnvironmentVariable(TraceEnabledKey, true);
+
+            if (automaticTraceEnabled || customTracingEnabled || needsDogStatsD)
+            {
+                StartupLogger.Log("Invoking managed method to start external processes.");
+                TryInvokeManagedMethod("Datadog.Trace.AgentProcessManager", "Initialize");
+            }
+
+            if (automaticTraceEnabled)
+            {
+                StartupLogger.Log("Invoking managed tracer.");
+                TryInvokeManagedMethod("Datadog.Trace.ClrProfiler.Instrumentation", "Initialize");
+            }
         }
 
         internal static string ManagedProfilerDirectory { get; }
 
-        private static void TryLoadManagedAssembly()
+        private static void TryInvokeManagedMethod(string typeName, string methodName)
         {
             try
             {
@@ -47,15 +74,14 @@ namespace Datadog.Trace.ClrProfiler.Managed.Loader
 
                 if (assembly != null)
                 {
-                    // call method Datadog.Trace.ClrProfiler.Instrumentation.Initialize()
-                    var type = assembly.GetType("Datadog.Trace.ClrProfiler.Instrumentation", throwOnError: false);
-                    var method = type?.GetRuntimeMethod("Initialize", parameters: Type.EmptyTypes);
+                    var type = assembly.GetType(typeName, throwOnError: false);
+                    var method = type?.GetRuntimeMethod(methodName, parameters: Type.EmptyTypes);
                     method?.Invoke(obj: null, parameters: null);
                 }
             }
             catch (Exception ex)
             {
-                StartupLogger.Log(ex, "Error when loading managed assemblies.");
+                StartupLogger.Log(ex, "Error when invoking managed method: {0}.{1}", typeName, methodName);
             }
         }
 
@@ -71,6 +97,17 @@ namespace Datadog.Trace.ClrProfiler.Managed.Loader
             }
 
             return null;
+        }
+
+        private static bool ReadBooleanEnvironmentVariable(string key, bool defaultValue)
+        {
+            var value = ReadEnvironmentVariable(key);
+            return value switch
+            {
+                "1" or "true" or "True" or "TRUE" or "t" or "T" => true,
+                "0" or "false" or "False" or "FALSE" or "f" or "F" => false,
+                _ => defaultValue
+            };
         }
     }
 }

tracer/src/Datadog.Trace.ClrProfiler.Native/environment_variables.h
@@ -73,6 +73,14 @@ namespace environment {
     // of Azure App Services
     const WSTRING azure_app_services = WStr("SIGNALFX_AZURE_APP_SERVICES");
 
+    // In AAS, Indicates whether the customer will need the trace agent even thoug
+    // automatic tracing is disabled
+    const WSTRING aas_needs_agent = WStr("SIGNALFX_AAS_ENABLE_CUSTOM_TRACING");
+
+    // In AAS, Indicates whether the customer will need dogstatsd even thoug
+    // automatic tracing is disabled
+    const WSTRING aas_needs_dogstatsd = WStr("SIGNALFX_AAS_ENABLE_CUSTOM_METRICS");
+
     // The app_pool_id in the context of azure app services
     const WSTRING azure_app_services_app_pool_id = WStr("APP_POOL_ID");
 

tracer/src/Datadog.Trace/PlatformHelpers/AzureAppServices.cs
@@ -79,6 +79,16 @@ namespace Datadog.Trace.PlatformHelpers
         /// </summary>
         internal const string OperatingSystemKey = "WEBSITE_OS";
 
+        /// <summary>
+        /// Used to force the loader to start the tracer agent (in case automatic instrumentation is disabled)
+        /// </summary>
+        public const string AasEnableCustomTracing = "SIGNALFX_AAS_ENABLE_CUSTOM_TRACING";
+
+        /// <summary>
+        /// Used to force the loader to start dogstatsd (in case automatic instrumentation is disabled)
+        /// </summary>
+        public const string AasEnableCustomMetrics = "SIGNALFX_AAS_ENABLE_CUSTOM_METRICS";
+
         private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(AzureAppServices));
 
         static AzureAppServices()
@@ -143,6 +153,8 @@ namespace Datadog.Trace.PlatformHelpers
                     Runtime = FrameworkDescription.Instance.Name;
 
                     DebugModeEnabled = GetVariableIfExists(Configuration.ConfigurationKeys.DebugEnabled, environmentVariables)?.ToBoolean() ?? false;
+                    CustomTracingEnabled = GetVariableIfExists(AasEnableCustomTracing, environmentVariables)?.ToBoolean() ?? false;
+                    NeedsDogStatsD = GetVariableIfExists(AasEnableCustomMetrics, environmentVariables)?.ToBoolean() ?? false;
                 }
             }
             catch (Exception ex)
@@ -155,6 +167,10 @@ namespace Datadog.Trace.PlatformHelpers
         public static AzureAppServices Metadata { get; set; }
 
         public bool DebugModeEnabled { get; }
+
+        public bool CustomTracingEnabled { get; }
+
+        public bool NeedsDogStatsD { get; }
 
         public bool IsRelevant { get; }
 