01e332a7c9234d1546a2471688839a58637d07ae
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2022-03-07 15:59:51 +0100

    [Profiler] Implement new pipeline to generate .pprof via libddprof (#2524)
    Build a new native only pipeline allowing multiple sample collectors (only WallTime in the PR) to provide samples to the aggregator that generates and send .pprof file via libddprof.
    
    Note: the integration is connected to a feature flag set in the DD_INTERNAL_PROFILING_LIBDDPROF_ENABLED environment variable
    
    * Make native .pprof generation the default (#2515)
    * fix Visual Studio launchSettings.json to run in the tracer repository
    * Fix unit test for libddprof usage during .pprof generation
    * Make the old profile exporter pipeline the default one
    
    We previously set the new profile exporter as the default one.
    But this has not been sufficiently tested.
    Make the old profile exporter pipeline the default and add tests for the new one:
    - Address sanitizer
    - Smoke tests
    - Benchmark tests
    
    Co-authored-by: chrisnas <chrisnas@users.noreply.github.com>
    Co-authored-by: Gregory LEOCADIE <gregory.leocadie@datadoghq.com>

--- profiler/src/ProfilerEngine/Datadog.Profiler.Native/EnvironmentVariables.h
+++ profiler/src/ProfilerEngine/Datadog.Profiler.Native/EnvironmentVariables.h
@@ -8,12 +8,27 @@
 class EnvironmentVariables final
 {
 public:
-    inline static const shared::WSTRING ProfilingEnabled = WStr("SIGNALFX_PROFILING_ENABLED");
-    inline static const shared::WSTRING DebugLogEnabled = WStr("SIGNALFX_TRACE_DEBUG");
-    inline static const shared::WSTRING LogPath = WStr("SIGNALFX_PROFILING_LOG_PATH");
-    inline static const shared::WSTRING LogDirectory = WStr("SIGNALFX_PROFILING_LOG_DIR");
-    inline static const shared::WSTRING OperationalMetricsEnabled = WStr("SIGNALFX_INTERNAL_OPERATIONAL_METRICS_ENABLED");
-    inline static const shared::WSTRING Version = WStr("SIGNALFX_VERSION");
-    inline static const shared::WSTRING ServiceName = WStr("SIGNALFX_SERVICE");
-    inline static const shared::WSTRING Environment = WStr("SIGNALFX_ENV");
+    inline static const shared::WSTRING ProfilingEnabled            = WStr("SIGNALFX_PROFILING_ENABLED");
+    inline static const shared::WSTRING DebugLogEnabled             = WStr("SIGNALFX_TRACE_DEBUG");
+    inline static const shared::WSTRING LogPath                     = WStr("SIGNALFX_PROFILING_LOG_PATH");
+    inline static const shared::WSTRING LogDirectory                = WStr("SIGNALFX_PROFILING_LOG_DIR");
+    inline static const shared::WSTRING OperationalMetricsEnabled   = WStr("SIGNALFX_INTERNAL_OPERATIONAL_METRICS_ENABLED");
+    inline static const shared::WSTRING Version                     = WStr("SIGNALFX_VERSION");
+    inline static const shared::WSTRING ServiceName                 = WStr("SIGNALFX_SERVICE");
+    inline static const shared::WSTRING Environment                 = WStr("SIGNALFX_ENV");
+    inline static const shared::WSTRING Site                        = WStr("SIGNALFX_SITE");
+    inline static const shared::WSTRING UploadInterval              = WStr("SIGNALFX_PROFILING_UPLOAD_PERIOD");
+    inline static const shared::WSTRING AgentUrl                    = WStr("SIGNALFX_TRACE_AGENT_URL");
+    inline static const shared::WSTRING AgentHost                   = WStr("SIGNALFX_AGENT_HOST");
+    inline static const shared::WSTRING AgentPort                   = WStr("SIGNALFX_TRACE_AGENT_PORT");
+    inline static const shared::WSTRING ApiKey                      = WStr("SIGNALFX_API_KEY");
+    inline static const shared::WSTRING Hostname                    = WStr("SIGNALFX_HOSTNAME");
+    inline static const shared::WSTRING Tags                        = WStr("SIGNALFX_TAGS");
+    inline static const shared::WSTRING NativeFramesEnabled         = WStr("SIGNALFX_PROFILING_FRAMES_NATIVE_ENABLED");
+    inline static const shared::WSTRING ProfilesOutputDir           = WStr("SIGNALFX_PROFILING_OUTPUT_DIR");
+    inline static const shared::WSTRING DevelopmentConfiguration    = WStr("SIGNALFX_INTERNAL_USE_DEVELOPMENT_CONFIGURATION");
+    inline static const shared::WSTRING Agentless                   = WStr("SIGNALFX_PROFILING_AGENTLESS");
+
+    // feature flags
+    inline static const shared::WSTRING FF_LibddprofEnabled = WStr("SIGNALFX_INTERNAL_PROFILING_LIBDDPROF_ENABLED");
 };
