fe6c272b606992a6a144de251057ac38209253fa
Zach Montoya <zach.montoya@datadoghq.com>
2022-03-07 16:54:05 -0800

    stop propagating `x-datadog-tags` header (#2507)

--- tracer/src/Datadog.Trace/SpanContext.cs
+++ tracer/src/Datadog.Trace/SpanContext.cs
@@ -18,7 +18,7 @@ namespace Datadog.Trace
     /// </summary>
     public class SpanContext : ISpanContext, IReadOnlyDictionary<string, string>
     {
-        private static readonly string[] KeyNames = { "trace-id", "parent-id", "sampling-priority", "origin", "tags" };
+        private static readonly string[] KeyNames = { "trace-id", "parent-id", "sampling-priority", "origin" };
 
         /// <summary>
         /// An <see cref="ISpanContext"/> with default values. Can be used as the value for
@@ -144,16 +144,6 @@ namespace Datadog.Trace
         internal string Origin { get; set; }
 
         /// <summary>
-        /// Gets or sets a collection of propagated internal Datadog tags,
-        /// formatted as "key1=value1,key2=value2".
-        /// </summary>
-        /// <remarks>
-        /// We're keeping this as the string representation to avoid having to parse.
-        /// For now, it's relatively easy to append new values when needed.
-        /// </remarks>
-        internal string DatadogTags { get; set; }
-
-        /// <summary>
         /// Gets the trace state for W3C propagation
         /// </summary>
         internal string TraceState { get; }
@@ -253,10 +243,6 @@ namespace Datadog.Trace
 
                 case "origin":
                     value = Origin;
-                    return true;
-
-                case "tags":
-                    value = DatadogTags;
                     return true;
             }
 
