d729dd33bb73618fd7e543334ae2e6ea437b30d7
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-03-09 09:24:54 +0100

    remove runner (#420)

--- tracer/build/_build/Projects.cs
+++ tracer/build/_build/Projects.cs
@@ -17,10 +17,5 @@ public static class Projects
     public const string TraceIntegrationTests = "Datadog.Trace.IntegrationTests";
     public const string OpenTracingIntegrationTests = "Datadog.Trace.OpenTracing.IntegrationTests";
     public const string ClrProfilerIntegrationTests = "Datadog.Trace.ClrProfiler.IntegrationTests";
-    public const string ToolIntegrationTests = "Datadog.Trace.Tools.Runner.IntegrationTests";
-    public const string ToolArtifactsTests = "Datadog.Trace.Tools.Runner.ArtifactTests";
-
     public const string AutomapperTest = "AutomapperTest";
-
-    public const string Tool = "Datadog.Trace.Tools.Runner";
 }
