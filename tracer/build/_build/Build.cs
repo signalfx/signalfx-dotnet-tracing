DataDog/dd-trace-dotnet

Make all integration tests utilize Native Loader instead of loading `Datadog.Trace.ClrProfiler.Native` directly (#2358)
* Make all integration tests utilize the Native Loader instead of loading Datadog.Trace.ClrProfiler.Native.dll directly

* Fix NUKE dependency graph to ensure we build Native Loader *before* running integration tests
Rename "BuildMonitoringHome" to "BuildNativeLoader" in CI scripts as well

* Fix Native Loader crashing on Linux due to missing -lstdc++fs compiler flag

* Fix Native Loader crashing on Debian when loader.conf contains empty lines

* Utilize the Native Loader on ARM64

* Adjust all launchSettings.json files to new folder structure

* Change tracer home folder to lower-case.

* Enable testing both with and without the Native Loader in CI.

* Integration tests only utilize NativeLoader when 'use_native_loader' is true.

tracer/build/_build/Build.cs
@@ -44,7 +44,7 @@ partial class Build : NukeBuild
 
     [Parameter("The location to create the monitoring home directory. Default is ./shared/bin/monitoring-home ")]
     readonly AbsolutePath MonitoringHome;
-    [Parameter("The location to create the tracer home directory. Default is ./bin/tracer-home ")]
+    [Parameter("The location to create the tracer home directory. Default is ./shared/bin/monitoring-home/tracer ")]
     readonly AbsolutePath TracerHome;
     [Parameter("The location to create the dd-trace home directory. Default is ./bin/dd-tracer-home ")]
     readonly AbsolutePath DDTracerHome;
@@ -159,8 +159,8 @@ partial class Build : NukeBuild
         .DependsOn(CompileProfilerManagedSrc)
         .DependsOn(CompileProfilerNativeSrc);
 
-    Target BuildMonitoringHome => _ => _
-        .Description("Builds the native and managed src for the entire .NET APM product. Publishes to the monitoring home directory")
+    Target BuildNativeLoader => _ => _
+        .Description("Builds the Native Loader, and publishes to the monitoring home directory")
         .After(Clean)
         .DependsOn(CompileNativeLoader)
         .DependsOn(PublishNativeLoader);
@@ -175,7 +175,7 @@ partial class Build : NukeBuild
 
     Target PackageMonitoringHomeBeta => _ => _
         .Description("Packages the already built src")
-        .After(Clean, BuildTracerHome, BuildProfilerHome, BuildMonitoringHome)
+        .After(Clean, BuildTracerHome, BuildProfilerHome, BuildNativeLoader)
         .DependsOn(BuildMsiBeta);
 
     Target BuildAndRunManagedUnitTests => _ => _
@@ -203,18 +203,20 @@ partial class Build : NukeBuild
         .DependsOn(CompileSamples)
         .DependsOn(PublishIisSamples)
         .DependsOn(CompileIntegrationTests)
+        .DependsOn(BuildNativeLoader)
         .DependsOn(BuildRunnerTool);
 
     Target BuildWindowsRegressionTests => _ => _
         .Unlisted()
         .Requires(() => IsWin)
-        .Description("Builds the integration tests for Windows")
+        .Description("Builds the regression tests for Windows")
         .DependsOn(CompileManagedTestHelpers)
         .DependsOn(CreatePlatformlessSymlinks)
         .DependsOn(CompileRegressionDependencyLibs)
         .DependsOn(CompileRegressionSamples)
         .DependsOn(CompileFrameworkReproductions)
-        .DependsOn(CompileIntegrationTests);
+        .DependsOn(CompileIntegrationTests)
+        .DependsOn(BuildNativeLoader);
 
     Target BuildAndRunWindowsIntegrationTests => _ => _
         .Requires(() => IsWin)
@@ -243,6 +245,7 @@ partial class Build : NukeBuild
         .DependsOn(CompileSamplesLinux)
         .DependsOn(CompileMultiApiPackageVersionSamples)
         .DependsOn(CompileLinuxIntegrationTests)
+        .DependsOn(BuildNativeLoader)
         .DependsOn(BuildRunnerTool);
 
     Target BuildAndRunLinuxIntegrationTests => _ => _

signalfx/signalfx-dotnet-tracing

tracer/build/_build/Build.cs
@@ -48,7 +48,7 @@ partial class Build : NukeBuild
 
     [Parameter("The location to create the monitoring home directory. Default is ./shared/bin/monitoring-home ")]
     readonly AbsolutePath MonitoringHome;
-    [Parameter("The location to create the tracer home directory. Default is ./bin/tracer-home ")]
+    [Parameter("The location to create the tracer home directory. Default is ./shared/bin/monitoring-home/tracer ")]
     readonly AbsolutePath TracerHome;
     [Parameter("The location to create the dd-trace home directory. Default is ./bin/dd-tracer-home ")]
     readonly AbsolutePath DDTracerHome;
@@ -162,8 +162,8 @@ partial class Build : NukeBuild
         .DependsOn(CompileProfilerManagedSrc)
         .DependsOn(CompileProfilerNativeSrc);
 
-    Target BuildMonitoringHome => _ => _
-        .Description("Builds the native and managed src for the entire .NET APM product. Publishes to the monitoring home directory")
+    Target BuildNativeLoader => _ => _
+        .Description("Builds the Native Loader, and publishes to the monitoring home directory")
         .After(Clean)
         .DependsOn(CompileNativeLoader)
         .DependsOn(PublishNativeLoader);
@@ -178,7 +178,7 @@ partial class Build : NukeBuild
 
     Target PackageMonitoringHomeBeta => _ => _
         .Description("Packages the already built src")
-        .After(Clean, BuildTracerHome, BuildProfilerHome, BuildMonitoringHome)
+        .After(Clean, BuildTracerHome, BuildProfilerHome, BuildNativeLoader)
         .DependsOn(BuildMsiBeta);
 
     Target BuildAndRunManagedUnitTests => _ => _
@@ -204,18 +204,20 @@ partial class Build : NukeBuild
         .DependsOn(CreatePlatformlessSymlinks)
         .DependsOn(CompileSamples)
         .DependsOn(PublishIisSamples)
-        .DependsOn(CompileIntegrationTests);
+        .DependsOn(CompileIntegrationTests)
+        .DependsOn(BuildNativeLoader);
 
     Target BuildWindowsRegressionTests => _ => _
         .Unlisted()
         .Requires(() => IsWin)
-        .Description("Builds the integration tests for Windows")
+        .Description("Builds the regression tests for Windows")
         .DependsOn(CompileManagedTestHelpers)
         .DependsOn(CreatePlatformlessSymlinks)
         .DependsOn(CompileRegressionDependencyLibs)
         .DependsOn(CompileRegressionSamples)
         .DependsOn(CompileFrameworkReproductions)
-        .DependsOn(CompileIntegrationTests);
+        .DependsOn(CompileIntegrationTests)
+        .DependsOn(BuildNativeLoader);
 
     Target BuildAndRunWindowsIntegrationTests => _ => _
         .Requires(() => IsWin)
@@ -243,7 +245,8 @@ partial class Build : NukeBuild
         .DependsOn(CompileManagedTestHelpers)
         .DependsOn(CompileSamplesLinux)
         .DependsOn(CompileMultiApiPackageVersionSamples)
-        .DependsOn(CompileLinuxIntegrationTests);
+        .DependsOn(CompileLinuxIntegrationTests)
+        .DependsOn(BuildNativeLoader);
 
     Target BuildAndRunLinuxIntegrationTests => _ => _
         .Requires(() => !IsWin)