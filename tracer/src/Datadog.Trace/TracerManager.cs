cce536f5c7f315a1c6c04fcb74fae629633f0a45
Andrew Lock <andrew.lock@datadoghq.com>
2022-03-10 11:33:23 +0000

    Add ASP.NET support for `DD_TRACE_EXPAND_ROUTE_TEMPLATES_ENABLED` (#2517)
    * Add graphql and additional constraints to test slugs
    
    * Add tests for new routes, for testing additional route templates
    
    * Add snapshots for existing behaviour with additional route templates
    
    * Add initial snapshots
    
    These are copies of the WithFF snapshots, so represent behaviour when the expand resource template setting is _disabled_
    
    * Add AspNetResourceNameHelper for "cleaning" ASP.NET route templates
    
    Overload required for backwards compatibility with existing implementations
    
    * Add MVC and WebApi2 implementation
    
    Preserve existing behaviour while optimising slightly to avoid as many String.Replace() calls
    
    * Add tests for route template expansion
    
    * Add integration tests for expand route templates behaviour
    
    Most of the snapshots _don't_ change, as the vast majority of the routes we're testing are identifier-like (numbers). The ones that _do_ change are parameters where we use true/false, where we pass in a non-integer value to an integer parameter, and *slug
    
    * Add telemetry for ExpandRouteTemplatesEnabled
    
    * Fix null trolling

--- tracer/src/Datadog.Trace/TracerManager.cs
+++ tracer/src/Datadog.Trace/TracerManager.cs
@@ -366,6 +366,9 @@ namespace Datadog.Trace
                     writer.WritePropertyName("routetemplate_resourcenames_enabled");
                     writer.WriteValue(instanceSettings.RouteTemplateResourceNamesEnabled);
 
+                    writer.WritePropertyName("routetemplate_expansion_enabled");
+                    writer.WriteValue(instanceSettings.ExpandRouteTemplatesEnabled);
+
                     writer.WritePropertyName("partialflush_enabled");
                     writer.WriteValue(instanceSettings.ExporterSettings.PartialFlushEnabled);
 
