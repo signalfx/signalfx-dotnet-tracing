DataDog/dd-trace-dotnet

SpanContextPropagator refactor (#2536)
* Start SpanContextPropagator refactor.

* Initial propagator implementation.

* Changes.

* Changes.

* Refactor to add ISpanContextInjector, ISpanContextExtractor

* changes

* Refactoring

* SpanContext keys refactor

* Fix test suite.

* Fix tests.

* Add environment variables support.

* changes.

* Optimizations.

* Changes.

* Add W3C SpanContext propagator tests.

* Adds B3 context propagator.

* Context propagators tests.

* fixes

* Add Multi context propagator tests.

* Adds B3 single header context propagator support.

* fix typo

* Refactor to remove Delegates for Carrier getter and setter.

* fixes typo in name

* Fix rebase bits

* Optimization to reduce allocations.

* Add ContextPropagators.Names dynamically

* Changes.

* Improvements.

* Improvements.

* Changes

* Restore Key readonly struct.

* Add new test cases to increase coverage.

* Add Context propagators tests

tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Logging/Serilog/LogsInjection/LoggerDispatchInstrumentation.cs
@@ -56,12 +56,18 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Logging.Serilog.LogsInje
                 AddPropertyIfAbsent(dict, CorrelationIdentifier.SerilogEnvKey, tracer.Settings.Environment);
 
                 var spanContext = tracer.DistributedSpanContext;
-                if (spanContext is not null
-                    && spanContext.TryGetValue(HttpHeaderNames.TraceId, out string traceId)
-                    && spanContext.TryGetValue(HttpHeaderNames.ParentId, out string spanId))
+                if (spanContext is not null)
                 {
-                    AddPropertyIfAbsent(dict, CorrelationIdentifier.SerilogTraceIdKey, traceId);
-                    AddPropertyIfAbsent(dict, CorrelationIdentifier.SerilogSpanIdKey, spanId);
+                    // For mismatch version support we need to keep requesting old keys.
+                    var hasTraceId = spanContext.TryGetValue(SpanContext.Keys.TraceId, out string traceId) ||
+                                     spanContext.TryGetValue(HttpHeaderNames.TraceId, out traceId);
+                    var hasSpanId = spanContext.TryGetValue(SpanContext.Keys.ParentId, out string spanId) ||
+                                    spanContext.TryGetValue(HttpHeaderNames.ParentId, out spanId);
+                    if (hasTraceId && hasSpanId)
+                    {
+                        AddPropertyIfAbsent(dict, CorrelationIdentifier.SerilogTraceIdKey, traceId);
+                        AddPropertyIfAbsent(dict, CorrelationIdentifier.SerilogSpanIdKey, spanId);
+                    }
                 }
             }
 

tracer/src/Datadog.Trace/SpanContext.cs
@@ -17,6 +17,13 @@ namespace Datadog.Trace
     {
         private static readonly string[] KeyNames =
         {
+            Keys.TraceId,
+            Keys.ParentId,
+            Keys.SamplingPriority,
+            Keys.Origin,
+            Keys.RawTraceId,
+            Keys.RawSpanId,
+            // For mismatch version support we need to keep supporting old keys.
             HttpHeaderNames.TraceId,
             HttpHeaderNames.ParentId,
             HttpHeaderNames.SamplingPriority,
@@ -62,6 +69,28 @@ namespace Datadog.Trace
             SpanId = spanId;
             SamplingPriority = samplingPriority;
             Origin = origin;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpanContext"/> class
+        /// from a propagated context. <see cref="Parent"/> will be null
+        /// since this is a root context locally.
+        /// </summary>
+        /// <param name="traceId">The propagated trace id.</param>
+        /// <param name="spanId">The propagated span id.</param>
+        /// <param name="samplingPriority">The propagated sampling priority.</param>
+        /// <param name="serviceName">The service name to propagate to child spans.</param>
+        /// <param name="origin">The propagated origin of the trace.</param>
+        /// <param name="rawTraceId">The raw propagated trace id</param>
+        /// <param name="rawSpanId">The raw propagated span id</param>
+        internal SpanContext(ulong? traceId, ulong spanId, int? samplingPriority, string serviceName, string origin, string rawTraceId, string rawSpanId)
+            : this(traceId, serviceName)
+        {
+            SpanId = spanId;
+            SamplingPriority = samplingPriority;
+            Origin = origin;
+            RawTraceId = rawTraceId;
+            RawSpanId = rawSpanId;
         }
 
         /// <summary>
@@ -145,6 +174,16 @@ namespace Datadog.Trace
         /// </summary>
         internal int? SamplingPriority { get; }
 
+        /// <summary>
+        /// Gets the raw traceId (to support > 64bits)
+        /// </summary>
+        internal string RawTraceId { get; }
+
+        /// <summary>
+        /// Gets the raw spanId
+        /// </summary>
+        internal string RawSpanId { get; }
+
         /// <inheritdoc/>
         int IReadOnlyCollection<KeyValuePair<string, string>>.Count => KeyNames.Length;
 
@@ -214,26 +253,49 @@ namespace Datadog.Trace
         {
             switch (key)
             {
+                case Keys.TraceId:
                 case HttpHeaderNames.TraceId:
                     value = TraceId.ToString();
                     return true;
 
+                case Keys.ParentId:
                 case HttpHeaderNames.ParentId:
                     value = SpanId.ToString();
                     return true;
 
+                case Keys.SamplingPriority:
                 case HttpHeaderNames.SamplingPriority:
                     value = SamplingPriority?.ToString();
                     return true;
 
+                case Keys.Origin:
                 case HttpHeaderNames.Origin:
                     value = Origin;
+                    return true;
+
+                case Keys.RawTraceId:
+                    value = RawTraceId;
+                    return true;
+
+                case Keys.RawSpanId:
+                    value = RawSpanId;
                     return true;
 
                 default:
                     value = null;
                     return false;
             }
+        }
+
+        internal static class Keys
+        {
+            private const string Prefix = "__DistributedKey-";
+            public const string TraceId = $"{Prefix}TraceId";
+            public const string ParentId = $"{Prefix}ParentId";
+            public const string SamplingPriority = $"{Prefix}SamplingPriority";
+            public const string Origin = $"{Prefix}Origin";
+            public const string RawTraceId = $"{Prefix}RawTraceId";
+            public const string RawSpanId = $"{Prefix}RawSpanId";
         }
     }
 }

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Logging/Serilog/LogsInjection/LoggerDispatchInstrumentation.cs
@@ -57,11 +57,19 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Logging.Serilog.LogsInje
                 AddPropertyIfAbsent(dict, CorrelationIdentifier.SerilogVersionKey, tracer.Settings.ServiceVersion);
                 AddPropertyIfAbsent(dict, CorrelationIdentifier.SerilogEnvKey, tracer.Settings.Environment);
 
-                var span = tracer.ActiveScope?.Span;
-                if (span is not null)
+                var spanContext = tracer.DistributedSpanContext;
+                if (spanContext is not null)
                 {
-                    AddPropertyIfAbsent(dict, CorrelationIdentifier.TraceIdKey, span.TraceId.ToString());
-                    AddPropertyIfAbsent(dict, CorrelationIdentifier.SpanIdKey, span.SpanId.ToString());
+                    // For mismatch version support we need to keep requesting old keys.
+                    var hasTraceId = spanContext.TryGetValue(SpanContext.Keys.TraceId, out string traceId) ||
+                                     spanContext.TryGetValue("trace-id", out traceId);
+                    var hasSpanId = spanContext.TryGetValue(SpanContext.Keys.ParentId, out string spanId) ||
+                                    spanContext.TryGetValue("parent-id", out spanId);
+                    if (hasTraceId && hasSpanId)
+                    {
+                        AddPropertyIfAbsent(dict, CorrelationIdentifier.TraceIdKey, traceId);
+                        AddPropertyIfAbsent(dict, CorrelationIdentifier.SpanIdKey, spanId);
+                    }
                 }
             }
 

tracer/src/Datadog.Trace/SpanContext.cs
@@ -18,7 +18,19 @@ namespace Datadog.Trace
     /// </summary>
     public class SpanContext : ISpanContext, IReadOnlyDictionary<string, string>
     {
-        private static readonly string[] KeyNames = { "trace-id", "parent-id", "sampling-priority", "origin" };
+        private static readonly string[] KeyNames =
+        { 
+            Keys.TraceId,
+            Keys.ParentId,
+            Keys.SamplingPriority,
+            Keys.Origin,
+            Keys.RawTraceId,
+            Keys.RawSpanId,
+            "trace-id",
+            "parent-id",
+            "sampling-priority",
+            "origin" 
+        };
 
         /// <summary>
         /// An <see cref="ISpanContext"/> with default values. Can be used as the value for
@@ -75,6 +87,28 @@ namespace Datadog.Trace
             SpanId = spanId;
             SamplingPriority = samplingPriority;
             Origin = origin;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpanContext"/> class
+        /// from a propagated context. <see cref="Parent"/> will be null
+        /// since this is a root context locally.
+        /// </summary>
+        /// <param name="traceId">The propagated trace id.</param>
+        /// <param name="spanId">The propagated span id.</param>
+        /// <param name="samplingPriority">The propagated sampling priority.</param>
+        /// <param name="serviceName">The service name to propagate to child spans.</param>
+        /// <param name="origin">The propagated origin of the trace.</param>
+        /// <param name="rawTraceId">The raw propagated trace id</param>
+        /// <param name="rawSpanId">The raw propagated span id</param>
+        internal SpanContext(TraceId? traceId, ulong spanId, int? samplingPriority, string serviceName, string origin, string rawTraceId, string rawSpanId)
+            : this(traceId, serviceName)
+        {
+            SpanId = spanId;
+            SamplingPriority = samplingPriority;
+            Origin = origin;
+            RawTraceId = rawTraceId;
+            RawSpanId = rawSpanId;
         }
 
         /// <summary>
@@ -170,6 +204,16 @@ namespace Datadog.Trace
         /// </summary>
         internal int? SamplingPriority { get; }
 
+        /// <summary>
+        /// Gets the raw traceId (to support > 64bits)
+        /// </summary>
+        internal string RawTraceId { get; }
+
+        /// <summary>
+        /// Gets the raw spanId
+        /// </summary>
+        internal string RawSpanId { get; }
+
         /// <inheritdoc/>
         int IReadOnlyCollection<KeyValuePair<string, string>>.Count => KeyNames.Length;
 
@@ -239,29 +283,49 @@ namespace Datadog.Trace
         {
             switch (key)
             {
+                case Keys.TraceId:
                 case "trace-id":
                     value = TraceId.ToString();
                     return true;
 
+                case Keys.ParentId:
                 case "parent-id":
                     value = SpanId.ToString();
                     return true;
 
+                case Keys.SamplingPriority:
                 case "sampling-priority":
                     value = SamplingPriority?.ToString();
                     return true;
 
+                case Keys.Origin:
                 case "origin":
                     value = Origin;
+                    return true;
+
+                case Keys.RawTraceId:
+                    value = RawTraceId;
+                    return true;
+
+                case Keys.RawSpanId:
+                    value = RawSpanId;
                     return true;
 
                 default:
                     value = null;
                     return false;
             }
+        }
 
-            value = null;
-            return false;
+        internal static class Keys
+        {
+            private const string Prefix = "__DistributedKey-";
+            public const string TraceId = $"{Prefix}TraceId";
+            public const string ParentId = $"{Prefix}ParentId";
+            public const string SamplingPriority = $"{Prefix}SamplingPriority";
+            public const string Origin = $"{Prefix}Origin";
+            public const string RawTraceId = $"{Prefix}RawTraceId";
+            public const string RawSpanId = $"{Prefix}RawSpanId";
         }
     }
 }