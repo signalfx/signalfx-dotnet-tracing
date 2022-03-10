DataDog/dd-trace-dotnet

[Cleanup] Move shared classes/files to shared and ensure Profiler, Tracer and Native Loader use them (#2493)
* Move spdlog, fmt libraries/include files from Tracer lib folder to shared
* Move coreclr include files from Tracer lib folder to shared
* Delete lib folder from Tracer
* Delete dotnet-runtime-coreclr from Profiler
* Make Tracer use the WSTRING from shared instead of its own string.
* Make sure Profiler, Tracer and Native Loader rely on coreclr, spdlog and fmt from shared.
* Make Profiler, Tracer and Native Loader use the the same native underlying mechanism (logger_impl.h)
* Delete duplicated files

tracer/src/Datadog.AutoInstrumentation.NativeLoader/util.h
@@ -10,13 +10,13 @@ EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 #include <dlfcn.h>
 #endif
 
-#include "pal.h"
-#include "string_utils.h"
+#include "../../../shared/src/native-src/pal.h"
+#include "../../../shared/src/native-src/string.h"
 
 using namespace datadog::shared::nativeloader;
 
 const std::string conf_filename = "loader.conf";
-const WSTRING cfg_filepath_env = WStr("DD_NATIVELOADER_CONFIGFILE");
+const ::shared::WSTRING cfg_filepath_env = WStr("DD_NATIVELOADER_CONFIGFILE");
 
 static std::filesystem::path GetCurrentModuleFolderPath()
 {
@@ -39,7 +39,7 @@ static std::filesystem::path GetCurrentModuleFolderPath()
 // Gets the configuration file path
 static std::filesystem::path GetConfigurationFilePath()
 {
-    std::filesystem::path env_configfile = GetEnvironmentValue(cfg_filepath_env);
+    std::filesystem::path env_configfile = shared::GetEnvironmentValue(cfg_filepath_env);
     if (!env_configfile.empty())
     {
         return env_configfile;

tracer/src/Datadog.Trace.ClrProfiler.Native/dd_profiler_constants.h
@@ -9,7 +9,7 @@
 namespace trace
 {
 
-const WSTRING env_vars_to_display[]{environment::tracing_enabled,
+const shared::WSTRING env_vars_to_display[]{environment::tracing_enabled,
                                     environment::debug_enabled,
                                     environment::profiler_home_path,
                                     environment::include_process_names,
@@ -30,7 +30,7 @@ const WSTRING env_vars_to_display[]{environment::tracing_enabled,
                                     environment::azure_app_services_app_pool_id,
                                     environment::azure_app_services_cli_telemetry_profile_value};
 
-const WSTRING skip_assembly_prefixes[]{
+const shared::WSTRING skip_assembly_prefixes[]{
     WStr("Microsoft.AI"),
     WStr("Microsoft.ApplicationInsights"),
     WStr("Microsoft.Build"),
@@ -66,7 +66,7 @@ const WSTRING skip_assembly_prefixes[]{
     WStr("System.Xml"),
 };
 
-const WSTRING skip_assemblies[]{WStr("mscorlib"),
+const shared::WSTRING skip_assemblies[]{WStr("mscorlib"),
                                 WStr("netstandard"),
                                 WStr("System.Configuration"),
                                 WStr("Microsoft.AspNetCore.Razor.Language"),
@@ -75,29 +75,29 @@ const WSTRING skip_assemblies[]{WStr("mscorlib"),
                                 WStr("Datadog.AutoInstrumentation.ManagedLoader"),
                                 WStr("ISymWrapper")};
 
-const WSTRING mscorlib_assemblyName = WStr("mscorlib");
-const WSTRING system_private_corelib_assemblyName = WStr("System.Private.CoreLib");
-const WSTRING datadog_trace_clrprofiler_managed_loader_assemblyName = WStr("Datadog.Trace.ClrProfiler.Managed.Loader");
+const shared::WSTRING mscorlib_assemblyName = WStr("mscorlib");
+const shared::WSTRING system_private_corelib_assemblyName = WStr("System.Private.CoreLib");
+const shared::WSTRING datadog_trace_clrprofiler_managed_loader_assemblyName = WStr("Datadog.Trace.ClrProfiler.Managed.Loader");
 
-const WSTRING managed_profiler_full_assembly_version =
+const shared::WSTRING managed_profiler_full_assembly_version =
     WStr("Datadog.Trace, Version=2.5.0.0, Culture=neutral, PublicKeyToken=def86d061d0d2eeb");
 
-const WSTRING managed_profiler_name = WStr("Datadog.Trace");
+const shared::WSTRING managed_profiler_name = WStr("Datadog.Trace");
 
-const WSTRING nonwindows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+NonWindows");
-const WSTRING windows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+Windows");
+const shared::WSTRING nonwindows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+NonWindows");
+const shared::WSTRING windows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+Windows");
 
-const WSTRING appsec_nonwindows_nativemethods_type = WStr("Datadog.Trace.AppSec.Waf.NativeBindings.NativeLibrary+NonWindows");
-const WSTRING appsec_windows_nativemethods_type = WStr("Datadog.Trace.AppSec.Waf.NativeBindings.NativeLibrary+Windows");
+const shared::WSTRING appsec_nonwindows_nativemethods_type = WStr("Datadog.Trace.AppSec.Waf.NativeBindings.NativeLibrary+NonWindows");
+const shared::WSTRING appsec_windows_nativemethods_type = WStr("Datadog.Trace.AppSec.Waf.NativeBindings.NativeLibrary+Windows");
 
-const WSTRING calltarget_modification_action = WStr("CallTargetModification");
+const shared::WSTRING calltarget_modification_action = WStr("CallTargetModification");
 
 #ifdef _WIN32
-const WSTRING native_dll_filename = WStr("DATADOG.TRACE.CLRPROFILER.NATIVE.DLL");
+const shared::WSTRING native_dll_filename = WStr("DATADOG.TRACE.CLRPROFILER.NATIVE.DLL");
 #elif MACOS
-const WSTRING native_dll_filename = WStr("Datadog.Trace.ClrProfiler.Native.dylib");
+const shared::WSTRING native_dll_filename = WStr("Datadog.Trace.ClrProfiler.Native.dylib");
 #else
-const WSTRING native_dll_filename = WStr("Datadog.Trace.ClrProfiler.Native.so");
+const shared::WSTRING native_dll_filename = WStr("Datadog.Trace.ClrProfiler.Native.so");
 #endif
 
 const AssemblyProperty managed_profiler_assembly_property = AssemblyProperty(

tracer/src/Datadog.Trace.ClrProfiler.Native/logger.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "environment_variables.h"
-#include "string.h"
-#include "logger_impl.h"
+#include "../../../shared/src/native-src/string.h"
+#include "../../../shared/src/native-src/logger_impl.h"
 
 #include <string>
 
@@ -14,15 +14,14 @@ struct TracerLoggerPolicy
 {
     inline static const std::string file_name = "dotnet-tracer-native";
 #ifdef _WIN32
-    // this field will be removed once merged with the profiler in order to have
-    // the same product folder name
-    inline static const WSTRING folder_path = WStr(R"(Datadog .NET Tracer\logs)");
+    inline static const shared::WSTRING folder_path = WStr(R"(Datadog .NET Tracer\logs)");
 #endif
     inline static const std::string pattern = "%D %I:%M:%S.%e %p [%P|%t] [%l] %v";
     struct logging_environment
     {
         // cannot reuse environment::log_path variable. On alpine, test fails
-        inline static const WSTRING log_path = WStr("DD_TRACE_LOG_PATH");
+        inline static const shared::WSTRING log_path = WStr("DD_TRACE_LOG_PATH");
+        inline static const shared::WSTRING log_directory = WStr("DD_TRACE_LOG_DIRECTORY");
     };
 };
 
@@ -39,49 +38,49 @@ public:
     template <typename... Args>
     static void Debug(const Args&... args)
     {
-        LoggerImpl<TracerLoggerPolicy>::Instance()->Debug(args...);
+        shared::LoggerImpl<TracerLoggerPolicy>::Instance()->Debug(args...);
     }
 
     template <typename... Args>
     static void Info(const Args&... args)
     {
-        LoggerImpl<TracerLoggerPolicy>::Instance()->Info(args...);
+        shared::LoggerImpl<TracerLoggerPolicy>::Instance()->Info(args...);
     }
 
     template <typename... Args>
     static void Warn(const Args&... args)
     {
-        LoggerImpl<TracerLoggerPolicy>::Instance()->Warn(args...);
+        shared::LoggerImpl<TracerLoggerPolicy>::Instance()->Warn(args...);
     }
     template <typename... Args>
     static void Error(const Args&... args)
     {
-        LoggerImpl<TracerLoggerPolicy>::Instance()->Error(args...);
+        shared::LoggerImpl<TracerLoggerPolicy>::Instance()->Error(args...);
     }
     template <typename... Args>
     static void Critical(const Args&... args)
     {
-        LoggerImpl<TracerLoggerPolicy>::Instance()->Critical(args...);
+        shared::LoggerImpl<TracerLoggerPolicy>::Instance()->Critical(args...);
     }
 
     static void EnableDebug()
     {
-        LoggerImpl<TracerLoggerPolicy>::Instance()->EnableDebug();
+        shared::LoggerImpl<TracerLoggerPolicy>::Instance()->EnableDebug();
     }
 
     static bool IsDebugEnabled()
     {
-        return LoggerImpl<TracerLoggerPolicy>::Instance()->IsDebugEnabled();
+        return shared::LoggerImpl<TracerLoggerPolicy>::Instance()->IsDebugEnabled();
     }
 
     static void Shutdown()
     {
-        LoggerImpl<TracerLoggerPolicy>::Shutdown();
+        shared::LoggerImpl<TracerLoggerPolicy>::Shutdown();
     }
 
     static void Flush()
     {
-        LoggerImpl<TracerLoggerPolicy>::Instance()->Flush();
+        shared::LoggerImpl<TracerLoggerPolicy>::Instance()->Flush();
     }
 };
 

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.AutoInstrumentation.NativeLoader/util.h
@@ -10,13 +10,13 @@ EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 #include <dlfcn.h>
 #endif
 
-#include "pal.h"
-#include "string_utils.h"
+#include "../../../shared/src/native-src/pal.h"
+#include "../../../shared/src/native-src/string.h"
 
 using namespace datadog::shared::nativeloader;
 
 const std::string conf_filename = "loader.conf";
-const WSTRING cfg_filepath_env = WStr("SIGNALFX_NATIVELOADER_CONFIGFILE");
+const ::shared::WSTRING cfg_filepath_env = WStr("SIGNALFX_NATIVELOADER_CONFIGFILE");
 
 static std::filesystem::path GetCurrentModuleFolderPath()
 {
@@ -39,7 +39,7 @@ static std::filesystem::path GetCurrentModuleFolderPath()
 // Gets the configuration file path
 static std::filesystem::path GetConfigurationFilePath()
 {
-    std::filesystem::path env_configfile = GetEnvironmentValue(cfg_filepath_env);
+    std::filesystem::path env_configfile = shared::GetEnvironmentValue(cfg_filepath_env);
     if (!env_configfile.empty())
     {
         return env_configfile;

tracer/src/Datadog.Trace.ClrProfiler.Native/dd_profiler_constants.h
@@ -9,7 +9,7 @@
 namespace trace
 {
 
-const WSTRING env_vars_to_display[]{environment::tracing_enabled,
+const shared::WSTRING env_vars_to_display[]{environment::tracing_enabled,
                                     environment::debug_enabled,
                                     environment::profiler_home_path,
                                     environment::include_process_names,
@@ -30,7 +30,7 @@ const WSTRING env_vars_to_display[]{environment::tracing_enabled,
                                     environment::azure_app_services_app_pool_id,
                                     environment::azure_app_services_cli_telemetry_profile_value};
 
-const WSTRING skip_assembly_prefixes[]{
+const shared::WSTRING skip_assembly_prefixes[]{
     WStr("Microsoft.AI"),
     WStr("Microsoft.ApplicationInsights"),
     WStr("Microsoft.Build"),
@@ -66,7 +66,7 @@ const WSTRING skip_assembly_prefixes[]{
     WStr("System.Xml"),
 };
 
-const WSTRING skip_assemblies[]{WStr("mscorlib"),
+const shared::WSTRING skip_assemblies[]{WStr("mscorlib"),
                                 WStr("netstandard"),
                                 WStr("System.Configuration"),
                                 WStr("Microsoft.AspNetCore.Razor.Language"),
@@ -75,26 +75,29 @@ const WSTRING skip_assemblies[]{WStr("mscorlib"),
                                 WStr("Datadog.AutoInstrumentation.ManagedLoader"),
                                 WStr("ISymWrapper")};
 
-const WSTRING mscorlib_assemblyName = WStr("mscorlib");
-const WSTRING system_private_corelib_assemblyName = WStr("System.Private.CoreLib");
-const WSTRING datadog_trace_clrprofiler_managed_loader_assemblyName = WStr("SignalFx.Tracing.ClrProfiler.Managed.Loader");
+const shared::WSTRING mscorlib_assemblyName = WStr("mscorlib");
+const shared::WSTRING system_private_corelib_assemblyName = WStr("System.Private.CoreLib");
+const shared::WSTRING datadog_trace_clrprofiler_managed_loader_assemblyName = WStr("SignalFx.Tracing.ClrProfiler.Managed.Loader");
 
-const WSTRING managed_profiler_full_assembly_version =
-    WStr("SignalFx.Tracing, Version=0.2.2.0, Culture=neutral, PublicKeyToken=e43a27c2023d388a");
+const shared::WSTRING managed_profiler_full_assembly_version =
+    WStr("SignalFx.Tracing, Version=0.2.2.0, Culture=neutral, PublicKeyToken=def86d061d0d2eeb");
 
-const WSTRING managed_profiler_name = WStr("SignalFx.Tracing");
+const shared::WSTRING managed_profiler_name = WStr("SignalFx.Tracing");
 
-const WSTRING nonwindows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+NonWindows");
-const WSTRING windows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+Windows");
+const shared::WSTRING nonwindows_nativemethods_type = WStr("SignalFx.Tracing.ClrProfiler.NativeMethods+NonWindows");
+const shared::WSTRING windows_nativemethods_type = WStr("SignalFx.Tracing.ClrProfiler.NativeMethods+Windows");
 
-const WSTRING calltarget_modification_action = WStr("CallTargetModification");
+const shared::WSTRING appsec_nonwindows_nativemethods_type = WStr("SignalFx.Tracing.AppSec.Waf.NativeBindings.NativeLibrary+NonWindows");
+const shared::WSTRING appsec_windows_nativemethods_type = WStr("SignalFx.Tracing.AppSec.Waf.NativeBindings.NativeLibrary+Windows");
+
+const shared::WSTRING calltarget_modification_action = WStr("CallTargetModification");
 
 #ifdef _WIN32
-const WSTRING native_dll_filename = WStr("SIGNALFX.TRACING.CLRPROFILER.NATIVE.DLL");
+const shared::WSTRING native_dll_filename = WStr("SIGNALFX.TRACING.CLRPROFILER.NATIVE.DLL");
 #elif MACOS
-const WSTRING native_dll_filename = WStr("SignalFx.Tracing.ClrProfiler.Native.dylib");
+const shared::WSTRING native_dll_filename = WStr("SignalFx.Tracing.ClrProfiler.Native.dylib");
 #else
-const WSTRING native_dll_filename = WStr("SignalFx.Tracing.ClrProfiler.Native.so");
+const shared::WSTRING native_dll_filename = WStr("SignalFx.Tracing.ClrProfiler.Native.so");
 #endif
 
 const AssemblyProperty managed_profiler_assembly_property = AssemblyProperty(

tracer/src/Datadog.Trace.ClrProfiler.Native/logger.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "environment_variables.h"
-#include "string.h"
-#include "logger_impl.h"
+#include "../../../shared/src/native-src/string.h"
+#include "../../../shared/src/native-src/logger_impl.h"
 
 #include <string>
 
@@ -16,13 +16,14 @@ struct TracerLoggerPolicy
 #ifdef _WIN32
     // this field will be removed once merged with the profiler in order to have
     // the same product folder name
-    inline static const WSTRING folder_path = WStr(R"(SignalFx .NET Tracing\logs)");
+    inline static const shared::WSTRING folder_path = WStr(R"(SignalFx .NET Tracing\logs)");
 #endif
     inline static const std::string pattern = "%D %I:%M:%S.%e %p [%P|%t] [%l] %v";
     struct logging_environment
     {
         // cannot reuse environment::log_path variable. On alpine, test fails
-        inline static const WSTRING log_path = WStr("SIGNALFX_TRACE_LOG_PATH");
+        inline static const shared::WSTRING log_path = WStr("SIGNALFX_TRACE_LOG_PATH");
+        inline static const shared::WSTRING log_directory = WStr("SIGNALFX_TRACE_LOG_DIRECTORY");
     };
 };
 
@@ -39,49 +40,49 @@ public:
     template <typename... Args>
     static void Debug(const Args&... args)
     {
-        LoggerImpl<TracerLoggerPolicy>::Instance()->Debug(args...);
+        shared::LoggerImpl<TracerLoggerPolicy>::Instance()->Debug(args...);
     }
 
     template <typename... Args>
     static void Info(const Args&... args)
     {
-        LoggerImpl<TracerLoggerPolicy>::Instance()->Info(args...);
+        shared::LoggerImpl<TracerLoggerPolicy>::Instance()->Info(args...);
     }
 
     template <typename... Args>
     static void Warn(const Args&... args)
     {
-        LoggerImpl<TracerLoggerPolicy>::Instance()->Warn(args...);
+        shared::LoggerImpl<TracerLoggerPolicy>::Instance()->Warn(args...);
     }
     template <typename... Args>
     static void Error(const Args&... args)
     {
-        LoggerImpl<TracerLoggerPolicy>::Instance()->Error(args...);
+        shared::LoggerImpl<TracerLoggerPolicy>::Instance()->Error(args...);
     }
     template <typename... Args>
     static void Critical(const Args&... args)
     {
-        LoggerImpl<TracerLoggerPolicy>::Instance()->Critical(args...);
+        shared::LoggerImpl<TracerLoggerPolicy>::Instance()->Critical(args...);
     }
 
     static void EnableDebug()
     {
-        LoggerImpl<TracerLoggerPolicy>::Instance()->EnableDebug();
+        shared::LoggerImpl<TracerLoggerPolicy>::Instance()->EnableDebug();
     }
 
     static bool IsDebugEnabled()
     {
-        return LoggerImpl<TracerLoggerPolicy>::Instance()->IsDebugEnabled();
+        return shared::LoggerImpl<TracerLoggerPolicy>::Instance()->IsDebugEnabled();
     }
 
     static void Shutdown()
     {
-        LoggerImpl<TracerLoggerPolicy>::Shutdown();
+        shared::LoggerImpl<TracerLoggerPolicy>::Shutdown();
     }
 
     static void Flush()
     {
-        LoggerImpl<TracerLoggerPolicy>::Instance()->Flush();
+        shared::LoggerImpl<TracerLoggerPolicy>::Instance()->Flush();
     }
 };
 