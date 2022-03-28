DataDog/dd-trace-dotnet

[CIApp] - Add support for XUnit LogsDirectSubmission (#2531)
* - Add support for XUnit Integration in DirectLogSubmission
- Add Flush support in DirectLogSubmission
- Add CI Visibility configuration settings.

* Add TestOutputHelper.QueueTestOutput integration.

* change comments.

* Remove other loggers than XUnit.

* Update tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Testing/XUnit/XUnitTestOutputHelperQueueTestOutputIntegration.cs

Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

* Update tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Testing/XUnit/XUnitTestOutputHelperQueueTestOutputIntegration.cs

Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

* Checks and changes.

* Improve flush.

* Fix build issues.

* Add _dd.origin tag to the logs.

* Add Tests.

* fix test suite

* Fix tests and mock

Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Testing/XUnit/XUnitTestOutputHelperQueueTestOutputIntegration.cs
@@ -1,0 +1,110 @@
+// <copyright file="XUnitTestOutputHelperQueueTestOutputIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.ComponentModel;
+using System.Text;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.Logging.DirectSubmission;
+using Datadog.Trace.Logging.DirectSubmission.Formatting;
+using Datadog.Trace.Logging.DirectSubmission.Sink;
+using Datadog.Trace.Vendors.Newtonsoft.Json;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Testing.XUnit
+{
+    /// <summary>
+    /// Xunit.Sdk.TestOutputHelper.QueueTestOutput calltarget instrumentation
+    /// </summary>
+    [InstrumentMethod(
+        AssemblyNames = new[] { "xunit.execution.dotnet", "xunit.execution.desktop" },
+        TypeName = "Xunit.Sdk.TestOutputHelper",
+        MethodName = "QueueTestOutput",
+        ReturnTypeName = ClrNames.Void,
+        ParameterTypeNames = new[] { ClrNames.String },
+        MinimumVersion = "2.2.0",
+        MaximumVersion = "2.*.*",
+        IntegrationName = XUnitIntegration.IntegrationName)]
+    [Browsable(false)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class XUnitTestOutputHelperQueueTestOutputIntegration
+    {
+        /// <summary>
+        /// OnMethodBegin callback
+        /// </summary>
+        /// <typeparam name="TTarget">Type of the target</typeparam>
+        /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
+        /// <param name="output">Output string</param>
+        /// <returns>Calltarget state value</returns>
+        internal static CallTargetState OnMethodBegin<TTarget>(TTarget instance, string output)
+        {
+            if (!XUnitIntegration.IsEnabled)
+            {
+                return CallTargetState.GetDefault();
+            }
+
+            var tracer = Tracer.Instance;
+            if (tracer.TracerManager.DirectLogSubmission.Settings.MinimumLevel < DirectSubmissionLogLevel.Information)
+            {
+                return CallTargetState.GetDefault();
+            }
+
+            var span = tracer.ActiveScope?.Span as Span;
+            tracer.TracerManager.DirectLogSubmission.Sink.EnqueueLog(new XUnitLogEvent(output, span));
+            return CallTargetState.GetDefault();
+        }
+
+        private class XUnitLogEvent : DatadogLogEvent
+        {
+            private readonly string _message;
+            private readonly Context? _context;
+
+            public XUnitLogEvent(string message, Span span)
+            {
+                _message = message;
+                _context = span is null ? null : new Context(span.TraceId, span.SpanId, span.Context.Origin);
+            }
+
+            public override void Format(StringBuilder sb, LogFormatter formatter)
+            {
+                formatter.FormatLog<Context?>(
+                    sb,
+                    _context,
+                    DateTime.UtcNow,
+                    _message,
+                    eventId: null,
+                    logLevel: DirectSubmissionLogLevelExtensions.Information,
+                    exception: null,
+                    (JsonTextWriter writer, in Context? state) =>
+                    {
+                        if (state.HasValue)
+                        {
+                            writer.WritePropertyName("dd.trace_id");
+                            writer.WriteValue($"{state.Value.TraceId}");
+                            writer.WritePropertyName("dd.span_id");
+                            writer.WriteValue($"{state.Value.SpanId}");
+                            writer.WritePropertyName("_dd.origin");
+                            writer.WriteValue($"{state.Value.Origin}");
+                        }
+
+                        return default;
+                    });
+            }
+
+            private readonly struct Context
+            {
+                public readonly ulong TraceId;
+                public readonly ulong SpanId;
+                public readonly string Origin;
+
+                public Context(ulong traceId, ulong spanId, string origin)
+                {
+                    TraceId = traceId;
+                    SpanId = spanId;
+                    Origin = origin;
+                }
+            }
+        }
+    }
+}

tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -323,6 +323,12 @@ namespace Datadog.Trace.Configuration
             /// Default is value is false (disabled).
             /// </summary>
             public const string AgentlessEnabled = "DD_CIVISIBILITY_AGENTLESS_ENABLED";
+
+            /// <summary>
+            /// Configuration key for enabling or disabling Logs direct submission.
+            /// Default is value is false (disabled).
+            /// </summary>
+            public const string Logs = "DD_CIVISIBILITY_LOGS_ENABLED";
         }
 
         /// <summary>

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Testing/XUnit/XUnitTestOutputHelperQueueTestOutputIntegration.cs
@@ -1,0 +1,112 @@
+// <copyright file="XUnitTestOutputHelperQueueTestOutputIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+// Modified by Splunk Inc.
+
+using System;
+using System.ComponentModel;
+using System.Text;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.Logging.DirectSubmission;
+using Datadog.Trace.Logging.DirectSubmission.Formatting;
+using Datadog.Trace.Logging.DirectSubmission.Sink;
+using Datadog.Trace.Vendors.Newtonsoft.Json;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Testing.XUnit
+{
+    /// <summary>
+    /// Xunit.Sdk.TestOutputHelper.QueueTestOutput calltarget instrumentation
+    /// </summary>
+    [InstrumentMethod(
+        AssemblyNames = new[] { "xunit.execution.dotnet", "xunit.execution.desktop" },
+        TypeName = "Xunit.Sdk.TestOutputHelper",
+        MethodName = "QueueTestOutput",
+        ReturnTypeName = ClrNames.Void,
+        ParameterTypeNames = new[] { ClrNames.String },
+        MinimumVersion = "2.2.0",
+        MaximumVersion = "2.*.*",
+        IntegrationName = XUnitIntegration.IntegrationName)]
+    [Browsable(false)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class XUnitTestOutputHelperQueueTestOutputIntegration
+    {
+        /// <summary>
+        /// OnMethodBegin callback
+        /// </summary>
+        /// <typeparam name="TTarget">Type of the target</typeparam>
+        /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
+        /// <param name="output">Output string</param>
+        /// <returns>Calltarget state value</returns>
+        internal static CallTargetState OnMethodBegin<TTarget>(TTarget instance, string output)
+        {
+            if (!XUnitIntegration.IsEnabled)
+            {
+                return CallTargetState.GetDefault();
+            }
+
+            var tracer = Tracer.Instance;
+            if (tracer.TracerManager.DirectLogSubmission.Settings.MinimumLevel < DirectSubmissionLogLevel.Information)
+            {
+                return CallTargetState.GetDefault();
+            }
+
+            var span = tracer.ActiveScope?.Span as Span;
+            tracer.TracerManager.DirectLogSubmission.Sink.EnqueueLog(new XUnitLogEvent(output, span));
+            return CallTargetState.GetDefault();
+        }
+
+        private class XUnitLogEvent : DatadogLogEvent
+        {
+            private readonly string _message;
+            private readonly Context? _context;
+
+            public XUnitLogEvent(string message, Span span)
+            {
+                _message = message;
+                _context = span is null ? null : new Context(span.TraceId, span.SpanId, span.Context.Origin);
+            }
+
+            public override void Format(StringBuilder sb, LogFormatter formatter)
+            {
+                formatter.FormatLog<Context?>(
+                    sb,
+                    _context,
+                    DateTime.UtcNow,
+                    _message,
+                    eventId: null,
+                    logLevel: DirectSubmissionLogLevelExtensions.Information,
+                    exception: null,
+                    (JsonTextWriter writer, in Context? state) =>
+                    {
+                        if (state.HasValue)
+                        {
+                            writer.WritePropertyName("dd.trace_id");
+                            writer.WriteValue($"{state.Value.TraceId}");
+                            writer.WritePropertyName("dd.span_id");
+                            writer.WriteValue($"{state.Value.SpanId}");
+                            writer.WritePropertyName("_dd.origin");
+                            writer.WriteValue($"{state.Value.Origin}");
+                        }
+
+                        return default;
+                    });
+            }
+
+            private readonly struct Context
+            {
+                public readonly TraceId TraceId;
+                public readonly ulong SpanId;
+                public readonly string Origin;
+
+                public Context(TraceId traceId, ulong spanId, string origin)
+                {
+                    TraceId = traceId;
+                    SpanId = spanId;
+                    Origin = origin;
+                }
+            }
+        }
+    }
+}

tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -392,6 +392,12 @@ namespace Datadog.Trace.Configuration
             /// Default is value is false (disabled).
             /// </summary>
             public const string AgentlessEnabled = "SIGNALFX_CIVISIBILITY_AGENTLESS_ENABLED";
+
+            /// <summary>
+            /// Configuration key for enabling or disabling Logs direct submission.
+            /// Default is value is false (disabled).
+            /// </summary>
+            public const string Logs = "SIGNALFX_CIVISIBILITY_LOGS_ENABLED";
         }
 
         /// <summary>