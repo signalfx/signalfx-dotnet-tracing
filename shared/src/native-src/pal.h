DataDog/dd-trace-dotnet

[Cleanup] Move shared classes/files to shared and ensure Profiler, Tracer and Native Loader use them (#2493)
* Move spdlog, fmt libraries/include files from Tracer lib folder to shared
* Move coreclr include files from Tracer lib folder to shared
* Delete lib folder from Tracer
* Delete dotnet-runtime-coreclr from Profiler
* Make Tracer use the WSTRING from shared instead of its own string.
* Make sure Profiler, Tracer and Native Loader rely on coreclr, spdlog and fmt from shared.
* Make Profiler, Tracer and Native Loader use the the same native underlying mechanism (logger_impl.h)
* Delete duplicated files

shared/src/native-src/pal.h
@@ -1,16 +1,16 @@
-#ifndef DD_CLR_PROFILER_PAL_H_
-#define DD_CLR_PROFILER_PAL_H_
+#pragma once
 
 #ifdef _WIN32
 
-#include <process.h>
-#include <filesystem>
 #include "windows.h"
+#include <filesystem>
+#include <process.h>
 
 #else
 
-#include <unistd.h>
+#include <dlfcn.h>
 #include <fstream>
+#include <unistd.h>
 
 #endif
 
@@ -18,80 +18,117 @@
 #include <libproc.h>
 #endif
 
-#include "environment_variables.h"
-#include "string.h"  // NOLINT
-#include "util.h"
+#include "../../../shared/src/native-src/string.h" // NOLINT
+#include "../../../shared/src/native-src/util.h"
 
-namespace shared {
+namespace shared
+{
 
-    inline WSTRING DatadogLogFilePath() {
-        WSTRING directory = GetEnvironmentValue(environment::log_directory);
+template <class TLoggerPolicy>
+inline shared::WSTRING GetDatadogLogFilePath(const std::string& file_name_suffix)
+{
+    const auto file_name = TLoggerPolicy::file_name + file_name_suffix + ".log";
 
-        if (directory.length() > 0) {
-            return directory +
+    WSTRING directory = GetEnvironmentValue(TLoggerPolicy::logging_environment::log_directory);
+
+    if (directory.length() > 0)
+    {
+        return directory +
 #ifdef _WIN32
-                WStr('\\') +
+               WStr('\\') +
 #else
-                WStr('/') +
+               WStr('/') +
 #endif
-                WStr("dotnet-tracer-native.log");
-        }
-
-        WSTRING path = GetEnvironmentValue(environment::log_path);
-
-        if (path.length() > 0) {
-            return path;
-        }
-
-#ifdef _WIN32
-        char* p_program_data;
-        size_t length;
-        const errno_t result = _dupenv_s(&p_program_data, &length, "PROGRAMDATA");
-        std::string program_data;
-
-        if (SUCCEEDED(result) && p_program_data != nullptr && length > 0) {
-            program_data = std::string(p_program_data);
-        }
-        else {
-            program_data = R"(C:\ProgramData)";
-        }
-
-        return ToWSTRING(program_data +
-            R"(\Datadog .NET Tracer\logs\dotnet-tracer-native.log)");
-#else
-        return WStr("/var/log/datadog/dotnet/dotnet-tracer-native.log");
-#endif
+               ToWSTRING(file_name);
     }
 
-    inline WSTRING GetCurrentProcessName() {
-#ifdef _WIN32
-        const DWORD length = 260;
-        WCHAR buffer[length]{};
+    WSTRING path = GetEnvironmentValue(TLoggerPolicy::logging_environment::log_path);
 
-        const DWORD len = GetModuleFileName(nullptr, buffer, length);
-        const WSTRING current_process_path(buffer);
-        return std::filesystem::path(current_process_path).filename();
+    if (path.length() > 0)
+    {
+        return path;
+    }
+
+#ifdef _WIN32
+    std::filesystem::path program_data_path;
+    program_data_path = GetEnvironmentValue(WStr("PROGRAMDATA"));
+
+    if (program_data_path.empty())
+    {
+        program_data_path = WStr(R"(C:\ProgramData)");
+    }
+
+    // on Windows WSTRING == wstring
+    return (program_data_path / TLoggerPolicy::folder_path / file_name).wstring();
+#else
+    return ToWSTRING("/var/log/datadog/dotnet/" + file_name);
+#endif
+}
+
+inline WSTRING GetCurrentProcessName()
+{
+#ifdef _WIN32
+    const DWORD length = 260;
+    WCHAR buffer[length]{};
+
+    const DWORD len = GetModuleFileName(nullptr, buffer, length);
+    const WSTRING current_process_path(buffer);
+    return std::filesystem::path(current_process_path).filename();
 #elif MACOS
-        const int length = 260;
-        char* buffer = new char[length];
-        proc_name(getpid(), buffer, length);
-        return ToWSTRING(std::string(buffer));
+    const int length = 260;
+    char* buffer = new char[length];
+    proc_name(getpid(), buffer, length);
+    return ToWSTRING(std::string(buffer));
 #else
-        std::fstream comm("/proc/self/comm");
-        std::string name;
-        std::getline(comm, name);
-        return ToWSTRING(name);
+    std::fstream comm("/proc/self/comm");
+    std::string name;
+    std::getline(comm, name);
+    return ToWSTRING(name);
 #endif
-    }
+}
 
-    inline int GetPID() {
+inline int GetPID()
+{
 #ifdef _WIN32
-        return _getpid();
+    return _getpid();
 #else
-        return getpid();
+    return getpid();
 #endif
+}
+
+inline WSTRING GetCurrentModuleFileName()
+{
+    static WSTRING moduleFileName = EmptyWStr;
+    if (moduleFileName != EmptyWStr)
+    {
+        // use cached version
+        return moduleFileName;
     }
 
-} // namespace trace
+#ifdef _WIN32
+    HMODULE hModule;
+    if (GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT | GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
+                           (LPCTSTR) GetCurrentModuleFileName, &hModule))
+    {
+        WCHAR lpFileName[1024];
+        DWORD lpFileNameLength = GetModuleFileNameW(hModule, lpFileName, 1024);
+        if (lpFileNameLength > 0)
+        {
+            moduleFileName = WSTRING(lpFileName, lpFileNameLength);
+            return moduleFileName;
+        }
+    }
+#else
+    Dl_info info;
+    if (dladdr((void*) GetCurrentModuleFileName, &info))
+    {
+        moduleFileName = ToWSTRING(ToString(info.dli_fname));
+        return moduleFileName;
+    }
+#endif
 
-#endif  // DD_CLR_PROFILER_PAL_H_
+    return EmptyWStr;
+}
+
+} // namespace shared
+

tracer/src/Datadog.Trace.ClrProfiler.Native/dd_profiler_constants.h
@@ -9,7 +9,7 @@
 namespace trace
 {
 
-const WSTRING env_vars_to_display[]{environment::tracing_enabled,
+const shared::WSTRING env_vars_to_display[]{environment::tracing_enabled,
                                     environment::debug_enabled,
                                     environment::profiler_home_path,
                                     environment::include_process_names,
@@ -30,7 +30,7 @@ const WSTRING env_vars_to_display[]{environment::tracing_enabled,
                                     environment::azure_app_services_app_pool_id,
                                     environment::azure_app_services_cli_telemetry_profile_value};
 
-const WSTRING skip_assembly_prefixes[]{
+const shared::WSTRING skip_assembly_prefixes[]{
     WStr("Microsoft.AI"),
     WStr("Microsoft.ApplicationInsights"),
     WStr("Microsoft.Build"),
@@ -66,7 +66,7 @@ const WSTRING skip_assembly_prefixes[]{
     WStr("System.Xml"),
 };
 
-const WSTRING skip_assemblies[]{WStr("mscorlib"),
+const shared::WSTRING skip_assemblies[]{WStr("mscorlib"),
                                 WStr("netstandard"),
                                 WStr("System.Configuration"),
                                 WStr("Microsoft.AspNetCore.Razor.Language"),
@@ -75,29 +75,29 @@ const WSTRING skip_assemblies[]{WStr("mscorlib"),
                                 WStr("Datadog.AutoInstrumentation.ManagedLoader"),
                                 WStr("ISymWrapper")};
 
-const WSTRING mscorlib_assemblyName = WStr("mscorlib");
-const WSTRING system_private_corelib_assemblyName = WStr("System.Private.CoreLib");
-const WSTRING datadog_trace_clrprofiler_managed_loader_assemblyName = WStr("Datadog.Trace.ClrProfiler.Managed.Loader");
+const shared::WSTRING mscorlib_assemblyName = WStr("mscorlib");
+const shared::WSTRING system_private_corelib_assemblyName = WStr("System.Private.CoreLib");
+const shared::WSTRING datadog_trace_clrprofiler_managed_loader_assemblyName = WStr("Datadog.Trace.ClrProfiler.Managed.Loader");
 
-const WSTRING managed_profiler_full_assembly_version =
+const shared::WSTRING managed_profiler_full_assembly_version =
     WStr("Datadog.Trace, Version=2.5.0.0, Culture=neutral, PublicKeyToken=def86d061d0d2eeb");
 
-const WSTRING managed_profiler_name = WStr("Datadog.Trace");
+const shared::WSTRING managed_profiler_name = WStr("Datadog.Trace");
 
-const WSTRING nonwindows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+NonWindows");
-const WSTRING windows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+Windows");
+const shared::WSTRING nonwindows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+NonWindows");
+const shared::WSTRING windows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+Windows");
 
-const WSTRING appsec_nonwindows_nativemethods_type = WStr("Datadog.Trace.AppSec.Waf.NativeBindings.NativeLibrary+NonWindows");
-const WSTRING appsec_windows_nativemethods_type = WStr("Datadog.Trace.AppSec.Waf.NativeBindings.NativeLibrary+Windows");
+const shared::WSTRING appsec_nonwindows_nativemethods_type = WStr("Datadog.Trace.AppSec.Waf.NativeBindings.NativeLibrary+NonWindows");
+const shared::WSTRING appsec_windows_nativemethods_type = WStr("Datadog.Trace.AppSec.Waf.NativeBindings.NativeLibrary+Windows");
 
-const WSTRING calltarget_modification_action = WStr("CallTargetModification");
+const shared::WSTRING calltarget_modification_action = WStr("CallTargetModification");
 
 #ifdef _WIN32
-const WSTRING native_dll_filename = WStr("DATADOG.TRACE.CLRPROFILER.NATIVE.DLL");
+const shared::WSTRING native_dll_filename = WStr("DATADOG.TRACE.CLRPROFILER.NATIVE.DLL");
 #elif MACOS
-const WSTRING native_dll_filename = WStr("Datadog.Trace.ClrProfiler.Native.dylib");
+const shared::WSTRING native_dll_filename = WStr("Datadog.Trace.ClrProfiler.Native.dylib");
 #else
-const WSTRING native_dll_filename = WStr("Datadog.Trace.ClrProfiler.Native.so");
+const shared::WSTRING native_dll_filename = WStr("Datadog.Trace.ClrProfiler.Native.so");
 #endif
 
 const AssemblyProperty managed_profiler_assembly_property = AssemblyProperty(

signalfx/signalfx-dotnet-tracing

shared/src/native-src/pal.h
@@ -1,16 +1,16 @@
-#ifndef DD_CLR_PROFILER_PAL_H_
-#define DD_CLR_PROFILER_PAL_H_
+#pragma once
 
 #ifdef _WIN32
 
-#include <process.h>
-#include <filesystem>
 #include "windows.h"
+#include <filesystem>
+#include <process.h>
 
 #else
 
-#include <unistd.h>
+#include <dlfcn.h>
 #include <fstream>
+#include <unistd.h>
 
 #endif
 
@@ -18,80 +18,117 @@
 #include <libproc.h>
 #endif
 
-#include "environment_variables.h"
-#include "string.h"  // NOLINT
-#include "util.h"
+#include "../../../shared/src/native-src/string.h" // NOLINT
+#include "../../../shared/src/native-src/util.h"
 
-namespace shared {
+namespace shared
+{
 
-    inline WSTRING DatadogLogFilePath() {
-        WSTRING directory = GetEnvironmentValue(environment::log_directory);
+template <class TLoggerPolicy>
+inline shared::WSTRING GetDatadogLogFilePath(const std::string& file_name_suffix)
+{
+    const auto file_name = TLoggerPolicy::file_name + file_name_suffix + ".log";
 
-        if (directory.length() > 0) {
-            return directory +
+    WSTRING directory = GetEnvironmentValue(TLoggerPolicy::logging_environment::log_directory);
+
+    if (directory.length() > 0)
+    {
+        return directory +
 #ifdef _WIN32
-                WStr('\\') +
+               WStr('\\') +
 #else
-                WStr('/') +
+               WStr('/') +
 #endif
-                WStr("dotnet-tracer-native.log");
-        }
-
-        WSTRING path = GetEnvironmentValue(environment::log_path);
-
-        if (path.length() > 0) {
-            return path;
-        }
-
-#ifdef _WIN32
-        char* p_program_data;
-        size_t length;
-        const errno_t result = _dupenv_s(&p_program_data, &length, "PROGRAMDATA");
-        std::string program_data;
-
-        if (SUCCEEDED(result) && p_program_data != nullptr && length > 0) {
-            program_data = std::string(p_program_data);
-        }
-        else {
-            program_data = R"(C:\ProgramData)";
-        }
-
-        return ToWSTRING(program_data +
-            R"(\SignalFx .NET Tracing\logs\dotnet-tracer-native.log)");
-#else
-        return WStr("/var/log/signalfx/dotnet/dotnet-tracer-native.log");
-#endif
+               ToWSTRING(file_name);
     }
 
-    inline WSTRING GetCurrentProcessName() {
-#ifdef _WIN32
-        const DWORD length = 260;
-        WCHAR buffer[length]{};
+    WSTRING path = GetEnvironmentValue(TLoggerPolicy::logging_environment::log_path);
 
-        const DWORD len = GetModuleFileName(nullptr, buffer, length);
-        const WSTRING current_process_path(buffer);
-        return std::filesystem::path(current_process_path).filename();
+    if (path.length() > 0)
+    {
+        return path;
+    }
+
+#ifdef _WIN32
+    std::filesystem::path program_data_path;
+    program_data_path = GetEnvironmentValue(WStr("PROGRAMDATA"));
+
+    if (program_data_path.empty())
+    {
+        program_data_path = WStr(R"(C:\ProgramData)");
+    }
+
+    // on Windows WSTRING == wstring
+    return (program_data_path / TLoggerPolicy::folder_path / file_name).wstring();
+#else
+    return ToWSTRING("/var/log/signalfx/dotnet/" + file_name);
+#endif
+}
+
+inline WSTRING GetCurrentProcessName()
+{
+#ifdef _WIN32
+    const DWORD length = 260;
+    WCHAR buffer[length]{};
+
+    const DWORD len = GetModuleFileName(nullptr, buffer, length);
+    const WSTRING current_process_path(buffer);
+    return std::filesystem::path(current_process_path).filename();
 #elif MACOS
-        const int length = 260;
-        char* buffer = new char[length];
-        proc_name(getpid(), buffer, length);
-        return ToWSTRING(std::string(buffer));
+    const int length = 260;
+    char* buffer = new char[length];
+    proc_name(getpid(), buffer, length);
+    return ToWSTRING(std::string(buffer));
 #else
-        std::fstream comm("/proc/self/comm");
-        std::string name;
-        std::getline(comm, name);
-        return ToWSTRING(name);
+    std::fstream comm("/proc/self/comm");
+    std::string name;
+    std::getline(comm, name);
+    return ToWSTRING(name);
 #endif
-    }
+}
 
-    inline int GetPID() {
+inline int GetPID()
+{
 #ifdef _WIN32
-        return _getpid();
+    return _getpid();
 #else
-        return getpid();
+    return getpid();
 #endif
+}
+
+inline WSTRING GetCurrentModuleFileName()
+{
+    static WSTRING moduleFileName = EmptyWStr;
+    if (moduleFileName != EmptyWStr)
+    {
+        // use cached version
+        return moduleFileName;
     }
 
-} // namespace trace
+#ifdef _WIN32
+    HMODULE hModule;
+    if (GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT | GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
+                           (LPCTSTR) GetCurrentModuleFileName, &hModule))
+    {
+        WCHAR lpFileName[1024];
+        DWORD lpFileNameLength = GetModuleFileNameW(hModule, lpFileName, 1024);
+        if (lpFileNameLength > 0)
+        {
+            moduleFileName = WSTRING(lpFileName, lpFileNameLength);
+            return moduleFileName;
+        }
+    }
+#else
+    Dl_info info;
+    if (dladdr((void*) GetCurrentModuleFileName, &info))
+    {
+        moduleFileName = ToWSTRING(ToString(info.dli_fname));
+        return moduleFileName;
+    }
+#endif
 
-#endif  // DD_CLR_PROFILER_PAL_H_
+    return EmptyWStr;
+}
+
+} // namespace shared
+

tracer/src/Datadog.Trace.ClrProfiler.Native/dd_profiler_constants.h
@@ -9,7 +9,7 @@
 namespace trace
 {
 
-const WSTRING env_vars_to_display[]{environment::tracing_enabled,
+const shared::WSTRING env_vars_to_display[]{environment::tracing_enabled,
                                     environment::debug_enabled,
                                     environment::profiler_home_path,
                                     environment::include_process_names,
@@ -30,7 +30,7 @@ const WSTRING env_vars_to_display[]{environment::tracing_enabled,
                                     environment::azure_app_services_app_pool_id,
                                     environment::azure_app_services_cli_telemetry_profile_value};
 
-const WSTRING skip_assembly_prefixes[]{
+const shared::WSTRING skip_assembly_prefixes[]{
     WStr("Microsoft.AI"),
     WStr("Microsoft.ApplicationInsights"),
     WStr("Microsoft.Build"),
@@ -66,7 +66,7 @@ const WSTRING skip_assembly_prefixes[]{
     WStr("System.Xml"),
 };
 
-const WSTRING skip_assemblies[]{WStr("mscorlib"),
+const shared::WSTRING skip_assemblies[]{WStr("mscorlib"),
                                 WStr("netstandard"),
                                 WStr("System.Configuration"),
                                 WStr("Microsoft.AspNetCore.Razor.Language"),
@@ -75,26 +75,26 @@ const WSTRING skip_assemblies[]{WStr("mscorlib"),
                                 WStr("Datadog.AutoInstrumentation.ManagedLoader"),
                                 WStr("ISymWrapper")};
 
-const WSTRING mscorlib_assemblyName = WStr("mscorlib");
-const WSTRING system_private_corelib_assemblyName = WStr("System.Private.CoreLib");
-const WSTRING datadog_trace_clrprofiler_managed_loader_assemblyName = WStr("SignalFx.Tracing.ClrProfiler.Managed.Loader");
+const shared::WSTRING mscorlib_assemblyName = WStr("mscorlib");
+const shared::WSTRING system_private_corelib_assemblyName = WStr("System.Private.CoreLib");
+const shared::WSTRING datadog_trace_clrprofiler_managed_loader_assemblyName = WStr("SignalFx.Tracing.ClrProfiler.Managed.Loader");
 
-const WSTRING managed_profiler_full_assembly_version =
+const shared::WSTRING managed_profiler_full_assembly_version =
     WStr("SignalFx.Tracing, Version=0.2.2.0, Culture=neutral, PublicKeyToken=e43a27c2023d388a");
 
-const WSTRING managed_profiler_name = WStr("SignalFx.Tracing");
+const shared::WSTRING managed_profiler_name = WStr("SignalFx.Tracing");
 
-const WSTRING nonwindows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+NonWindows");
-const WSTRING windows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+Windows");
+const shared::WSTRING nonwindows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+NonWindows");
+const shared::WSTRING windows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+Windows");
 
-const WSTRING calltarget_modification_action = WStr("CallTargetModification");
+const shared::WSTRING calltarget_modification_action = WStr("CallTargetModification");
 
 #ifdef _WIN32
-const WSTRING native_dll_filename = WStr("SIGNALFX.TRACING.CLRPROFILER.NATIVE.DLL");
+const shared::WSTRING native_dll_filename = WStr("SIGNALFX.TRACING.CLRPROFILER.NATIVE.DLL");
 #elif MACOS
-const WSTRING native_dll_filename = WStr("SignalFx.Tracing.ClrProfiler.Native.dylib");
+const shared::WSTRING native_dll_filename = WStr("SignalFx.Tracing.ClrProfiler.Native.dylib");
 #else
-const WSTRING native_dll_filename = WStr("SignalFx.Tracing.ClrProfiler.Native.so");
+const shared::WSTRING native_dll_filename = WStr("SignalFx.Tracing.ClrProfiler.Native.so");
 #endif
 
 const AssemblyProperty managed_profiler_assembly_property = AssemblyProperty(