513215ca9f7a3300cba0250843649a0023743bcc
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-03-29 11:59:45 +0200

    include F# class in AlwaysOnProfiler tests

--- tracer/test/test-applications/integrations/dependency-libs/Samples.AlwaysOnProfiler.Vb/ClassVb.vb
+++ tracer/test/test-applications/integrations/dependency-libs/Samples.AlwaysOnProfiler.Vb/ClassVb.vb
@@ -1,9 +1,5 @@
-Imports System.Threading
-
 Public Class ClassVb
     Public Shared Sub MethodVb(testParam As String)
-        Console.WriteLine("Thread.Sleep - starting " + testParam)
-        Thread.Sleep(TimeSpan.FromSeconds(6))
-        Console.WriteLine("Thread.Sleep - finished")
+        Fs.ClassFs.methodFs(testParam)
     End Sub
 End Class
