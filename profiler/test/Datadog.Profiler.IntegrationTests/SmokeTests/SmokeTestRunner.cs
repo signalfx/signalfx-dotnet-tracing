DataDog/dd-trace-dotnet

[Profiler] Activate checks in smoke tests for the new pipeline (#2552)
* Update test checks: check that no managed log files are created

profiler/test/Datadog.Profiler.IntegrationTests/SmokeTests/SmokeTestRunner.cs
@@ -20,7 +20,7 @@ namespace Datadog.Profiler.SmokeTests
 {
     public class SmokeTestRunner
     {
-        private static readonly Dictionary<string, string> ActivateNewPipeline = new Dictionary<string, string>() { { "DD_INTERNAL_PROFILING_LIBDDPROF_ENABLED", "1" } };
+        private static readonly string NewPipelineEnvVar = "DD_INTERNAL_PROFILING_LIBDDPROF_ENABLED";
 
         private readonly ITestOutputHelper _output;
         // The max test duration is _really_ big on some runners the test(s) can
@@ -45,10 +45,11 @@ namespace Datadog.Profiler.SmokeTests
         // additional parameter to the command line if needed (for --scenario support)
         private readonly string _commandLine;
 
+        private readonly Dictionary<string, string> _additionalEnvVars;
+
         private string _testLogDir;
         private string _testPprofDir;
         private string _appListenerPort;
-        private IReadOnlyDictionary<string, string> _additionalEnvVars;
 
         public SmokeTestRunner(string appName, string framework, string appAssembly, string commandLine, ITestOutputHelper output, bool useDefaultLogDir = false, bool useDefaultPprofDir = false)
         {
@@ -63,7 +64,7 @@ namespace Datadog.Profiler.SmokeTests
             _testLogDir = ConfigurationProviderUtils.GetOsSpecificDefaultLogDirectory();
             _testPprofDir = ConfigurationProviderUtils.GetOsSpecificDefaultPProfDirectory();
             EnvironmentHelper = new EnvironmentHelper(_framework);
-            _additionalEnvVars = null;
+            _additionalEnvVars = new();
         }
 
         public SmokeTestRunner(string appName, string framework, string appAssembly, ITestOutputHelper output, bool useDefaultLogDir = false, bool useDefaultPprofDir = false)
@@ -71,19 +72,18 @@ namespace Datadog.Profiler.SmokeTests
         {
         }
 
-        public SmokeTestRunner WithNewExporterPipeline()
-        {
-            _additionalEnvVars = ActivateNewPipeline;
-            return this;
-        }
-
         public EnvironmentHelper EnvironmentHelper { get; }
-
         public static string GetApplicationOutputFolderPath(string appName)
         {
             string configurationAndPlatform = $"{EnvironmentHelper.GetConfiguration()}-{EnvironmentHelper.GetPlatform()}";
             string binPath = EnvironmentHelper.GetBinOutputPath();
             return Path.Combine(binPath, configurationAndPlatform, "profiler", "src", "Demos", appName);
+        }
+
+        public SmokeTestRunner WithNewExporterPipeline()
+        {
+            _additionalEnvVars.Add(NewPipelineEnvVar, "1");
+            return this;
         }
 
         public void RunAndCheck()
@@ -93,8 +93,11 @@ namespace Datadog.Profiler.SmokeTests
                 RunTest(datadogMockAgent.Port);
                 PrintTestInfo();
 
-                // Avoid CI flackiness: checking pprof files is enough
-                // RunChecks(datadogMockAgent);
+                // Today, with the old pipeline the checks are flaky.
+                // With the new pipeline this should be fixed.
+                // So run the checks only for the new
+                var isRunningWithNewPipeline = _additionalEnvVars.TryGetValue(NewPipelineEnvVar, out var v) && string.Equals(v, "1", StringComparison.InvariantCultureIgnoreCase);
+                RunChecks(datadogMockAgent, isRunningWithNewPipeline);
             }
         }
 
@@ -135,8 +138,13 @@ namespace Datadog.Profiler.SmokeTests
             _output.WriteLine($"* PprofDir: {_testPprofDir}");
         }
 
-        private void RunChecks(MockDatadogAgent agent)
+        private void RunChecks(MockDatadogAgent agent, bool isRunningWithNewPipeline)
         {
+            if (!isRunningWithNewPipeline)
+            {
+                return;
+            }
+
             CheckLogFiles();
             CheckPprofFiles();
             CheckAgent(agent);
@@ -144,18 +152,25 @@ namespace Datadog.Profiler.SmokeTests
 
         private void CheckLogFiles()
         {
-            CheckLogFiles("DD-DotNet-Common-ManagedLoader*.*");
-            CheckLogFiles("DD-DotNet-Profiler-Managed*.*");
             CheckLogFiles("DD-DotNet-Profiler-Native*.*");
+            CheckNoLogFiles("DD-DotNet-Profiler-Managed*.*");
+            CheckNoLogFiles("DD-DotNet-Common-ManagedLoader*.*");
+        }
+
+        private void CheckNoLogFiles(string filePattern)
+        {
+            var files = Directory.EnumerateFiles(_testLogDir, filePattern, SearchOption.AllDirectories).ToList();
+
+            Assert.Empty(files);
         }
 
         private void CheckLogFiles(string filePattern)
         {
-            List<string> managedLoaderLogFiles = Directory.EnumerateFiles(_testLogDir, filePattern, SearchOption.AllDirectories).ToList();
+            List<string> files = Directory.EnumerateFiles(_testLogDir, filePattern, SearchOption.AllDirectories).ToList();
 
-            Assert.NotEmpty(managedLoaderLogFiles);
+            Assert.NotEmpty(files);
 
-            foreach (string logFile in managedLoaderLogFiles)
+            foreach (string logFile in files)
             {
                 Assert.False(LogFileContainsErrorMessage(logFile), $"Found error message in the log file {logFile}");
             }

signalfx/signalfx-dotnet-tracing

profiler/test/Datadog.Profiler.IntegrationTests/SmokeTests/SmokeTestRunner.cs
@@ -22,7 +22,7 @@ namespace Datadog.Profiler.SmokeTests
 {
     public class SmokeTestRunner
     {
-        private static readonly Dictionary<string, string> ActivateNewPipeline = new Dictionary<string, string>() { { "SIGNALFX_INTERNAL_PROFILING_LIBDDPROF_ENABLED", "1" } };
+        private static readonly string NewPipelineEnvVar = "SIGNALFX_INTERNAL_PROFILING_LIBDDPROF_ENABLED";
 
         private readonly ITestOutputHelper _output;
         // The max test duration is _really_ big on some runners the test(s) can
@@ -47,10 +47,11 @@ namespace Datadog.Profiler.SmokeTests
         // additional parameter to the command line if needed (for --scenario support)
         private readonly string _commandLine;
 
+        private readonly Dictionary<string, string> _additionalEnvVars;
+
         private string _testLogDir;
         private string _testPprofDir;
         private string _appListenerPort;
-        private IReadOnlyDictionary<string, string> _additionalEnvVars;
 
         public SmokeTestRunner(string appName, string framework, string appAssembly, string commandLine, ITestOutputHelper output, bool useDefaultLogDir = false, bool useDefaultPprofDir = false)
         {
@@ -65,7 +66,7 @@ namespace Datadog.Profiler.SmokeTests
             _testLogDir = ConfigurationProviderUtils.GetOsSpecificDefaultLogDirectory();
             _testPprofDir = ConfigurationProviderUtils.GetOsSpecificDefaultPProfDirectory();
             EnvironmentHelper = new EnvironmentHelper(_framework);
-            _additionalEnvVars = null;
+            _additionalEnvVars = new();
         }
 
         public SmokeTestRunner(string appName, string framework, string appAssembly, ITestOutputHelper output, bool useDefaultLogDir = false, bool useDefaultPprofDir = false)
@@ -73,19 +74,18 @@ namespace Datadog.Profiler.SmokeTests
         {
         }
 
-        public SmokeTestRunner WithNewExporterPipeline()
-        {
-            _additionalEnvVars = ActivateNewPipeline;
-            return this;
-        }
-
         public EnvironmentHelper EnvironmentHelper { get; }
-
         public static string GetApplicationOutputFolderPath(string appName)
         {
             string configurationAndPlatform = $"{EnvironmentHelper.GetConfiguration()}-{EnvironmentHelper.GetPlatform()}";
             string binPath = EnvironmentHelper.GetBinOutputPath();
             return Path.Combine(binPath, configurationAndPlatform, "profiler", "src", "Demos", appName);
+        }
+
+        public SmokeTestRunner WithNewExporterPipeline()
+        {
+            _additionalEnvVars.Add(NewPipelineEnvVar, "1");
+            return this;
         }
 
         public void RunAndCheck()
@@ -95,8 +95,11 @@ namespace Datadog.Profiler.SmokeTests
                 RunTest(datadogMockAgent.Port);
                 PrintTestInfo();
 
-                // Avoid CI flackiness: checking pprof files is enough
-                // RunChecks(datadogMockAgent);
+                // Today, with the old pipeline the checks are flaky.
+                // With the new pipeline this should be fixed.
+                // So run the checks only for the new
+                var isRunningWithNewPipeline = _additionalEnvVars.TryGetValue(NewPipelineEnvVar, out var v) && string.Equals(v, "1", StringComparison.InvariantCultureIgnoreCase);
+                RunChecks(datadogMockAgent, isRunningWithNewPipeline);
             }
         }
 
@@ -137,8 +140,13 @@ namespace Datadog.Profiler.SmokeTests
             _output.WriteLine($"* PprofDir: {_testPprofDir}");
         }
 
-        private void RunChecks(MockDatadogAgent agent)
+        private void RunChecks(MockDatadogAgent agent, bool isRunningWithNewPipeline)
         {
+            if (!isRunningWithNewPipeline)
+            {
+                return;
+            }
+
             CheckLogFiles();
             CheckPprofFiles();
             CheckAgent(agent);
@@ -146,18 +154,25 @@ namespace Datadog.Profiler.SmokeTests
 
         private void CheckLogFiles()
         {
-            CheckLogFiles("DD-DotNet-Common-ManagedLoader*.*");
-            CheckLogFiles("DD-DotNet-Profiler-Managed*.*");
             CheckLogFiles("DD-DotNet-Profiler-Native*.*");
+            CheckNoLogFiles("DD-DotNet-Profiler-Managed*.*");
+            CheckNoLogFiles("DD-DotNet-Common-ManagedLoader*.*");
+        }
+
+        private void CheckNoLogFiles(string filePattern)
+        {
+            var files = Directory.EnumerateFiles(_testLogDir, filePattern, SearchOption.AllDirectories).ToList();
+
+            Assert.Empty(files);
         }
 
         private void CheckLogFiles(string filePattern)
         {
-            List<string> managedLoaderLogFiles = Directory.EnumerateFiles(_testLogDir, filePattern, SearchOption.AllDirectories).ToList();
+            List<string> files = Directory.EnumerateFiles(_testLogDir, filePattern, SearchOption.AllDirectories).ToList();
 
-            Assert.NotEmpty(managedLoaderLogFiles);
+            Assert.NotEmpty(files);
 
-            foreach (string logFile in managedLoaderLogFiles)
+            foreach (string logFile in files)
             {
                 Assert.False(LogFileContainsErrorMessage(logFile), $"Found error message in the log file {logFile}");
             }