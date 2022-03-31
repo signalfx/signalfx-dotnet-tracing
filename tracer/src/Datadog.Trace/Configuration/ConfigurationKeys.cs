DataDog/dd-trace-dotnet

Add DD_TRACE_RATE_LIMIT parameter (#2557)
* Add DD_TRACE_RATE_LIMIT parameter

It is the parameter used by other tracers

* add pragma

* change test files

tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -151,7 +151,15 @@ namespace Datadog.Trace.Configuration
         /// to be submitted per second.
         /// </summary>
         /// <seealso cref="TracerSettings.MaxTracesSubmittedPerSecond"/>
+        [Obsolete("This parameter is obsolete and should be replaced by `DD_TRACE_RATE_LIMIT`")]
         public const string MaxTracesSubmittedPerSecond = "DD_MAX_TRACES_PER_SECOND";
+
+        /// <summary>
+        /// Configuration key for setting the number of traces allowed
+        /// to be submitted per second.
+        /// </summary>
+        /// <seealso cref="TracerSettings.MaxTracesSubmittedPerSecond"/>
+        public const string TraceRateLimit = "DD_TRACE_RATE_LIMIT";
 
         /// <summary>
         /// Configuration key for enabling or disabling the diagnostic log at startup

tracer/test/test-applications/integrations/Samples.RateLimiter/Program.cs
@@ -20,7 +20,7 @@ namespace Samples.RateLimiter
         {
             var numberOfSeconds = 5;
             var maxMilliseconds = numberOfSeconds * 1000;
-            var configuredLimitPerSecond = int.Parse(Environment.GetEnvironmentVariables()["DD_MAX_TRACES_PER_SECOND"].ToString());
+            var configuredLimitPerSecond = int.Parse(Environment.GetEnvironmentVariables()["DD_TRACE_RATE_LIMIT"].ToString());
 
             Console.WriteLine($"Ready to run for {numberOfSeconds} seconds.");
             Console.WriteLine($"Configured rate limit of {configuredLimitPerSecond}");

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -147,7 +147,15 @@ namespace Datadog.Trace.Configuration
         /// to be submitted per second.
         /// </summary>
         /// <seealso cref="TracerSettings.MaxTracesSubmittedPerSecond"/>
+        [Obsolete("This parameter is obsolete and should be replaced by `SIGNALFX_TRACE_RATE_LIMIT`")]
         public const string MaxTracesSubmittedPerSecond = "SIGNALFX_MAX_TRACES_PER_SECOND";
+
+        /// <summary>
+        /// Configuration key for setting the number of traces allowed
+        /// to be submitted per second.
+        /// </summary>
+        /// <seealso cref="TracerSettings.MaxTracesSubmittedPerSecond"/>
+        public const string TraceRateLimit = "SIGNALFX_TRACE_RATE_LIMIT";
 
         /// <summary>
         /// Configuration key for enabling or disabling the diagnostic log at startup

tracer/test/test-applications/integrations/Samples.RateLimiter/Program.cs
@@ -22,7 +22,7 @@ namespace Samples.RateLimiter
         {
             var numberOfSeconds = 5;
             var maxMilliseconds = numberOfSeconds * 1000;
-            var configuredLimitPerSecond = int.Parse(Environment.GetEnvironmentVariables()["SIGNALFX_MAX_TRACES_PER_SECOND"].ToString());
+            var configuredLimitPerSecond = int.Parse(Environment.GetEnvironmentVariables()["SIGNALFX_TRACE_RATE_LIMIT"].ToString());
 
             Console.WriteLine($"Ready to run for {numberOfSeconds} seconds.");
             Console.WriteLine($"Configured rate limit of {configuredLimitPerSecond}");