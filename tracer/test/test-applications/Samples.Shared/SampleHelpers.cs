DataDog/dd-trace-dotnet

Remove direct Datadog.Trace assembly references in integration tests (#2586)
* Replace hard Datadog.Trace usages in test-application:
1. Tracer.Instance.StartActive => SampleHelpers.CreateScope()
2. scope.Span.SetTag => SampleHelpers.TrySetTag()
3. Tracer.Instance.ForceFlushAsync => SampleHelpers.ForceTracerFlushAsync()
4. CorrelationIdentifier.TraceId => SampleHelpers.GetCorrelationIdentifierTraceId()
5. scope.Span.ResourceName => SampleHelpers.TrySetResourceName()

* Fix CorrelationIdentifierTraceIdProperty reflection lookup

* Add fallback code in SampleHelpers in case automatic instrumentation is not set up correctly

tracer/test/test-applications/Samples.Shared/SampleHelpers.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Samples
 {
@@ -12,11 +13,24 @@ namespace Samples
         private static readonly Type TracerType = Type.GetType("Datadog.Trace.Tracer, Datadog.Trace");
         private static readonly Type ScopeType = Type.GetType("Datadog.Trace.Scope, Datadog.Trace");
         private static readonly Type SpanType = Type.GetType("Datadog.Trace.Span, Datadog.Trace");
-        private static readonly MethodInfo GetTracerInstance = TracerType.GetProperty("Instance").GetMethod;
-        private static readonly MethodInfo StartActiveMethod = TracerType.GetMethod("StartActive", types: new[] { typeof(string) });
-        private static readonly MethodInfo ActiveScopeProperty = TracerType.GetProperty("ActiveScope").GetMethod;
-        private static readonly MethodInfo SpanProperty = ScopeType.GetProperty("Span", BindingFlags.NonPublic | BindingFlags.Instance).GetMethod;
-        private static readonly MethodInfo SetExceptionMethod = SpanType.GetMethod("SetException", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly Type CorrelationIdentifierType = Type.GetType("Datadog.Trace.CorrelationIdentifier, Datadog.Trace");
+        private static readonly MethodInfo GetTracerInstance = TracerType?.GetProperty("Instance")?.GetMethod;
+        private static readonly MethodInfo StartActiveMethod = TracerType?.GetMethod("StartActive", types: new[] { typeof(string) });
+        private static readonly MethodInfo ForceFlushAsyncMethod = TracerType?.GetMethod("ForceFlushAsync", BindingFlags.Public | BindingFlags.Instance);
+        private static readonly MethodInfo ActiveScopeProperty = TracerType?.GetProperty("ActiveScope")?.GetMethod;
+        private static readonly MethodInfo SpanProperty = ScopeType?.GetProperty("Span", BindingFlags.NonPublic | BindingFlags.Instance)?.GetMethod;
+        private static readonly MethodInfo CorrelationIdentifierTraceIdProperty = CorrelationIdentifierType?.GetProperty("TraceId", BindingFlags.Public | BindingFlags.Static)?.GetMethod;
+        private static readonly MethodInfo SetResourceNameProperty = SpanType?.GetProperty("ResourceName", BindingFlags.NonPublic | BindingFlags.Instance)?.SetMethod;
+        private static readonly MethodInfo SetTagMethod = SpanType?.GetMethod("SetTag", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly MethodInfo SetExceptionMethod = SpanType?.GetMethod("SetException", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        static SampleHelpers()
+        {
+            if (TracerType is null)
+            {
+                Console.WriteLine("*** [Warning] SampleHelpers.TracerType is null so you may experience missing spans. Ensure automatic instrumentation is correctly enabled for this application to make sure spans are generated. ***");
+            }
+        }
 
         public static bool IsProfilerAttached()
         {
@@ -62,12 +76,71 @@ namespace Samples
 
         public static IDisposable CreateScope(string operationName)
         {
+            if (GetTracerInstance is null || StartActiveMethod is null)
+            {
+                return new NoOpDisposable();
+            }
+
             var tracer = GetTracerInstance.Invoke(null, Array.Empty<object>());
             return (IDisposable) StartActiveMethod.Invoke(tracer, new object[] { operationName });
+        }
+        public static Task ForceTracerFlushAsync()
+        {
+            if (GetTracerInstance is null || ForceFlushAsyncMethod is null)
+            {
+                return Task.CompletedTask;
+            }
+
+            var tracer = GetTracerInstance.Invoke(null, Array.Empty<object>());
+            return (Task)ForceFlushAsyncMethod.Invoke(tracer, Array.Empty<object>());
+        }
+
+        public static IDisposable GetActiveScope()
+        {
+            if (GetTracerInstance is null || ActiveScopeProperty is null)
+            {
+                return new NoOpDisposable();
+            }
+
+            var tracer = GetTracerInstance.Invoke(null, Array.Empty<object>());
+            return (IDisposable) ActiveScopeProperty.Invoke(tracer, Array.Empty<object>());
+        }
+
+        public static ulong GetCorrelationIdentifierTraceId()
+        {
+            if (CorrelationIdentifierTraceIdProperty is null)
+            {
+                return 0;
+            }
+
+            return (ulong)CorrelationIdentifierTraceIdProperty.Invoke(null, Array.Empty<object>());
+        }
+
+        public static void TrySetResourceName(object scope, string resourceName)
+        {
+            if (SpanProperty != null && SetResourceNameProperty != null)
+            {
+                var span = SpanProperty.Invoke(scope, Array.Empty<object>());
+                SetResourceNameProperty.Invoke(span, new object[] { resourceName });
+            }
+        }
+
+        public static void TrySetTag(object scope, string key, string value)
+        {
+            if (SpanProperty != null && SetTagMethod != null)
+            {
+                var span = SpanProperty.Invoke(scope, Array.Empty<object>());
+                SetTagMethod.Invoke(span, new object[] { key, value });
+            }
         }
 
         public static void TrySetExceptionOnActiveScope(Exception exception)
         {
+            if (GetTracerInstance is null || ActiveScopeProperty is null || SpanProperty is null || SetExceptionMethod is null)
+            {
+                return;
+            }
+
             var tracer = GetTracerInstance.Invoke(null, Array.Empty<object>());
             var scope = ActiveScopeProperty.Invoke(tracer, Array.Empty<object>());
             if (scope is null)
@@ -92,6 +165,11 @@ namespace Samples
                           select new KeyValuePair<string, string>(key, value);
 
             return envVars.ToList();
+        }
+
+        class NoOpDisposable : IDisposable
+        {
+            public void Dispose() { }
         }
     }
 }

signalfx/signalfx-dotnet-tracing

tracer/test/test-applications/Samples.Shared/SampleHelpers.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Samples
 {
@@ -15,11 +16,24 @@ namespace Samples
         private static readonly Type TracerType = Type.GetType("Datadog.Trace.Tracer, SignalFx.Tracing");
         private static readonly Type ScopeType = Type.GetType("Datadog.Trace.Scope, SignalFx.Tracing");
         private static readonly Type SpanType = Type.GetType("Datadog.Trace.Span, SignalFx.Tracing");
-        private static readonly MethodInfo GetTracerInstance = TracerType.GetProperty("Instance").GetMethod;
-        private static readonly MethodInfo StartActiveMethod = TracerType.GetMethod("StartActive", types: new[] { typeof(string) });
-        private static readonly MethodInfo ActiveScopeProperty = TracerType.GetProperty("ActiveScope").GetMethod;
-        private static readonly MethodInfo SpanProperty = ScopeType.GetProperty("Span", BindingFlags.NonPublic | BindingFlags.Instance).GetMethod;
-        private static readonly MethodInfo SetExceptionMethod = SpanType.GetMethod("SetException", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly Type CorrelationIdentifierType = Type.GetType("Datadog.Trace.CorrelationIdentifier, SignalFx.Tracing");
+        private static readonly MethodInfo GetTracerInstance = TracerType?.GetProperty("Instance")?.GetMethod;
+        private static readonly MethodInfo StartActiveMethod = TracerType?.GetMethod("StartActive", types: new[] { typeof(string) });
+        private static readonly MethodInfo ForceFlushAsyncMethod = TracerType?.GetMethod("ForceFlushAsync", BindingFlags.Public | BindingFlags.Instance);
+        private static readonly MethodInfo ActiveScopeProperty = TracerType?.GetProperty("ActiveScope")?.GetMethod;
+        private static readonly MethodInfo SpanProperty = ScopeType?.GetProperty("Span", BindingFlags.NonPublic | BindingFlags.Instance)?.GetMethod;
+        private static readonly MethodInfo CorrelationIdentifierTraceIdProperty = CorrelationIdentifierType?.GetProperty("TraceId", BindingFlags.Public | BindingFlags.Static)?.GetMethod;
+        private static readonly MethodInfo SetResourceNameProperty = SpanType?.GetProperty("ResourceName", BindingFlags.NonPublic | BindingFlags.Instance)?.SetMethod;
+        private static readonly MethodInfo SetTagMethod = SpanType?.GetMethod("SetTag", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly MethodInfo SetExceptionMethod = SpanType?.GetMethod("SetException", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        static SampleHelpers()
+        {
+            if (TracerType is null)
+            {
+                Console.WriteLine("*** [Warning] SampleHelpers.TracerType is null so you may experience missing spans. Ensure automatic instrumentation is correctly enabled for this application to make sure spans are generated. ***");
+            }
+        }
 
         public static bool IsProfilerAttached()
         {
@@ -72,12 +86,71 @@ namespace Samples
 
         public static IDisposable CreateScope(string operationName)
         {
+            if (GetTracerInstance is null || StartActiveMethod is null)
+            {
+                return new NoOpDisposable();
+            }
+
             var tracer = GetTracerInstance.Invoke(null, Array.Empty<object>());
             return (IDisposable) StartActiveMethod.Invoke(tracer, new object[] { operationName });
+        }
+        public static Task ForceTracerFlushAsync()
+        {
+            if (GetTracerInstance is null || ForceFlushAsyncMethod is null)
+            {
+                return Task.CompletedTask;
+            }
+
+            var tracer = GetTracerInstance.Invoke(null, Array.Empty<object>());
+            return (Task)ForceFlushAsyncMethod.Invoke(tracer, Array.Empty<object>());
+        }
+
+        public static IDisposable GetActiveScope()
+        {
+            if (GetTracerInstance is null || ActiveScopeProperty is null)
+            {
+                return new NoOpDisposable();
+            }
+
+            var tracer = GetTracerInstance.Invoke(null, Array.Empty<object>());
+            return (IDisposable) ActiveScopeProperty.Invoke(tracer, Array.Empty<object>());
+        }
+
+        public static ulong GetCorrelationIdentifierTraceId()
+        {
+            if (CorrelationIdentifierTraceIdProperty is null)
+            {
+                return 0;
+            }
+
+            return (ulong)CorrelationIdentifierTraceIdProperty.Invoke(null, Array.Empty<object>());
+        }
+
+        public static void TrySetResourceName(object scope, string resourceName)
+        {
+            if (SpanProperty != null && SetResourceNameProperty != null)
+            {
+                var span = SpanProperty.Invoke(scope, Array.Empty<object>());
+                SetResourceNameProperty.Invoke(span, new object[] { resourceName });
+            }
+        }
+
+        public static void TrySetTag(object scope, string key, string value)
+        {
+            if (SpanProperty != null && SetTagMethod != null)
+            {
+                var span = SpanProperty.Invoke(scope, Array.Empty<object>());
+                SetTagMethod.Invoke(span, new object[] { key, value });
+            }
         }
 
         public static void TrySetExceptionOnActiveScope(Exception exception)
         {
+            if (GetTracerInstance is null || ActiveScopeProperty is null || SpanProperty is null || SetExceptionMethod is null)
+            {
+                return;
+            }
+
             var tracer = GetTracerInstance.Invoke(null, Array.Empty<object>());
             var scope = ActiveScopeProperty.Invoke(tracer, Array.Empty<object>());
             if (scope is null)
@@ -116,6 +189,11 @@ namespace Samples
                 // it should prevent loading types in SignalFx.Tracing.OpenTracing existing in OpenTracing library
                 return rtlex.Types.Where(t => t != null);
             }
+        }
+
+        class NoOpDisposable : IDisposable
+        {
+            public void Dispose() { }
         }
     }
 }