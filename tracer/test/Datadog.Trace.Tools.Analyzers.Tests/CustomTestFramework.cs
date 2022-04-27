b477864f7c0039bb5a14d23c8c3f2e7baa275e6c
Andrew Lock <andrew.lock@datadoghq.com>
2022-04-27 09:50:53 +0100

    Consolidate custom frameworks (#2729)
    * Update CustomTestFramework implementations to use the TestHelpers one
    
    They were identical, except for the fact the TestHelpers one enforces the profiler target folder locating. Adds another constructor to skip that when not required
    
    * Fix reported status for skipped tests
    
    * Add custom test framework to remaining unit test projects
    
    * Add custom framework to more test projects
    
    * Add warning message back into test framework

--- tracer/test/Datadog.Trace.Tools.Analyzers.Tests/CustomTestFramework.cs
+++ tracer/test/Datadog.Trace.Tools.Analyzers.Tests/CustomTestFramework.cs
@@ -1,4 +1,4 @@
-// <copyright file="CustomTestFramework.cs" company="Datadog">
+﻿// <copyright file="CustomTestFramework.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
@@ -6,11 +6,11 @@
 using Xunit;
 using Xunit.Abstractions;
 
-[assembly: TestFramework("Datadog.Trace.ClrProfiler.Managed.Tests.CustomTestFramework", "Datadog.Trace.ClrProfiler.Managed.Tests")]
+[assembly: TestFramework("Datadog.Trace.Tools.Analyzers.Tests.CustomTestFramework", "Datadog.Trace.Tools.Analyzers.Tests")]
 
-namespace Datadog.Trace.ClrProfiler.Managed.Tests
+namespace Datadog.Trace.Tools.Analyzers.Tests
 {
-    public class CustomTestFramework : TestHelpers.CustomTestFramework
+    public class CustomTestFramework : Datadog.Trace.TestHelpers.CustomTestFramework
     {
         public CustomTestFramework(IMessageSink messageSink)
             : base(messageSink)
