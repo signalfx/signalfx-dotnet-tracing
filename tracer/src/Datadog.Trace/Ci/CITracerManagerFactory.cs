DataDog/dd-trace-dotnet

Refactor base endpoint calculations (#2688)
* Fix MockTracerAgent UDS handling and HTTP generation

The previous code assumed that the client disconnects after receiving the response, which is not always the case (e.g. when using `HttpClient`). Consequently, we would hang indefinitely if the client didn't hang up.

A space as the first character in the response headers also causes HttpClient to fail

* Refactor how we calculate the base endpoint

For some transports (e.g. UDS/named pipes) the "agent URI" is a filepath, not an `http://` path. Currently, many places assume the latter, for example constructing the endpoint by appending a path to it. This is invalid for UDS (e.g. it creates URLs like `C:\temp\path\fdyt3ifhyg.dsf/v4/traces`).

Up to now, this had some strange, but apparently(?) harmless,  effects for UDS, though I'm not entirely convinced on that front.

For example, currently (i.e. on master), we are sending the following headers when making a UDS request on Windows:

   POST C:/v0.4/traces HTTP/1.1
   Host:
   Accept-Encoding:identity
   Content-Length:1
   Datadog-Meta-Lang:.NET
   Datadog-Meta-Tracer-Version:2.7.0.0
   x-datadog-tracing-enabled:false
   Datadog-Meta-Lang-Interpreter:.NET
   Datadog-Meta-Lang-Version:6.0.4
   Datadog-Client-Computed-Top-Level:1
   Content-Type:application/msgpack

Notice the query path and Host tags are wrong...

With these changes, we use localhost as the Hostname for requests where the "host" doesn't make sense (named pipes and uds). This also resolves the "path" issue.

* Fix UDS telemetry tests

And stop depending on Datadog.Trace directly

* minor: remove unused parameters in CiIAgentlessWriter

* Add trace transport type to telemetry

* Add transport tests to explicitly test over UDS

* Add XmlDocs

* Add tests for the "agent_transport" key

tracer/src/Datadog.Trace/Ci/CITracerManagerFactory.cs
@@ -56,7 +56,7 @@ namespace Datadog.Trace.Ci
             {
                 if (!string.IsNullOrEmpty(_settings.ApiKey))
                 {
-                    return new CIAgentlessWriter(settings, sampler, new CIWriterHttpSender(GetRequestFactory()));
+                    return new CIAgentlessWriter(new CIWriterHttpSender(GetRequestFactory(settings)));
                 }
                 else
                 {
@@ -74,17 +74,17 @@ namespace Datadog.Trace.Ci
             }
         }
 
-        private IApiRequestFactory GetRequestFactory()
+        private IApiRequestFactory GetRequestFactory(ImmutableTracerSettings settings)
         {
             IApiRequestFactory factory = null;
             TimeSpan agentlessTimeout = TimeSpan.FromSeconds(15);
 
 #if NETCOREAPP
             Log.Information("Using {FactoryType} for trace transport.", nameof(HttpClientRequestFactory));
-            factory = new HttpClientRequestFactory(AgentHttpHeaderNames.DefaultHeaders, timeout: agentlessTimeout);
+            factory = new HttpClientRequestFactory(settings.Exporter.AgentUri, AgentHttpHeaderNames.DefaultHeaders, timeout: agentlessTimeout);
 #else
             Log.Information("Using {FactoryType} for trace transport.", nameof(ApiWebRequestFactory));
-            factory = new ApiWebRequestFactory(AgentHttpHeaderNames.DefaultHeaders, timeout: agentlessTimeout);
+            factory = new ApiWebRequestFactory(settings.Exporter.AgentUri, AgentHttpHeaderNames.DefaultHeaders, timeout: agentlessTimeout);
 #endif
 
             if (!string.IsNullOrWhiteSpace(_settings.ProxyHttps))

tracer/src/Datadog.Trace/Telemetry/Collectors/ConfigurationTelemetryCollector.cs
@@ -113,6 +113,7 @@ namespace Datadog.Trace.Telemetry
                 new(ConfigTelemetryData.Platform, value: FrameworkDescription.Instance.ProcessArchitecture),
                 new(ConfigTelemetryData.Enabled, value: settings.TraceEnabled),
                 new(ConfigTelemetryData.AgentUrl, value: settings.Exporter.AgentUri.ToString()),
+                new(ConfigTelemetryData.AgentTraceTransport, value: settings.Exporter.TracesTransport.ToString()),
                 new(ConfigTelemetryData.Debug, value: GlobalSettings.Source.DebugEnabled),
 #pragma warning disable CS0618
                 new(ConfigTelemetryData.AnalyticsEnabled, value: settings.AnalyticsEnabled),

tracer/src/Datadog.Trace/TracerManager.cs
@@ -292,6 +292,9 @@ namespace Datadog.Trace
                     writer.WritePropertyName("agent_url");
                     writer.WriteValue(instanceSettings.Exporter.AgentUri);
 
+                    writer.WritePropertyName("agent_transport");
+                    writer.WriteValue(instanceSettings.Exporter.TracesTransport.ToString());
+
                     writer.WritePropertyName("debug");
                     writer.WriteValue(GlobalSettings.Source.DebugEnabled);
 

tracer/src/Datadog.Trace/TracerManagerFactory.cs
@@ -145,7 +145,7 @@ namespace Datadog.Trace
         protected virtual IAgentWriter GetAgentWriter(ImmutableTracerSettings settings, IDogStatsd statsd, ISampler sampler)
         {
             var apiRequestFactory = TracesTransportStrategy.Get(settings.Exporter);
-            var api = new Api(settings.Exporter.AgentUri, apiRequestFactory, statsd, rates => sampler.SetDefaultSampleRates(rates), settings.Exporter.PartialFlushEnabled);
+            var api = new Api(apiRequestFactory, statsd, rates => sampler.SetDefaultSampleRates(rates), settings.Exporter.PartialFlushEnabled);
             return new AgentWriter(api, statsd, maxBufferSize: settings.TraceBufferSize);
         }
 

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/Ci/CITracerManagerFactory.cs
@@ -60,7 +60,7 @@ namespace Datadog.Trace.Ci
             {
                 if (!string.IsNullOrEmpty(_settings.ApiKey))
                 {
-                    return new CIAgentlessWriter(settings, sampler, new CIWriterHttpSender(GetRequestFactory()));
+                    return new CIAgentlessWriter(new CIWriterHttpSender(GetRequestFactory(settings)));
                 }
                 else
                 {
@@ -78,17 +78,17 @@ namespace Datadog.Trace.Ci
             }
         }
 
-        private IApiRequestFactory GetRequestFactory()
+        private IApiRequestFactory GetRequestFactory(ImmutableTracerSettings settings)
         {
             IApiRequestFactory factory = null;
             TimeSpan agentlessTimeout = TimeSpan.FromSeconds(15);
 
 #if NETCOREAPP
             Log.Information("Using {FactoryType} for trace transport.", nameof(HttpClientRequestFactory));
-            factory = new HttpClientRequestFactory(AgentHttpHeaderNames.DefaultHeaders, timeout: agentlessTimeout);
+            factory = new HttpClientRequestFactory(settings.ExporterSettings.AgentUri, AgentHttpHeaderNames.DefaultHeaders, timeout: agentlessTimeout);
 #else
             Log.Information("Using {FactoryType} for trace transport.", nameof(ApiWebRequestFactory));
-            factory = new ApiWebRequestFactory(AgentHttpHeaderNames.DefaultHeaders, timeout: agentlessTimeout);
+            factory = new ApiWebRequestFactory(settings.ExporterSettings.AgentUri, AgentHttpHeaderNames.DefaultHeaders, timeout: agentlessTimeout);
 #endif
 
             if (!string.IsNullOrWhiteSpace(_settings.ProxyHttps))

tracer/src/Datadog.Trace/Telemetry/Collectors/ConfigurationTelemetryCollector.cs
@@ -107,6 +107,7 @@ namespace Datadog.Trace.Telemetry
                 new(ConfigTelemetryData.Platform, value: FrameworkDescription.Instance.ProcessArchitecture),
                 new(ConfigTelemetryData.Enabled, value: settings.TraceEnabled),
                 new(ConfigTelemetryData.AgentUrl, value: settings.ExporterSettings.AgentUri.ToString()),
+                new(ConfigTelemetryData.AgentTraceTransport, value: settings.ExporterSettings.TracesTransport.ToString()),
                 new(ConfigTelemetryData.Debug, value: GlobalSettings.Source.DebugEnabled),
 #pragma warning disable CS0618
                 new(ConfigTelemetryData.AnalyticsEnabled, value: settings.AnalyticsEnabled),

tracer/src/Datadog.Trace/TracerManager.cs
@@ -304,6 +304,9 @@ namespace Datadog.Trace
                     writer.WritePropertyName("metrics_agent_url");
                     writer.WriteValue(instanceSettings.ExporterSettings.MetricsEndpointUrl);
 
+                    writer.WritePropertyName("agent_transport");
+                    writer.WriteValue(instanceSettings.ExporterSettings.TracesTransport.ToString());
+
                     writer.WritePropertyName("debug");
                     writer.WriteValue(GlobalSettings.Source.DebugEnabled);
 

tracer/src/Datadog.Trace/TracerManagerFactory.cs
@@ -163,7 +163,7 @@ namespace Datadog.Trace
                     return new ExporterWriter(new ZipkinExporter(settings), metrics);
                 default:
                     var apiRequestFactory = TracesTransportStrategy.Get(settings.ExporterSettings);
-                    var api = new Api(settings.ExporterSettings.AgentUri, apiRequestFactory, statsd, rates => sampler.SetDefaultSampleRates(rates), settings.ExporterSettings.PartialFlushEnabled);
+                    var api = new Api(apiRequestFactory, statsd, rates => sampler.SetDefaultSampleRates(rates), settings.ExporterSettings.PartialFlushEnabled);
                     return new AgentWriter(api, metrics, maxBufferSize: settings.TraceBufferSize);
             }
         }