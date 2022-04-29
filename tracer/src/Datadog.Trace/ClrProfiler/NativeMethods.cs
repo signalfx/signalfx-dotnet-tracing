DataDog/dd-trace-dotnet

Automatically instrument methods decorated with `[Trace]` (`Datadog.Trace.Annotations.TraceAttribute`) (#2606)
* Implement feature to customize span operation and resource name in TraceAnnotationsIntegration spans by decorating a method specified by DD_TRACE_METHODS with a Datadog.Trace.TraceAttribute

Next: Have the profiler automatically find methods with the Datadog.Trace.TraceAttribute and instrument them with the TraceAnnotationsIntegration

* Test: Update tests with a brand new type that isn't not specified in DD_TRACE_METHODS, decorate it with the Datadog.Trace.TraceAttribute, and assert that we have a new span. Also apply the attribute to methods already specified by DD_TRACE_METHODS and assert that we have the same number of spans and that the attribute's ResourceName and OperationName properties get applied to the generated span.

Product:
- Modify the ModuleLoadFinished callback to look through a module (so long as we don't intentionally skip it) to look for methods decorated with  Datadog.Trace.TraceAttribute and request ReJIT on them. If the integrations have not been loaded yet, they're put on a queue to be requested for ReJIT at a later ModuleLoadFinished callback.
- Add a new PInvoke in the profiler to add the TraceAnnotationsIntegration type earlier in the startup code path than the InitializeTraceMethods PInvoke, which occurs after Tracer initialization

* Remove Samples.TraceAnnotations.Program[RunTestsAsync] from DD_TRACE_METHODS and mark it with [Trace] to demonstrate that we can instrument early methods (except for the entrypoint Main) at this point

* Update GetIntegrationsFromTraceMethodsConfiguration signature to accept a TypeReference argument so we can re-use the one trace_annotation_integration_type TypeReference

* Add documentation to Datadog.Trace.TraceAttribute and update public api test

* Add log message in TraceAnnotationInfoFactory

* Try to remove whitespace changes

* Fix typo in test case

* Improve loggin

tracer/src/Datadog.Trace/ClrProfiler/NativeMethods.cs
@@ -81,6 +81,24 @@ namespace Datadog.Trace.ClrProfiler
             }
         }
 
+        public static void AddTraceAttributeInstrumentation(string id, string assemblyName, string typeName)
+        {
+            if (string.IsNullOrWhiteSpace(assemblyName)
+                || string.IsNullOrWhiteSpace(typeName))
+            {
+                return;
+            }
+
+            if (IsWindows)
+            {
+                Windows.AddTraceAttributeInstrumentation(id, assemblyName, typeName);
+            }
+            else
+            {
+                NonWindows.AddTraceAttributeInstrumentation(id, assemblyName, typeName);
+            }
+        }
+
         public static void InitializeTraceMethods(string id, string assemblyName, string typeName, string configuration)
         {
             if (string.IsNullOrWhiteSpace(configuration)
@@ -120,6 +138,9 @@ namespace Datadog.Trace.ClrProfiler
             public static extern void AddDerivedInstrumentations([MarshalAs(UnmanagedType.LPWStr)] string id, [In] NativeCallTargetDefinition[] methodArrays, int size);
 
             [DllImport("Datadog.Trace.ClrProfiler.Native.dll")]
+            public static extern void AddTraceAttributeInstrumentation([MarshalAs(UnmanagedType.LPWStr)] string id, [MarshalAs(UnmanagedType.LPWStr)] string assemblyName, [MarshalAs(UnmanagedType.LPWStr)] string typeName);
+
+            [DllImport("Datadog.Trace.ClrProfiler.Native.dll")]
             public static extern void InitializeTraceMethods([MarshalAs(UnmanagedType.LPWStr)] string id, [MarshalAs(UnmanagedType.LPWStr)] string assemblyName, [MarshalAs(UnmanagedType.LPWStr)] string typeName, [MarshalAs(UnmanagedType.LPWStr)] string configuration);
         }
 
@@ -140,6 +161,9 @@ namespace Datadog.Trace.ClrProfiler
 
             [DllImport("Datadog.Trace.ClrProfiler.Native")]
             public static extern void AddDerivedInstrumentations([MarshalAs(UnmanagedType.LPWStr)] string id, [In] NativeCallTargetDefinition[] methodArrays, int size);
+
+            [DllImport("Datadog.Trace.ClrProfiler.Native")]
+            public static extern void AddTraceAttributeInstrumentation([MarshalAs(UnmanagedType.LPWStr)] string id, [MarshalAs(UnmanagedType.LPWStr)] string assemblyName, [MarshalAs(UnmanagedType.LPWStr)] string typeName);
 
             [DllImport("Datadog.Trace.ClrProfiler.Native")]
             public static extern void InitializeTraceMethods([MarshalAs(UnmanagedType.LPWStr)] string id, [MarshalAs(UnmanagedType.LPWStr)] string assemblyName, [MarshalAs(UnmanagedType.LPWStr)] string typeName, [MarshalAs(UnmanagedType.LPWStr)] string configuration);

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/ClrProfiler/NativeMethods.cs
@@ -83,6 +83,24 @@ namespace Datadog.Trace.ClrProfiler
             }
         }
 
+        public static void AddTraceAttributeInstrumentation(string id, string assemblyName, string typeName)
+        {
+            if (string.IsNullOrWhiteSpace(assemblyName)
+                || string.IsNullOrWhiteSpace(typeName))
+            {
+                return;
+            }
+
+            if (IsWindows)
+            {
+                Windows.AddTraceAttributeInstrumentation(id, assemblyName, typeName);
+            }
+            else
+            {
+                NonWindows.AddTraceAttributeInstrumentation(id, assemblyName, typeName);
+            }
+        }
+
         public static void InitializeTraceMethods(string id, string assemblyName, string typeName, string configuration)
         {
             if (string.IsNullOrWhiteSpace(configuration)
@@ -139,6 +157,9 @@ namespace Datadog.Trace.ClrProfiler
             public static extern void AddDerivedInstrumentations([MarshalAs(UnmanagedType.LPWStr)] string id, [In] NativeCallTargetDefinition[] methodArrays, int size);
 
             [DllImport("SignalFx.Tracing.ClrProfiler.Native.dll")]
+            public static extern void AddTraceAttributeInstrumentation([MarshalAs(UnmanagedType.LPWStr)] string id, [MarshalAs(UnmanagedType.LPWStr)] string assemblyName, [MarshalAs(UnmanagedType.LPWStr)] string typeName);
+
+            [DllImport("SignalFx.Tracing.ClrProfiler.Native.dll")]
             public static extern void InitializeTraceMethods([MarshalAs(UnmanagedType.LPWStr)] string id, [MarshalAs(UnmanagedType.LPWStr)] string assemblyName, [MarshalAs(UnmanagedType.LPWStr)] string typeName, [MarshalAs(UnmanagedType.LPWStr)] string configuration);
 
             [DllImport("SignalFx.Tracing.ClrProfiler.Native.dll")]
@@ -165,6 +186,9 @@ namespace Datadog.Trace.ClrProfiler
 
             [DllImport("SignalFx.Tracing.ClrProfiler.Native")]
             public static extern void AddDerivedInstrumentations([MarshalAs(UnmanagedType.LPWStr)] string id, [In] NativeCallTargetDefinition[] methodArrays, int size);
+
+            [DllImport("SignalFx.Tracing.ClrProfiler.Native")]
+            public static extern void AddTraceAttributeInstrumentation([MarshalAs(UnmanagedType.LPWStr)] string id, [MarshalAs(UnmanagedType.LPWStr)] string assemblyName, [MarshalAs(UnmanagedType.LPWStr)] string typeName);
 
             [DllImport("SignalFx.Tracing.ClrProfiler.Native")]
             public static extern void InitializeTraceMethods([MarshalAs(UnmanagedType.LPWStr)] string id, [MarshalAs(UnmanagedType.LPWStr)] string assemblyName, [MarshalAs(UnmanagedType.LPWStr)] string typeName, [MarshalAs(UnmanagedType.LPWStr)] string configuration);