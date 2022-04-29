DataDog/dd-trace-dotnet

[Profiler] CPU profiling implementation (#2645)
* Implement the "simple" CPU consumption computation
* Refactor with CollectorBase/ProviderBase

profiler/src/ProfilerEngine/Datadog.Profiler.Native/EnvironmentVariables.h
@@ -25,6 +25,7 @@ public:
     inline static const shared::WSTRING Hostname                    = WStr("DD_HOSTNAME");
     inline static const shared::WSTRING Tags                        = WStr("DD_TAGS");
     inline static const shared::WSTRING NativeFramesEnabled         = WStr("DD_PROFILING_FRAMES_NATIVE_ENABLED");
+    inline static const shared::WSTRING CpuProfilingEnabled         = WStr("DD_PROFILING_CPU_ENABLED");
     inline static const shared::WSTRING ProfilesOutputDir           = WStr("DD_INTERNAL_PROFILING_OUTPUT_DIR");
     inline static const shared::WSTRING DevelopmentConfiguration    = WStr("DD_INTERNAL_USE_DEVELOPMENT_CONFIGURATION");
     inline static const shared::WSTRING Agentless                   = WStr("DD_PROFILING_AGENTLESS");

profiler/test/Datadog.Profiler.IntegrationTests/Helpers/EnvironmentVariables.cs
@@ -7,11 +7,12 @@ namespace Datadog.Profiler.IntegrationTests.Helpers
 {
     internal class EnvironmentVariables
     {
-        public const string LibDdPprofPipepline = "DD_INTERNAL_PROFILING_LIBDDPROF_ENABLED";
+        public const string LibDdPprofPipeline = "DD_INTERNAL_PROFILING_LIBDDPROF_ENABLED";
         public const string ProfilingLogDir = "DD_PROFILING_LOG_DIR";
         public const string ProfilingPprofDir = "DD_INTERNAL_PROFILING_OUTPUT_DIR";
         public const string ProfilerInstallationFolder = "DD_TESTING_PROFILER_FOLDER";
         public const string CodeHotSpotsEnable = "DD_PROFILING_CODEHOTSPOTS_ENABLED";
         public const string UseNativeLoader = "USE_NATIVE_LOADER";
+        public const string CpuProfilerEnabled = "DD_PROFILING_CPU_ENABLED";
     }
 }

signalfx/signalfx-dotnet-tracing

profiler/src/ProfilerEngine/Datadog.Profiler.Native/EnvironmentVariables.h
@@ -25,6 +25,7 @@ public:
     inline static const shared::WSTRING Hostname                    = WStr("SIGNALFX_HOSTNAME");
     inline static const shared::WSTRING Tags                        = WStr("SIGNALFX_TAGS");
     inline static const shared::WSTRING NativeFramesEnabled         = WStr("SIGNALFX_PROFILING_FRAMES_NATIVE_ENABLED");
+    inline static const shared::WSTRING CpuProfilingEnabled         = WStr("SIGNALFX_PROFILING_CPU_ENABLED");
     inline static const shared::WSTRING ProfilesOutputDir           = WStr("SIGNALFX_PROFILING_OUTPUT_DIR");
     inline static const shared::WSTRING DevelopmentConfiguration    = WStr("SIGNALFX_INTERNAL_USE_DEVELOPMENT_CONFIGURATION");
     inline static const shared::WSTRING Agentless                   = WStr("SIGNALFX_PROFILING_AGENTLESS");

profiler/test/Datadog.Profiler.IntegrationTests/Helpers/EnvironmentVariables.cs
@@ -7,11 +7,12 @@ namespace Datadog.Profiler.IntegrationTests.Helpers
 {
     internal class EnvironmentVariables
     {
-        public const string LibDdPprofPipepline = "SIGNALFX_INTERNAL_PROFILING_LIBDDPROF_ENABLED";
+        public const string LibDdPprofPipeline = "SIGNALFX_INTERNAL_PROFILING_LIBDDPROF_ENABLED";
         public const string ProfilingLogDir = "SIGNALFX_PROFILING_LOG_DIR";
         public const string ProfilingPprofDir = "SIGNALFX_INTERNAL_PROFILING_OUTPUT_DIR";
         public const string ProfilerInstallationFolder = "SIGNALFX_TESTING_PROFILER_FOLDER";
         public const string CodeHotSpotsEnable = "SIGNALFX_PROFILING_CODEHOTSPOTS_ENABLED";
         public const string UseNativeLoader = "SIGNALFX_USE_NATIVE_LOADER";
+        public const string CpuProfilerEnabled = "SIGNALFX_PROFILING_CPU_ENABLED";
     }
 }