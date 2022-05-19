DataDog/dd-trace-dotnet

Write integration test logs directly to `build_data` directory (#2773)
* Write log files directly to build-data, instead of system

There's something weird going on with the hosted agents, where we're seeing logs from other pipeline runs. To try and avoid that, set `DD_TRACE_LOG_DIRECTORY` so logs are written directly to the build_data folder, instead of copying them later. That should avoid the issue (whatever it is)

* Fix NoMultiLoaderTests

tracer/build/_build/Build.ExplorationTests.cs
@@ -129,6 +129,7 @@ partial class Build
                .After(Clean, BuildTracerHome, SetUpExplorationTests)
                .Executes(() =>
                 {
+                    FileSystemTasks.EnsureExistingDirectory(TestLogsDirectory);
                     try
                     {
                         var envVariables = GetEnvironmentVariables();
@@ -137,7 +138,7 @@ partial class Build
                     }
                     finally
                     {
-                        MoveLogsToBuildData();
+                        CopyDumpsToBuildData();
                     }
                 })
         ;
@@ -146,6 +147,7 @@ partial class Build
     {
         var envVariables = new Dictionary<string, string>
         {
+            ["DD_TRACE_LOG_DIRECTORY"] = TestLogsDirectory,
             ["DD_SERVICE"] = "exploration_tests",
             ["DD_VERSION"] = Version
         };

tracer/build/_build/NukeExtensions/DotNetSettingsExtensions.cs
@@ -67,6 +67,12 @@ internal static partial class DotNetSettingsExtensions
     {
         return settings.SetProcessEnvironmentVariable("DD_SERVICE_NAME", serviceName);
     }
+
+    public static T SetLogsDirectory<T>(this T settings, AbsolutePath logsDirectory)
+        where T: ToolSettings
+    {
+        return settings.SetProcessEnvironmentVariable("DD_TRACE_LOG_DIRECTORY", logsDirectory);
+    }
     
     public static T SetProcessEnvironmentVariables<T>(this T settings, IEnumerable<KeyValuePair<string, string>> variables)
         where T: ToolSettings

signalfx/signalfx-dotnet-tracing

tracer/build/_build/Build.ExplorationTests.cs
@@ -131,6 +131,7 @@ partial class Build
                .After(Clean, BuildTracerHome, SetUpExplorationTests)
                .Executes(() =>
                 {
+                    FileSystemTasks.EnsureExistingDirectory(TestLogsDirectory);
                     try
                     {
                         var envVariables = GetEnvironmentVariables();
@@ -139,7 +140,7 @@ partial class Build
                     }
                     finally
                     {
-                        MoveLogsToBuildData();
+                        CopyDumpsToBuildData();
                     }
                 })
         ;
@@ -148,6 +149,7 @@ partial class Build
     {
         var envVariables = new Dictionary<string, string>
         {
+            ["SIGNALFX_LOG_DIRECTORY"] = TestLogsDirectory,
             ["SIGNALFX_SERVICE_NAME"] = "exploration_tests",
             ["SIGNALFX_VERSION"] = Version
         };

tracer/build/_build/NukeExtensions/DotNetSettingsExtensions.cs
@@ -69,6 +69,12 @@ internal static partial class DotNetSettingsExtensions
     {
         return settings.SetProcessEnvironmentVariable("SIGNALFX_SERVICE_NAME", serviceName);
     }
+
+    public static T SetLogsDirectory<T>(this T settings, AbsolutePath logsDirectory)
+        where T: ToolSettings
+    {
+        return settings.SetProcessEnvironmentVariable("SIGNALFX_LOG_DIRECTORY", logsDirectory);
+    }
     
     public static T SetProcessEnvironmentVariables<T>(this T settings, IEnumerable<KeyValuePair<string, string>> variables)
         where T: ToolSettings