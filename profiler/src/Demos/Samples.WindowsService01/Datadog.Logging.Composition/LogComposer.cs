DataDog/dd-trace-dotnet

[Profiler] Rename tests apps with "Samples." prefix instead of "Datadog.Demos." (#2788)
* Rename tests apps with "Samples." prefix instead of "Datadog.Demos." to be able to test Only My Code
and be consistent with Tracer

profiler/src/Demos/Samples.WindowsService01/Datadog.Logging.Composition/LogComposer.cs
@@ -1,0 +1,120 @@
+// <auto-generated />
+// This .CS file is automatically generated. If you modify its contents, your changes will be overwritten.
+// Modify the respective T4 templates if changes are required.
+
+// <auto-generated />
+// ----------- ----------- ----------- ----------- -----------
+// The source code below is included via a T4 template.
+// The template calling must specify the value of the <c>NamespacesAndMonikersOfLogsToCompose</c> meta-variable.
+// ----------- ----------- ----------- ----------- -----------
+
+using System;
+using System.Collections.Generic;
+
+namespace Datadog.Logging.Composition
+{
+    /// <summary>
+    /// Collects data from many Log-sources and sends it to the specified Log Sink.
+    /// This class has been generated using a T4 template. It covers the following logging components:
+    ///   1) Logger type:               "Samples.WindowsService01.Log"
+    ///      Logging component moniker: "Samples.WindowsService01"
+    ///
+    /// TOTAL: 1 loggers.
+    /// </summary>
+    internal static class LogComposer
+    {
+        private const string IsDebugLoggingEnabledEnvVarName = "DD_TRACE_DEBUG";
+
+        private static bool s_isDebugLoggingEnabled = true;
+
+        public static bool IsDebugLoggingEnabled
+        {
+            get
+            {
+                return s_isDebugLoggingEnabled;
+            }
+
+            set
+            {
+                s_isDebugLoggingEnabled = value;
+                {
+                    global::Samples.WindowsService01.Log.Configure.DebugLoggingEnabled(s_isDebugLoggingEnabled);
+                }
+            }
+        }
+
+        public static void SetDebugLoggingEnabledBasedOnEnvironment()
+        {
+            bool envSetting = GetDebugLoggingEnabledEnvironmentSetting();
+            IsDebugLoggingEnabled = envSetting;
+        }
+
+        public static void RedirectLogs(ILogSink logSink, out IReadOnlyDictionary<Type, LogSourceNameCompositionLogSink> redirectionLogSinks)
+        {
+            var redirectionSinks = new Dictionary<Type, LogSourceNameCompositionLogSink>(capacity: 2);
+
+            {
+                Type loggerType = typeof(global::Samples.WindowsService01.Log);
+                const string logComponentGroupMoniker = "Samples.WindowsService01";
+
+                if (logSink == null)
+                {
+                    redirectionSinks[loggerType] = null;
+                    global::Samples.WindowsService01.Log.Configure.EventHandlers.Error(null);
+                    global::Samples.WindowsService01.Log.Configure.EventHandlers.Info(null);
+                    global::Samples.WindowsService01.Log.Configure.EventHandlers.Debug(null);
+                }
+                else
+                {
+                    var redirectionLogSink = new LogSourceNameCompositionLogSink(logComponentGroupMoniker, logSink);
+                    var logToSinkAdapter = new LogEventHandlersToLogSinkAdapter(redirectionLogSink);
+                    redirectionSinks[loggerType] = redirectionLogSink;
+                    global::Samples.WindowsService01.Log.Configure.EventHandlers.Error(logToSinkAdapter.Error);
+                    global::Samples.WindowsService01.Log.Configure.EventHandlers.Info(logToSinkAdapter.Info);
+                    global::Samples.WindowsService01.Log.Configure.EventHandlers.Debug(logToSinkAdapter.Debug);
+                }
+
+                Samples.WindowsService01.Log.Configure.DebugLoggingEnabled(IsDebugLoggingEnabled);
+            }
+
+            redirectionLogSinks = redirectionSinks;
+        }
+
+        private static bool GetDebugLoggingEnabledEnvironmentSetting()
+        {
+            // Unless the debug log is explicitly disabled, we assume that it is enabled.
+            try
+            {
+                string IsDebugLoggingEnabledEnvVarValue = ReadEnvironmentVariable(IsDebugLoggingEnabledEnvVarName);
+
+                if (IsDebugLoggingEnabledEnvVarValue != null)
+                {
+                    if (IsDebugLoggingEnabledEnvVarValue.Equals("false", System.StringComparison.OrdinalIgnoreCase)
+                            || IsDebugLoggingEnabledEnvVarValue.Equals("no", System.StringComparison.OrdinalIgnoreCase)
+                            || IsDebugLoggingEnabledEnvVarValue.Equals("n", System.StringComparison.OrdinalIgnoreCase)
+                            || IsDebugLoggingEnabledEnvVarValue.Equals("f", System.StringComparison.OrdinalIgnoreCase)
+                            || IsDebugLoggingEnabledEnvVarValue.Equals("0", System.StringComparison.OrdinalIgnoreCase))
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch
+            { }
+
+            return true;
+        }
+
+        private static string ReadEnvironmentVariable(string envVarName)
+        {
+            try
+            {
+                return Environment.GetEnvironmentVariable(envVarName);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}

signalfx/signalfx-dotnet-tracing

profiler/src/Demos/Samples.WindowsService01/Datadog.Logging.Composition/LogComposer.cs
@@ -1,0 +1,120 @@
+// <auto-generated />
+// This .CS file is automatically generated. If you modify its contents, your changes will be overwritten.
+// Modify the respective T4 templates if changes are required.
+
+// <auto-generated />
+// ----------- ----------- ----------- ----------- -----------
+// The source code below is included via a T4 template.
+// The template calling must specify the value of the <c>NamespacesAndMonikersOfLogsToCompose</c> meta-variable.
+// ----------- ----------- ----------- ----------- -----------
+
+using System;
+using System.Collections.Generic;
+
+namespace Datadog.Logging.Composition
+{
+    /// <summary>
+    /// Collects data from many Log-sources and sends it to the specified Log Sink.
+    /// This class has been generated using a T4 template. It covers the following logging components:
+    ///   1) Logger type:               "Samples.WindowsService01.Log"
+    ///      Logging component moniker: "Samples.WindowsService01"
+    ///
+    /// TOTAL: 1 loggers.
+    /// </summary>
+    internal static class LogComposer
+    {
+        private const string IsDebugLoggingEnabledEnvVarName = "SIGNALFX_TRACE_DEBUG";
+
+        private static bool s_isDebugLoggingEnabled = true;
+
+        public static bool IsDebugLoggingEnabled
+        {
+            get
+            {
+                return s_isDebugLoggingEnabled;
+            }
+
+            set
+            {
+                s_isDebugLoggingEnabled = value;
+                {
+                    global::Samples.WindowsService01.Log.Configure.DebugLoggingEnabled(s_isDebugLoggingEnabled);
+                }
+            }
+        }
+
+        public static void SetDebugLoggingEnabledBasedOnEnvironment()
+        {
+            bool envSetting = GetDebugLoggingEnabledEnvironmentSetting();
+            IsDebugLoggingEnabled = envSetting;
+        }
+
+        public static void RedirectLogs(ILogSink logSink, out IReadOnlyDictionary<Type, LogSourceNameCompositionLogSink> redirectionLogSinks)
+        {
+            var redirectionSinks = new Dictionary<Type, LogSourceNameCompositionLogSink>(capacity: 2);
+
+            {
+                Type loggerType = typeof(global::Samples.WindowsService01.Log);
+                const string logComponentGroupMoniker = "Samples.WindowsService01";
+
+                if (logSink == null)
+                {
+                    redirectionSinks[loggerType] = null;
+                    global::Samples.WindowsService01.Log.Configure.EventHandlers.Error(null);
+                    global::Samples.WindowsService01.Log.Configure.EventHandlers.Info(null);
+                    global::Samples.WindowsService01.Log.Configure.EventHandlers.Debug(null);
+                }
+                else
+                {
+                    var redirectionLogSink = new LogSourceNameCompositionLogSink(logComponentGroupMoniker, logSink);
+                    var logToSinkAdapter = new LogEventHandlersToLogSinkAdapter(redirectionLogSink);
+                    redirectionSinks[loggerType] = redirectionLogSink;
+                    global::Samples.WindowsService01.Log.Configure.EventHandlers.Error(logToSinkAdapter.Error);
+                    global::Samples.WindowsService01.Log.Configure.EventHandlers.Info(logToSinkAdapter.Info);
+                    global::Samples.WindowsService01.Log.Configure.EventHandlers.Debug(logToSinkAdapter.Debug);
+                }
+
+                Samples.WindowsService01.Log.Configure.DebugLoggingEnabled(IsDebugLoggingEnabled);
+            }
+
+            redirectionLogSinks = redirectionSinks;
+        }
+
+        private static bool GetDebugLoggingEnabledEnvironmentSetting()
+        {
+            // Unless the debug log is explicitly disabled, we assume that it is enabled.
+            try
+            {
+                string IsDebugLoggingEnabledEnvVarValue = ReadEnvironmentVariable(IsDebugLoggingEnabledEnvVarName);
+
+                if (IsDebugLoggingEnabledEnvVarValue != null)
+                {
+                    if (IsDebugLoggingEnabledEnvVarValue.Equals("false", System.StringComparison.OrdinalIgnoreCase)
+                            || IsDebugLoggingEnabledEnvVarValue.Equals("no", System.StringComparison.OrdinalIgnoreCase)
+                            || IsDebugLoggingEnabledEnvVarValue.Equals("n", System.StringComparison.OrdinalIgnoreCase)
+                            || IsDebugLoggingEnabledEnvVarValue.Equals("f", System.StringComparison.OrdinalIgnoreCase)
+                            || IsDebugLoggingEnabledEnvVarValue.Equals("0", System.StringComparison.OrdinalIgnoreCase))
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch
+            { }
+
+            return true;
+        }
+
+        private static string ReadEnvironmentVariable(string envVarName)
+        {
+            try
+            {
+                return Environment.GetEnvironmentVariable(envVarName);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}