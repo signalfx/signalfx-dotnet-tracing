DataDog/dd-trace-dotnet

Add support for UDS to telemetry transport (#2617)
* Add XML docs around using http://localhost for base endpoint with UDS/Named pipes

* Add explicit configuration key for enabling/disabling agentless telemetry

* Refactor TelemetrySettings so they are always in a consistent state

i.e. discriminated union of agentless settings or agent settings

* Update Telemetry transport to reuse IApiRequestFactory

Includes adding support for named pipes and UDS

* Build in support for telemetry testing into `MockTracerAgent`

When we're sending to the "agent" endpoint, we need to reuse the same Socket/HttpListener for both traces and telemetry, so we need to incorporate the `MockTelemetryAgent` functionality into the `MockTracerAgent`, and split based on path.

This required adding support for parsing the URL requested from the HTTP request in `MockHttpParser`.

* Fix TraceAnnotationsVersionMismatchNewerNuGetTests

This test currently uses the _old_ env vars for configuring telemetry, so need to use those
We can't use the MockAgent-based telemetry because the older nuget doesn't support agent-based telemetry, only agentless

* PR Feedback

* Set Connection: Close on responses to try and fix flakiness in UDS

minor: switch to StringBuilder

tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/Helpers/TelemetryHelper.cs
@@ -1,9 +1,8 @@
-// <copyright file="TelemetryHelper.cs" company="Datadog">
+﻿// <copyright file="TelemetryHelper.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-using System.Collections.Generic;
 using System.Linq;
 using Datadog.Trace.Configuration;
 using Datadog.Trace.Telemetry;
@@ -20,8 +19,11 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             var telemetry = new MockTelemetryAgent<TelemetryData>(telemetryPort);
 
             helper.SetEnvironmentVariable("DD_INSTRUMENTATION_TELEMETRY_ENABLED", "true");
+            helper.SetEnvironmentVariable("DD_INSTRUMENTATION_TELEMETRY_AGENTLESS_ENABLED", "true");
+            helper.SetEnvironmentVariable("DD_INSTRUMENTATION_TELEMETRY_URL", $"http://localhost:{telemetry.Port}");
+            // removed, but necessary for some version conflict tests (e.g. TraceAnnotationsVersionMismatchNewerNuGetTests)
             helper.SetEnvironmentVariable("DD_TRACE_TELEMETRY_URL", $"http://localhost:{telemetry.Port}");
-            // add an api key to force using the custom url
+            // API key is required when using the custom url
             helper.SetEnvironmentVariable("DD_API_KEY", "INVALID_KEY_FOR_TESTS");
             return telemetry;
         }
@@ -36,12 +38,79 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             return telemetry.AssertIntegration(integrationId, enabled: false, autoEnabled: true);
         }
 
+        public static TelemetryData AssertIntegrationEnabled(this MockTracerAgent mockAgent, IntegrationId integrationId)
+        {
+            return mockAgent.AssertIntegration(integrationId, enabled: true, autoEnabled: true);
+        }
+
+        public static TelemetryData AssertIntegrationDisabled(this MockTracerAgent mockAgent, IntegrationId integrationId)
+        {
+            return mockAgent.AssertIntegration(integrationId, enabled: false, autoEnabled: true);
+        }
+
+        public static TelemetryData AssertIntegration(this MockTracerAgent mockAgent, IntegrationId integrationId, bool enabled, bool? autoEnabled)
+        {
+            mockAgent.WaitForLatestTelemetry(x => ((TelemetryData)x).RequestType == TelemetryRequestTypes.AppClosing);
+
+            var allData = mockAgent.Telemetry.Cast<TelemetryData>().ToArray();
+            return AssertIntegration(allData, integrationId, enabled, autoEnabled);
+        }
+
         public static TelemetryData AssertIntegration(this MockTelemetryAgent<TelemetryData> telemetry, IntegrationId integrationId, bool enabled, bool? autoEnabled)
         {
             telemetry.WaitForLatestTelemetry(x => x.RequestType == TelemetryRequestTypes.AppClosing);
 
             var allData = telemetry.Telemetry.ToArray();
+            return AssertIntegration(allData, integrationId, enabled, autoEnabled);
+        }
 
+        public static TelemetryData AssertConfiguration(this MockTracerAgent mockAgent, string key)
+        {
+            mockAgent.WaitForLatestTelemetry(x => ((TelemetryData)x).RequestType == TelemetryRequestTypes.AppStarted);
+
+            var allData = mockAgent.Telemetry.Cast<TelemetryData>().ToArray();
+            return AssertConfiguration(allData, key);
+        }
+
+        public static TelemetryData AssertConfiguration(this MockTelemetryAgent<TelemetryData> telemetry, string key, string value)
+        {
+            telemetry.WaitForLatestTelemetry(x => x.RequestType == TelemetryRequestTypes.AppStarted);
+
+            var allData = telemetry.Telemetry.ToArray();
+            return AssertConfiguration(allData, key, value);
+        }
+
+        public static TelemetryData AssertConfiguration(this MockTelemetryAgent<TelemetryData> telemetry, string key) => telemetry.AssertConfiguration(key, value: null);
+
+        private static TelemetryData AssertConfiguration(TelemetryData[] allData, string key, string value = null)
+        {
+            var (latestConfigurationData, configurationPayload) =
+                allData
+                   .Where(x => x.RequestType == TelemetryRequestTypes.AppStarted)
+                   .OrderByDescending(x => x.SeqId)
+                   .Select(
+                        data =>
+                        {
+                            var configuration = ((AppStartedPayload)data.Payload).Configuration;
+                            return (data, configuration);
+                        })
+                   .FirstOrDefault(x => x.configuration is not null);
+
+            latestConfigurationData.Should().NotBeNull();
+            configurationPayload.Should().NotBeNull();
+
+            var config = configurationPayload.Should().ContainSingle(telemetryValue => telemetryValue.Name == key).Subject;
+            config.Should().NotBeNull();
+            if (value is not null)
+            {
+                config.Value.Should().Be(value);
+            }
+
+            return latestConfigurationData;
+        }
+
+        private static TelemetryData AssertIntegration(TelemetryData[] allData, IntegrationId integrationId, bool enabled, bool? autoEnabled)
+        {
             allData.Should().ContainSingle(x => x.RequestType == TelemetryRequestTypes.AppClosing);
 
             var (latestIntegrationsData, integrationsPayload) =
@@ -76,44 +145,6 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             integration.Error.Should().BeNullOrEmpty();
 
             return latestIntegrationsData;
-        }
-
-        public static TelemetryData AssertConfiguration(this MockTelemetryAgent<TelemetryData> telemetry, string key)
-        {
-            var (latestConfigurationData, configurationPayload) = telemetry.AssertConfiguration();
-            configurationPayload.Should().ContainSingle(telemetryValue => telemetryValue.Name == key);
-            return latestConfigurationData;
-        }
-
-        public static TelemetryData AssertConfiguration(this MockTelemetryAgent<TelemetryData> telemetry, string key, string value)
-        {
-            var (latestConfigurationData, configurationPayload) = telemetry.AssertConfiguration();
-            configurationPayload.Should()
-                                .ContainSingle(telemetryValue => telemetryValue.Name == key && telemetryValue.Value.ToString() == value);
-
-            return latestConfigurationData;
-        }
-
-        private static (TelemetryData LatestConfigurationData, ICollection<TelemetryValue> ConfigurationPayload) AssertConfiguration(this MockTelemetryAgent<TelemetryData> telemetry)
-        {
-            telemetry.WaitForLatestTelemetry(x => x.RequestType == TelemetryRequestTypes.AppStarted);
-            var allData = telemetry.Telemetry.ToArray();
-            var (latestConfigurationData, configurationPayload) =
-                allData
-                   .Where(x => x.RequestType == TelemetryRequestTypes.AppStarted)
-                   .OrderByDescending(x => x.SeqId)
-                   .Select(
-                        data =>
-                        {
-                            var configuration = ((AppStartedPayload)data.Payload).Configuration;
-                            return (data, configuration);
-                        })
-                   .FirstOrDefault(x => x.configuration is not null);
-
-            latestConfigurationData.Should().NotBeNull();
-            configurationPayload.Should().NotBeNull();
-
-            return (latestConfigurationData, configurationPayload);
         }
     }
 }

tracer/test/Datadog.Trace.TestHelpers/EnvironmentHelper.cs
@@ -271,7 +271,11 @@ namespace Datadog.Trace.TestHelpers
 
             // set consistent env name (can be overwritten by custom environment variable)
             environmentVariables["DD_ENV"] = "integration_tests";
-            environmentVariables[ConfigurationKeys.Telemetry.Enabled] = "false";
+            environmentVariables[ConfigurationKeys.Telemetry.Enabled] = agent.TelemetryEnabled.ToString();
+            if (agent.TelemetryEnabled)
+            {
+                environmentVariables[ConfigurationKeys.Telemetry.AgentlessEnabled] = "0";
+            }
 
             // Don't attach the profiler to these processes
             environmentVariables["DD_PROFILER_EXCLUDE_PROCESSES"] =
@@ -510,7 +514,7 @@ namespace Datadog.Trace.TestHelpers
 #endif
         }
 
-        public MockTracerAgent GetMockAgent(bool useStatsD = false, int? fixedPort = null)
+        public MockTracerAgent GetMockAgent(bool useStatsD = false, int? fixedPort = null, bool useTelemetry = false)
         {
             MockTracerAgent agent = null;
 
@@ -521,31 +525,31 @@ namespace Datadog.Trace.TestHelpers
 #if NETCOREAPP3_1_OR_GREATER
                 var tracesUdsPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
                 var metricsUdsPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
-                agent = new MockTracerAgent(new UnixDomainSocketConfig(tracesUdsPath, metricsUdsPath) { UseDogstatsD = useStatsD });
+                agent = new MockTracerAgent(new UnixDomainSocketConfig(tracesUdsPath, metricsUdsPath) { UseDogstatsD = useStatsD, UseTelemetry = useTelemetry });
 #else
             throw new NotSupportedException("UDS is not supported in non-netcore applications or < .NET Core 3.1 ");
 #endif
             }
             else if (TransportType == TestTransports.WindowsNamedPipe)
             {
-                agent = new MockTracerAgent(new WindowsPipesConfig($"trace-{Guid.NewGuid()}", $"metrics-{Guid.NewGuid()}") { UseDogstatsD = useStatsD });
+                agent = new MockTracerAgent(new WindowsPipesConfig($"trace-{Guid.NewGuid()}", $"metrics-{Guid.NewGuid()}") { UseDogstatsD = useStatsD, UseTelemetry = useTelemetry });
             }
             else
             {
                 // Default
                 var agentPort = fixedPort ?? TcpPortProvider.GetOpenPort();
-                agent = new MockTracerAgent(agentPort, useStatsd: useStatsD);
+                agent = new MockTracerAgent(agentPort, useStatsd: useStatsD, useTelemetry: useTelemetry);
             }
 #else
             if (TransportType == TestTransports.WindowsNamedPipe)
             {
-                agent = new MockTracerAgent(new WindowsPipesConfig($"trace-{Guid.NewGuid()}", $"metrics-{Guid.NewGuid()}"));
+                agent = new MockTracerAgent(new WindowsPipesConfig($"trace-{Guid.NewGuid()}", $"metrics-{Guid.NewGuid()}") { UseDogstatsD = useStatsD, UseTelemetry = useTelemetry });
             }
             else
             {
                 // Default
                 var agentPort = fixedPort ?? TcpPortProvider.GetOpenPort();
-                agent = new MockTracerAgent(agentPort, useStatsd: useStatsD);
+                agent = new MockTracerAgent(agentPort, useStatsd: useStatsD, useTelemetry: useTelemetry);
             }
 #endif
 

signalfx/signalfx-dotnet-tracing

tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/Helpers/TelemetryHelper.cs
@@ -1,4 +1,4 @@
-// <copyright file="TelemetryHelper.cs" company="Datadog">
+﻿// <copyright file="TelemetryHelper.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
@@ -22,8 +22,11 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             var telemetry = new MockTelemetryAgent<TelemetryData>(telemetryPort);
 
             helper.SetEnvironmentVariable("SIGNALFX_INSTRUMENTATION_TELEMETRY_ENABLED", "true");
+            helper.SetEnvironmentVariable("SIGNALFX_INSTRUMENTATION_TELEMETRY_AGENTLESS_ENABLED", "true");
+            helper.SetEnvironmentVariable("SIGNALFX_INSTRUMENTATION_TELEMETRY_URL", $"http://localhost:{telemetry.Port}");
+            // removed, but necessary for some version conflict tests (e.g. TraceAnnotationsVersionMismatchNewerNuGetTests)
             helper.SetEnvironmentVariable("SIGNALFX_TRACE_TELEMETRY_URL", $"http://localhost:{telemetry.Port}");
-            // add an api key to force using the custom url
+            // API key is required when using the custom url
             helper.SetEnvironmentVariable("SIGNALFX_API_KEY", "INVALID_KEY_FOR_TESTS");
             return telemetry;
         }
@@ -38,12 +41,79 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             return telemetry.AssertIntegration(integrationId, enabled: false, autoEnabled: true);
         }
 
+        public static TelemetryData AssertIntegrationEnabled(this MockTracerAgent mockAgent, IntegrationId integrationId)
+        {
+            return mockAgent.AssertIntegration(integrationId, enabled: true, autoEnabled: true);
+        }
+
+        public static TelemetryData AssertIntegrationDisabled(this MockTracerAgent mockAgent, IntegrationId integrationId)
+        {
+            return mockAgent.AssertIntegration(integrationId, enabled: false, autoEnabled: true);
+        }
+
+        public static TelemetryData AssertIntegration(this MockTracerAgent mockAgent, IntegrationId integrationId, bool enabled, bool? autoEnabled)
+        {
+            mockAgent.WaitForLatestTelemetry(x => ((TelemetryData)x).RequestType == TelemetryRequestTypes.AppClosing);
+
+            var allData = mockAgent.Telemetry.Cast<TelemetryData>().ToArray();
+            return AssertIntegration(allData, integrationId, enabled, autoEnabled);
+        }
+
         public static TelemetryData AssertIntegration(this MockTelemetryAgent<TelemetryData> telemetry, IntegrationId integrationId, bool enabled, bool? autoEnabled)
         {
             telemetry.WaitForLatestTelemetry(x => x.RequestType == TelemetryRequestTypes.AppClosing);
 
             var allData = telemetry.Telemetry.ToArray();
+            return AssertIntegration(allData, integrationId, enabled, autoEnabled);
+        }
 
+        public static TelemetryData AssertConfiguration(this MockTracerAgent mockAgent, string key)
+        {
+            mockAgent.WaitForLatestTelemetry(x => ((TelemetryData)x).RequestType == TelemetryRequestTypes.AppStarted);
+
+            var allData = mockAgent.Telemetry.Cast<TelemetryData>().ToArray();
+            return AssertConfiguration(allData, key);
+        }
+
+        public static TelemetryData AssertConfiguration(this MockTelemetryAgent<TelemetryData> telemetry, string key, string value)
+        {
+            telemetry.WaitForLatestTelemetry(x => x.RequestType == TelemetryRequestTypes.AppStarted);
+
+            var allData = telemetry.Telemetry.ToArray();
+            return AssertConfiguration(allData, key, value);
+        }
+
+        public static TelemetryData AssertConfiguration(this MockTelemetryAgent<TelemetryData> telemetry, string key) => telemetry.AssertConfiguration(key, value: null);
+
+        private static TelemetryData AssertConfiguration(TelemetryData[] allData, string key, string value = null)
+        {
+            var (latestConfigurationData, configurationPayload) =
+                allData
+                   .Where(x => x.RequestType == TelemetryRequestTypes.AppStarted)
+                   .OrderByDescending(x => x.SeqId)
+                   .Select(
+                        data =>
+                        {
+                            var configuration = ((AppStartedPayload)data.Payload).Configuration;
+                            return (data, configuration);
+                        })
+                   .FirstOrDefault(x => x.configuration is not null);
+
+            latestConfigurationData.Should().NotBeNull();
+            configurationPayload.Should().NotBeNull();
+
+            var config = configurationPayload.Should().ContainSingle(telemetryValue => telemetryValue.Name == key).Subject;
+            config.Should().NotBeNull();
+            if (value is not null)
+            {
+                config.Value.Should().Be(value);
+            }
+
+            return latestConfigurationData;
+        }
+
+        private static TelemetryData AssertIntegration(TelemetryData[] allData, IntegrationId integrationId, bool enabled, bool? autoEnabled)
+        {
             allData.Should().ContainSingle(x => x.RequestType == TelemetryRequestTypes.AppClosing);
 
             var (latestIntegrationsData, integrationsPayload) =
@@ -78,44 +148,6 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             integration.Error.Should().BeNullOrEmpty();
 
             return latestIntegrationsData;
-        }
-
-        public static TelemetryData AssertConfiguration(this MockTelemetryAgent<TelemetryData> telemetry, string key)
-        {
-            var (latestConfigurationData, configurationPayload) = telemetry.AssertConfiguration();
-            configurationPayload.Should().ContainSingle(telemetryValue => telemetryValue.Name == key);
-            return latestConfigurationData;
-        }
-
-        public static TelemetryData AssertConfiguration(this MockTelemetryAgent<TelemetryData> telemetry, string key, string value)
-        {
-            var (latestConfigurationData, configurationPayload) = telemetry.AssertConfiguration();
-            configurationPayload.Should()
-                                .ContainSingle(telemetryValue => telemetryValue.Name == key && telemetryValue.Value.ToString() == value);
-
-            return latestConfigurationData;
-        }
-
-        private static (TelemetryData LatestConfigurationData, ICollection<TelemetryValue> ConfigurationPayload) AssertConfiguration(this MockTelemetryAgent<TelemetryData> telemetry)
-        {
-            telemetry.WaitForLatestTelemetry(x => x.RequestType == TelemetryRequestTypes.AppStarted);
-            var allData = telemetry.Telemetry.ToArray();
-            var (latestConfigurationData, configurationPayload) =
-                allData
-                   .Where(x => x.RequestType == TelemetryRequestTypes.AppStarted)
-                   .OrderByDescending(x => x.SeqId)
-                   .Select(
-                        data =>
-                        {
-                            var configuration = ((AppStartedPayload)data.Payload).Configuration;
-                            return (data, configuration);
-                        })
-                   .FirstOrDefault(x => x.configuration is not null);
-
-            latestConfigurationData.Should().NotBeNull();
-            configurationPayload.Should().NotBeNull();
-
-            return (latestConfigurationData, configurationPayload);
         }
     }
 }

tracer/test/Datadog.Trace.TestHelpers/EnvironmentHelper.cs
@@ -277,7 +277,7 @@ namespace Datadog.Trace.TestHelpers
 
             // set consistent env name (can be overwritten by custom environment variable)
             environmentVariables["SIGNALFX_ENV"] = "integration_tests";
-            environmentVariables[ConfigurationKeys.Telemetry.Enabled] = "false";
+            environmentVariables[ConfigurationKeys.Telemetry.Enabled] = "false"
 
             // Don't attach the profiler to these processes
             environmentVariables["SIGNALFX_PROFILER_EXCLUDE_PROCESSES"] =
@@ -501,7 +501,7 @@ namespace Datadog.Trace.TestHelpers
             TransportType = TestTransports.Tcp;
         }
 
-        public MockTracerAgent GetMockAgent(bool useStatsD = false, int? fixedPort = null)
+        public MockTracerAgent GetMockAgent(bool useStatsD = false, int? fixedPort = null, bool useTelemetry = false)
         {
             MockTracerAgent agent = null;
 
@@ -515,18 +515,18 @@ namespace Datadog.Trace.TestHelpers
             {
                 // Default
                 var agentPort = fixedPort ?? TcpPortProvider.GetOpenPort();
-                agent = new MockTracerAgent(agentPort, useSfxMetrics: useStatsD);
+                agent = new MockTracerAgent(agentPort, useSfxMetrics: useStatsD, useTelemetry: useTelemetry);
             }
 #else
             if (TransportType == TestTransports.WindowsNamedPipe)
             {
-                agent = new MockTracerAgent(new WindowsPipesConfig($"trace-{Guid.NewGuid()}", $"metrics-{Guid.NewGuid()}"));
+                agent = new MockTracerAgent(new WindowsPipesConfig($"trace-{Guid.NewGuid()}", $"metrics-{Guid.NewGuid()}") { UseDogstatsD = useStatsD, UseTelemetry = useTelemetry });
             }
             else
             {
                 // Default
                 var agentPort = fixedPort ?? TcpPortProvider.GetOpenPort();
-                agent = new MockTracerAgent(agentPort, useSfxMetrics: useStatsD);
+                agent = new MockTracerAgent(agentPort, useSfxMetrics: useStatsD, useTelemetry: useTelemetry);
             }
 #endif
 