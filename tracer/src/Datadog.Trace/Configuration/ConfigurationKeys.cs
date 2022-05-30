DataDog/dd-trace-dotnet

[CIApp] - Per-test Code Coverage (#2739)
* Initial Datadog.Trace.Ci.Coverage code.

* Add Datadog.Trace.Coverage.collector project.

* Initial collector code.

* fix package versions

* fix build

* Changes.

* Add coverage collector reference.

* Add coverage collector to the runner.

* Fixes.

* Fix public api

* Changes.

* Add coverage on test spans for testing purposes.

* Enable relative paths

* Handle different CorLib version.

* Collector logger.

* Changes.

* changes

* Add pdb streams to the collector.

* changes

* Add configuration settings.

* Add signed assemblies support.

* Fix support for StrongName key

* Add wrap arguments containing spaces in CLI

* fixes.

* Re-use MakeRelativePathFromSourceRoot method.

* Fix Tests

* change comments.

* Update payload specs.

* Update for net6.0 support

* Remove embedded resources and change them to copy the assemblies directly from the tracer home folder.

* Adds a range validator to avoid an overflow.

* Do backup into another folder.

* Changes and fixes.

* changes.

* Add FixDoubleQuotes unit test

* Adds VS 2022 support.

* Add rewriter snapshot tests.

* Add missing symbols file

* Copy test assembly to a temporal folder.

* Addressing comments.

* Change algorithm to avoid two enumerations and to compare using the fullname of the type.

* Handle .NET Core releases in a more general way.

* Reduce CloneInstruction method code.

* Throw if the instruction offset is not found, so the assembly is skipped.

* Address PR comments.

* Addressing PR comments.

* Change the order of the instructions injection.

* Add checks in the deps.json rewriter.

* Add comments about the rewriting process.

* Addressing PR comments.

* Rename the collector namespace to Collector.

* Fix tests

* Add more test cases.

tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -304,13 +304,13 @@ namespace Datadog.Trace.Configuration
         {
             /// <summary>
             /// Configuration key for enabling or disabling CI Visibility.
-            /// Default is value is false (disabled).
+            /// Default value is false (disabled).
             /// </summary>
             public const string Enabled = "DD_CIVISIBILITY_ENABLED";
 
             /// <summary>
             /// Configuration key for enabling or disabling Agentless in CI Visibility.
-            /// Default is value is false (disabled).
+            /// Default value is false (disabled).
             /// </summary>
             public const string AgentlessEnabled = "DD_CIVISIBILITY_AGENTLESS_ENABLED";
 
@@ -321,9 +321,20 @@ namespace Datadog.Trace.Configuration
 
             /// <summary>
             /// Configuration key for enabling or disabling Logs direct submission.
-            /// Default is value is false (disabled).
+            /// Default value is false (disabled).
             /// </summary>
             public const string Logs = "DD_CIVISIBILITY_LOGS_ENABLED";
+
+            /// <summary>
+            /// Configuration key for enabling or disabling Code Coverage in CI Visibility.
+            /// Default value is false (disabled).
+            /// </summary>
+            public const string CodeCoverage = "DD_CIVISIBILITY_CODE_COVERAGE_ENABLED";
+
+            /// <summary>
+            /// Configuration key for re-signing assemblies after the Code Coverage modification.
+            /// </summary>
+            public const string CodeCoverageSnkFile = "DD_CIVISIBILITY_CODE_COVERAGE_SNK_FILEPATH";
         }
 
         /// <summary>

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -397,13 +397,13 @@ namespace Datadog.Trace.Configuration
         {
             /// <summary>
             /// Configuration key for enabling or disabling CI Visibility.
-            /// Default is value is false (disabled).
+            /// Default value is false (disabled).
             /// </summary>
             public const string Enabled = "SIGNALFX_CIVISIBILITY_ENABLED";
 
             /// <summary>
             /// Configuration key for enabling or disabling Agentless in CI Visibility.
-            /// Default is value is false (disabled).
+            /// Default value is false (disabled).
             /// </summary>
             public const string AgentlessEnabled = "SIGNALFX_CIVISIBILITY_AGENTLESS_ENABLED";
 
@@ -414,9 +414,20 @@ namespace Datadog.Trace.Configuration
 
             /// <summary>
             /// Configuration key for enabling or disabling Logs direct submission.
-            /// Default is value is false (disabled).
+            /// Default value is false (disabled).
             /// </summary>
             public const string Logs = "SIGNALFX_CIVISIBILITY_LOGS_ENABLED";
+
+            /// <summary>
+            /// Configuration key for enabling or disabling Code Coverage in CI Visibility.
+            /// Default value is false (disabled).
+            /// </summary>
+            public const string CodeCoverage = "SIGNALFX_CIVISIBILITY_CODE_COVERAGE_ENABLED";
+
+            /// <summary>
+            /// Configuration key for re-signing assemblies after the Code Coverage modification.
+            /// </summary>
+            public const string CodeCoverageSnkFile = "SIGNALFX_CIVISIBILITY_CODE_COVERAGE_SNK_FILEPATH";
         }
 
         /// <summary>