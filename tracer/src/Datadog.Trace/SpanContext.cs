DataDog/dd-trace-dotnet

remove unused `ITraceContext` (#2225)
* remove unused code in SpanExtensionsTests

* replace last uses of ITraceContext with TraceContext

* remove unused ITraceContext

* missed a spot

tracer/src/Datadog.Trace/SpanContext.cs
@@ -59,7 +59,7 @@ namespace Datadog.Trace
         /// <param name="serviceName">The service name to propagate to child spans.</param>
         /// <param name="traceId">Override the trace id if there's no parent.</param>
         /// <param name="spanId">The propagated span id.</param>
-        internal SpanContext(ISpanContext parent, ITraceContext traceContext, string serviceName, ulong? traceId = null, ulong? spanId = null)
+        internal SpanContext(ISpanContext parent, TraceContext traceContext, string serviceName, ulong? traceId = null, ulong? spanId = null)
             : this(parent?.TraceId ?? traceId, serviceName)
         {
             SpanId = spanId ?? SpanIdGenerator.ThreadInstance.CreateNew();
@@ -114,7 +114,7 @@ namespace Datadog.Trace
         /// Gets the trace context.
         /// Returns null for contexts created from incoming propagated context.
         /// </summary>
-        internal ITraceContext TraceContext { get; }
+        internal TraceContext TraceContext { get; }
 
         /// <summary>
         /// Gets the sampling priority for contexts created from incoming propagated context.

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/SpanContext.cs
@@ -120,7 +120,7 @@ namespace Datadog.Trace
         /// <param name="serviceName">The service name to propagate to child spans.</param>
         /// <param name="traceId">Override the trace id if there's no parent.</param>
         /// <param name="spanId">The propagated span id.</param>
-        internal SpanContext(ISpanContext parent, ITraceContext traceContext, string serviceName, TraceId? traceId = null, ulong? spanId = null)
+        internal SpanContext(ISpanContext parent, TraceContext traceContext, string serviceName, TraceId? traceId = null, ulong? spanId = null)
             : this(parent?.TraceId ?? traceId, serviceName)
         {
             SpanId = spanId ?? SpanIdGenerator.ThreadInstance.CreateNew();
@@ -186,7 +186,7 @@ namespace Datadog.Trace
         /// Gets the trace context.
         /// Returns null for contexts created from incoming propagated context.
         /// </summary>
-        internal ITraceContext TraceContext { get; }
+        internal TraceContext TraceContext { get; }
 
         /// <summary>
         /// Gets the sampling priority for contexts created from incoming propagated context.