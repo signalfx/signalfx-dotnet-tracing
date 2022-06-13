DataDog/dd-trace-dotnet

Activity Compatibility Layer (#2446)
* Initial approach to an activity listener using ducktype.

* Start changes for 4.4.x support.

* Initial code for support 4.4.x

* Initial implementation.

* changes

* Changes.

* Changes.

* changes.

* Allow to call Initialize multiple times if a DiagnosticSource cannot be bound.

* Create Spans from Activities.

* fixes.

* changes.

* Changes.

* Add more sources to ignore.

* updates and more try/catch

* Improve resilience on DiagnosticSource incomplete events.

* remove the default ActivitySource from the ignore list.

* Add IgnoreOperationNames list

* Add support for handlers.

* Add retries support for late binding scenarios.

* Adds GetCurrentActivity method.

* Change log levels.

* Improve the IgnoreOperationNames option.

* Refactor

* Extracting tags from activities in the AspNetCore integration.

* Fixes and logs.

* Add check in ActivityStopped

* expose activity's internal traceid and spanid

* Changes in traceId and spanId

* handles traceId and spanId

* fix observer

* fixes.

* temporal workaround

* Remove intermediary span.

* fix stupid bug

* fixes.

* Update branch

* Feature flag implementation

* Fix merge

* Fixes and Tests

* Fix tests

* add tags to tests

* fixes.

* Fix implementation and tests.

* Skip netcoreapp2.1 ARM64 tests

* Add CancellationToken to initialization.

* Apply suggestions from code review

Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

* Handle activity type nullability from BindAndCreateDelegates method.

* Update tracer/src/Datadog.Trace/Activity/ActivityListener.cs

Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

* Ensure DefaultIdFormat for all DiagnosticSource versions.

* Fix possible race condition.

* Transform ActivityListenerDelegatesBuilder to an static class with pure functions.

* Update tracer/src/Datadog.Trace/Activity/ActivityListene

tracer/src/Datadog.Trace/SpanContext.cs
@@ -102,7 +102,9 @@ namespace Datadog.Trace
         /// <param name="serviceName">The service name to propagate to child spans.</param>
         /// <param name="traceId">Override the trace id if there's no parent.</param>
         /// <param name="spanId">The propagated span id.</param>
-        internal SpanContext(ISpanContext parent, TraceContext traceContext, string serviceName, ulong? traceId = null, ulong? spanId = null)
+        /// <param name="rawTraceId">Raw trace id value</param>
+        /// <param name="rawSpanId">Raw span id value</param>
+        internal SpanContext(ISpanContext parent, TraceContext traceContext, string serviceName, ulong? traceId = null, ulong? spanId = null, string rawTraceId = null, string rawSpanId = null)
             : this(parent?.TraceId ?? traceId, serviceName)
         {
             SpanId = spanId ?? SpanIdGenerator.ThreadInstance.CreateNew();
@@ -111,7 +113,14 @@ namespace Datadog.Trace
             if (parent is SpanContext spanContext)
             {
                 Origin = spanContext.Origin;
+                RawTraceId = spanContext.RawTraceId ?? rawTraceId;
             }
+            else
+            {
+                RawTraceId = rawTraceId;
+            }
+
+            RawSpanId = rawSpanId;
         }
 
         private SpanContext(ulong? traceId, string serviceName)

tracer/src/Datadog.Trace/Tracer.cs
@@ -333,7 +333,7 @@ namespace Datadog.Trace
             return TracerManager.ScopeManager.Activate(span, finishOnClose);
         }
 
-        internal SpanContext CreateSpanContext(ISpanContext parent = null, string serviceName = null, ulong? traceId = null, ulong? spanId = null)
+        internal SpanContext CreateSpanContext(ISpanContext parent = null, string serviceName = null, ulong? traceId = null, ulong? spanId = null, string rawTraceId = null, string rawSpanId = null)
         {
             // null parent means use the currently active span
             parent ??= DistributedTracer.Instance.GetSpanContext() ?? TracerManager.ScopeManager.Active?.Span?.Context;
@@ -356,10 +356,21 @@ namespace Datadog.Trace
             {
                 traceContext = new TraceContext(this);
                 traceContext.SetSamplingPriority(DistributedTracer.Instance.GetSamplingPriority());
+
+                if (traceId == null)
+                {
+                    var activity = Activity.ActivityListener.GetCurrentActivity();
+                    if (activity is Activity.DuckTypes.IW3CActivity w3CActivity)
+                    {
+                        // If there's an existing activity we use the same traceId (converted).
+                        rawTraceId = w3CActivity.TraceId;
+                        traceId = Convert.ToUInt64(w3CActivity.TraceId.Substring(16), 16);
+                    }
+                }
             }
 
             var finalServiceName = serviceName ?? DefaultServiceName;
-            return new SpanContext(parent, traceContext, finalServiceName, traceId: traceId, spanId: spanId);
+            return new SpanContext(parent, traceContext, finalServiceName, traceId: traceId, spanId: spanId, rawTraceId: rawTraceId, rawSpanId: rawSpanId);
         }
 
         internal Scope StartActiveInternal(string operationName, ISpanContext parent = null, string serviceName = null, DateTimeOffset? startTime = null, bool finishOnClose = true, ITags tags = null)
@@ -368,9 +379,9 @@ namespace Datadog.Trace
             return TracerManager.ScopeManager.Activate(span, finishOnClose);
         }
 
-        internal Span StartSpan(string operationName, ITags tags = null, ISpanContext parent = null, string serviceName = null, DateTimeOffset? startTime = null, ulong? traceId = null, ulong? spanId = null, bool addToTraceContext = true)
+        internal Span StartSpan(string operationName, ITags tags = null, ISpanContext parent = null, string serviceName = null, DateTimeOffset? startTime = null, ulong? traceId = null, ulong? spanId = null, string rawTraceId = null, string rawSpanId = null, bool addToTraceContext = true)
         {
-            var spanContext = CreateSpanContext(parent, serviceName, traceId, spanId);
+            var spanContext = CreateSpanContext(parent, serviceName, traceId, spanId, rawTraceId, rawSpanId);
 
             var span = new Span(spanContext, startTime, tags)
             {

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/SpanContext.cs
@@ -120,7 +120,9 @@ namespace Datadog.Trace
         /// <param name="serviceName">The service name to propagate to child spans.</param>
         /// <param name="traceId">Override the trace id if there's no parent.</param>
         /// <param name="spanId">The propagated span id.</param>
-        internal SpanContext(ISpanContext parent, TraceContext traceContext, string serviceName, TraceId? traceId = null, ulong? spanId = null)
+        /// <param name="rawTraceId">Raw trace id value</param>
+        /// <param name="rawSpanId">Raw span id value</param>
+        internal SpanContext(ISpanContext parent, TraceContext traceContext, string serviceName, TraceId? traceId = null, ulong? spanId = null, string rawTraceId = null, string rawSpanId = null)
             : this(parent?.TraceId ?? traceId, serviceName)
         {
             SpanId = spanId ?? SpanIdGenerator.ThreadInstance.CreateNew();
@@ -129,7 +131,14 @@ namespace Datadog.Trace
             if (parent is SpanContext spanContext)
             {
                 Origin = spanContext.Origin;
+                RawTraceId = spanContext.RawTraceId ?? rawTraceId;
             }
+            else
+            {
+                RawTraceId = rawTraceId;
+            }
+
+            RawSpanId = rawSpanId;
         }
 
         private SpanContext(TraceId? traceId, string serviceName)

tracer/src/Datadog.Trace/Tracer.cs
@@ -320,7 +320,7 @@ namespace Datadog.Trace
             return TracerManager.ScopeManager.Activate(span, finishOnClose);
         }
 
-        internal SpanContext CreateSpanContext(ISpanContext parent = null, string serviceName = null, TraceId? traceId = null, ulong? spanId = null)
+        internal SpanContext CreateSpanContext(ISpanContext parent = null, string serviceName = null, ulong? traceId = null, ulong? spanId = null, string rawTraceId = null, string rawSpanId = null)
         {
             // null parent means use the currently active span
             parent ??= DistributedTracer.Instance.GetSpanContext() ?? TracerManager.ScopeManager.Active?.Span?.Context;
@@ -343,10 +343,21 @@ namespace Datadog.Trace
             {
                 traceContext = new TraceContext(this);
                 traceContext.SetSamplingPriority(DistributedTracer.Instance.GetSamplingPriority());
+
+                if (traceId == null)
+                {
+                    var activity = Activity.ActivityListener.GetCurrentActivity();
+                    if (activity is Activity.DuckTypes.IW3CActivity w3CActivity)
+                    {
+                        // If there's an existing activity we use the same traceId (converted).
+                        rawTraceId = w3CActivity.TraceId;
+                        traceId = Convert.ToUInt64(w3CActivity.TraceId.Substring(16), 16);
+                    }
+                }
             }
 
             var finalServiceName = serviceName ?? DefaultServiceName;
-            return new SpanContext(parent, traceContext, finalServiceName, traceId: traceId, spanId: spanId);
+            return new SpanContext(parent, traceContext, finalServiceName, traceId: traceId, spanId: spanId, rawTraceId: rawTraceId, rawSpanId: rawSpanId);
         }
 
         internal Scope StartActiveInternal(string operationName, ISpanContext parent = null, string serviceName = null, DateTimeOffset? startTime = null, bool finishOnClose = true, ITags tags = null)
@@ -355,9 +366,9 @@ namespace Datadog.Trace
             return TracerManager.ScopeManager.Activate(span, finishOnClose);
         }
 
-        internal Span StartSpan(string operationName, ITags tags = null, ISpanContext parent = null, string serviceName = null, DateTimeOffset? startTime = null, TraceId? traceId = null, ulong? spanId = null, bool addToTraceContext = true)
+        internal Span StartSpan(string operationName, ITags tags = null, ISpanContext parent = null, string serviceName = null, DateTimeOffset? startTime = null, TraceId? traceId = null, ulong? spanId = null, string rawTraceId = null, string rawSpanId = null, bool addToTraceContext = true)
         {
-            var spanContext = CreateSpanContext(parent, serviceName, traceId, spanId);
+            var spanContext = CreateSpanContext(parent, serviceName, traceId, spanId, rawTraceId, rawSpanId);
 
             var span = new Span(spanContext, startTime, tags)
             {