DataDog/dd-trace-dotnet

Package the profiler, tracer and native loader together on Linux (#2777)
* Revisit Linux package for backward compatibility
* Run integration tests linux against already built/packaged artifacts
* Use native loader for integration tests
* Move GenerateRuntimeId in shared code
* Fix msi_and_package stage
* Rewrite PInvoke maps for the profiler
* Apply suggestions from code review

Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

tracer/src/Datadog.Trace.ClrProfiler.Native/cor_profiler.cpp
@@ -358,7 +358,7 @@ HRESULT STDMETHODCALLTYPE CorProfiler::AssemblyLoadFinished(AssemblyID assembly_
     return S_OK;
 }
 
-void CorProfiler::RewritingPInvokeMaps(const ModuleMetadata& module_metadata, const shared::WSTRING& nativemethods_type_name)
+void CorProfiler::RewritingPInvokeMaps(const ModuleMetadata& module_metadata, const shared::WSTRING& nativemethods_type_name, const shared::WSTRING& library_path)
 {
     HRESULT hr;
     const auto& metadata_import = module_metadata.metadata_import;
@@ -370,10 +370,18 @@ void CorProfiler::RewritingPInvokeMaps(const ModuleMetadata& module_metadata, co
         FindTypeDefByName(nativemethods_type_name, module_metadata.assemblyName, metadata_import, nativeMethodsTypeDef);
     if (foundType)
     {
-        // Define the actual profiler file path as a ModuleRef
-        shared::WSTRING native_profiler_file = shared::GetCurrentModuleFileName();
+        // get the native profiler file path.
+        auto native_profiler_file = library_path.empty() ? shared::GetCurrentModuleFileName() : library_path;
+
+        if (!fs::exists(native_profiler_file))
+        {
+            Logger::Warn("Unable to rewrite PInvokes. Native library not found: ", native_profiler_file);
+            return;
+        }
+
         Logger::Info("Rewriting PInvokes to native: ", native_profiler_file);
 
+        // Define the actual profiler file path as a ModuleRef
         mdModuleRef profiler_ref;
         hr = metadata_emit->DefineModuleRef(native_profiler_file.c_str(), &profiler_ref);
         if (SUCCEEDED(hr))
@@ -506,6 +514,13 @@ HRESULT STDMETHODCALLTYPE CorProfiler::ModuleLoadFinished(ModuleID module_id, HR
     }
 
     return hr;
+}
+
+bool ShouldRewriteProfilerMaps()
+{
+    auto strValue = shared::GetEnvironmentValue(WStr("DD_PROFILING_ENABLED"));
+    bool is_profiler_enabled;
+    return shared::TryParseBooleanEnvironmentValue(strValue, is_profiler_enabled) && is_profiler_enabled;
 }
 
 HRESULT CorProfiler::TryRejitModule(ModuleID module_id)
@@ -662,6 +677,12 @@ HRESULT CorProfiler::TryRejitModule(ModuleID module_id)
         RewritingPInvokeMaps(module_metadata, nonwindows_nativemethods_type);
         RewritingPInvokeMaps(module_metadata, appsec_nonwindows_nativemethods_type);
 #endif // _WIN32
+
+        if (ShouldRewriteProfilerMaps())
+        {
+            auto profiler_library_path = shared::GetEnvironmentValue(WStr("DD_INTERNAL_PROFILING_NATIVE_ENGINE_PATH"));
+            RewritingPInvokeMaps(module_metadata, profiler_nativemethods_type, profiler_library_path);
+        }
 
         if (IsVersionCompatibilityEnabled())
         {
@@ -1675,7 +1696,7 @@ HRESULT CorProfiler::EmitDistributedTracerTargetMethod(const ModuleMetadata& mod
         Logger::Warn("Error rewriting for Distributed Tracing on getting IDistributedTracer TypeDef");
         return hr;
     }
-    
+
     COR_SIGNATURE instanceSignature[16];
 
     unsigned type_buffer;
@@ -1697,7 +1718,7 @@ HRESULT CorProfiler::EmitDistributedTracerTargetMethod(const ModuleMetadata& mod
         Logger::Warn("Error rewriting for Distributed Tracing on defining get_Instance MemberRef");
         return hr;
     }
-    
+
 
     constexpr COR_SIGNATURE signature[] = {
         IMAGE_CEE_CS_CALLCONV_DEFAULT, // Calling convention
@@ -1791,7 +1812,7 @@ HRESULT CorProfiler::RewriteForDistributedTracing(const ModuleMetadata& module_m
         Logger::Info("sizeof(pulHashAlgId): ", sizeof(managed_profiler_assembly_property.pulHashAlgId));
         Logger::Info("assemblyFlags: ", managed_profiler_assembly_property.assemblyFlags);
     }
-    
+
     //
     // *** Get DistributedTracer TypeDef
     //

tracer/test/Datadog.Trace.TestHelpers/EnvironmentHelper.cs
@@ -42,10 +42,9 @@ namespace Datadog.Trace.TestHelpers
             _output = output;
             TracerHome = GetTracerHomePath();
 
-            // The Tracer is not currently utilizing the Native Loader in production. It is only being used in the Continuous Profiler beta.
-            // Because of that, we don't test it in the default pipeline.
-            bool useNativeLoader = string.Equals("true", Environment.GetEnvironmentVariable("USE_NATIVE_LOADER"), StringComparison.InvariantCultureIgnoreCase);
-            ProfilerPath = useNativeLoader ? GetNativeLoaderPath() : GetTracerNativeDLLPath();
+            // The Native loader is used only on Windows and Linux x64.
+            // We need to keep this check until all platforms/configurations are supported.
+            ProfilerPath = UseNativeLoader ? GetNativeLoaderPath() : GetTracerNativeDLLPath();
 
             var parts = _targetFramework.FrameworkName.Split(',');
             _runtime = parts[0];
@@ -80,6 +79,8 @@ namespace Datadog.Trace.TestHelpers
         public string TracerHome { get; }
 
         public string FullSampleName => $"{_appNamePrepend}{SampleName}";
+
+        public bool UseNativeLoader => string.Equals("true", Environment.GetEnvironmentVariable("USE_NATIVE_LOADER"), StringComparison.InvariantCultureIgnoreCase);
 
         public static bool IsNet5()
         {
@@ -137,7 +138,7 @@ namespace Datadog.Trace.TestHelpers
             {
                 ("win", "X64")     => "Datadog.AutoInstrumentation.NativeLoader.x64.dll",
                 ("win", "X86")     => "Datadog.AutoInstrumentation.NativeLoader.x86.dll",
-                ("linux", "X64")   => "Datadog.AutoInstrumentation.NativeLoader.so",
+                ("linux", "X64")   => "Datadog.Trace.ClrProfiler.Native.so",
                 ("linux", "Arm64") => "Datadog.AutoInstrumentation.NativeLoader.so",
                 ("osx", _)         => "Datadog.AutoInstrumentation.NativeLoader.dylib",
                 _ => throw new PlatformNotSupportedException()

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace.ClrProfiler.Native/cor_profiler.cpp
@@ -381,7 +381,7 @@ HRESULT STDMETHODCALLTYPE CorProfiler::AssemblyLoadFinished(AssemblyID assembly_
     return S_OK;
 }
 
-void CorProfiler::RewritingPInvokeMaps(const ModuleMetadata& module_metadata, const shared::WSTRING& nativemethods_type_name)
+void CorProfiler::RewritingPInvokeMaps(const ModuleMetadata& module_metadata, const shared::WSTRING& nativemethods_type_name, const shared::WSTRING& library_path)
 {
     HRESULT hr;
     const auto& metadata_import = module_metadata.metadata_import;
@@ -393,10 +393,18 @@ void CorProfiler::RewritingPInvokeMaps(const ModuleMetadata& module_metadata, co
         FindTypeDefByName(nativemethods_type_name, module_metadata.assemblyName, metadata_import, nativeMethodsTypeDef);
     if (foundType)
     {
-        // Define the actual profiler file path as a ModuleRef
-        shared::WSTRING native_profiler_file = shared::GetCurrentModuleFileName();
+        // get the native profiler file path.
+        auto native_profiler_file = library_path.empty() ? shared::GetCurrentModuleFileName() : library_path;
+
+        if (!fs::exists(native_profiler_file))
+        {
+            Logger::Warn("Unable to rewrite PInvokes. Native library not found: ", native_profiler_file);
+            return;
+        }
+
         Logger::Info("Rewriting PInvokes to native: ", native_profiler_file);
 
+        // Define the actual profiler file path as a ModuleRef
         mdModuleRef profiler_ref;
         hr = metadata_emit->DefineModuleRef(native_profiler_file.c_str(), &profiler_ref);
         if (SUCCEEDED(hr))
@@ -529,6 +537,13 @@ HRESULT STDMETHODCALLTYPE CorProfiler::ModuleLoadFinished(ModuleID module_id, HR
     }
 
     return hr;
+}
+
+bool ShouldRewriteProfilerMaps()
+{
+    auto strValue = shared::GetEnvironmentValue(WStr("SIGNALFX_PROFILING_ENABLED"));
+    bool is_profiler_enabled;
+    return shared::TryParseBooleanEnvironmentValue(strValue, is_profiler_enabled) && is_profiler_enabled;
 }
 
 HRESULT CorProfiler::TryRejitModule(ModuleID module_id)
@@ -683,6 +698,12 @@ HRESULT CorProfiler::TryRejitModule(ModuleID module_id)
 #else
         RewritingPInvokeMaps(module_metadata, nonwindows_nativemethods_type);
 #endif // _WIN32
+
+        if (ShouldRewriteProfilerMaps())
+        {
+            auto profiler_library_path = shared::GetEnvironmentValue(WStr("SIGNALFX_INTERNAL_PROFILING_NATIVE_ENGINE_PATH"));
+            RewritingPInvokeMaps(module_metadata, profiler_nativemethods_type, profiler_library_path);
+        }
 
         if (IsVersionCompatibilityEnabled())
         {
@@ -1696,7 +1717,7 @@ HRESULT CorProfiler::EmitDistributedTracerTargetMethod(const ModuleMetadata& mod
         Logger::Warn("Error rewriting for Distributed Tracing on getting IDistributedTracer TypeDef");
         return hr;
     }
-    
+
     COR_SIGNATURE instanceSignature[16];
 
     unsigned type_buffer;
@@ -1718,7 +1739,7 @@ HRESULT CorProfiler::EmitDistributedTracerTargetMethod(const ModuleMetadata& mod
         Logger::Warn("Error rewriting for Distributed Tracing on defining get_Instance MemberRef");
         return hr;
     }
-    
+
 
     constexpr COR_SIGNATURE signature[] = {
         IMAGE_CEE_CS_CALLCONV_DEFAULT, // Calling convention
@@ -1812,7 +1833,7 @@ HRESULT CorProfiler::RewriteForDistributedTracing(const ModuleMetadata& module_m
         Logger::Info("sizeof(pulHashAlgId): ", sizeof(managed_profiler_assembly_property.pulHashAlgId));
         Logger::Info("assemblyFlags: ", managed_profiler_assembly_property.assemblyFlags);
     }
-    
+
     //
     // *** Get DistributedTracer TypeDef
     //

tracer/test/Datadog.Trace.TestHelpers/EnvironmentHelper.cs
@@ -45,10 +45,9 @@ namespace Datadog.Trace.TestHelpers
             _output = output;
             TracerHome = GetTracerHomePath();
 
-            // The Tracer is not currently utilizing the Native Loader in production. It is only being used in the Continuous Profiler beta.
-            // Because of that, we don't test it in the default pipeline.
-            bool useNativeLoader = string.Equals("true", Environment.GetEnvironmentVariable("SIGNALFX_USE_NATIVE_LOADER"), StringComparison.InvariantCultureIgnoreCase);
-            ProfilerPath = useNativeLoader ? GetNativeLoaderPath() : GetTracerNativeDLLPath();
+            // The Native loader is used only on Windows and Linux x64.
+            // We need to keep this check until all platforms/configurations are supported.
+            ProfilerPath = UseNativeLoader ? GetNativeLoaderPath() : GetTracerNativeDLLPath();
 
             var parts = _targetFramework.FrameworkName.Split(',');
             _runtime = parts[0];
@@ -83,6 +82,8 @@ namespace Datadog.Trace.TestHelpers
         public string TracerHome { get; }
 
         public string FullSampleName => $"{_appNamePrepend}{SampleName}";
+
+        public bool UseNativeLoader => string.Equals("true", Environment.GetEnvironmentVariable("SIGNALFX_USE_NATIVE_LOADER"), StringComparison.InvariantCultureIgnoreCase);
 
         public static bool IsNet5()
         {