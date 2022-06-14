DataDog/dd-trace-dotnet

[Tracer] Fix precedence in ExporterSettings (#2737)
* Add more tests

* fix tests irt to new specs

* change

* fix tests

* implement feedbacks

* fix uri precedence for metrics and a bit more inlining

* Explicit that we don't handle relative paths for uds

* Implement other round of feedback

* Update metrics when updating URL

- Recompute metrics transport on URL update
- Always compute Dogstatsdport for simplicity
- Reintroduce setting the URI when we use the default socket file
- fix the tests

tracer/src/Datadog.Trace/TracerManager.cs
@@ -397,6 +397,16 @@ namespace Datadog.Trace
                     writer.WritePropertyName("direct_logs_submission_error");
                     writer.WriteValue(string.Join(", ", instanceSettings.LogSubmissionSettings.ValidationErrors));
 
+                    writer.WritePropertyName("exporter_settings_warning");
+                    writer.WriteStartArray();
+
+                    foreach (var warning in instanceSettings.Exporter.ValidationWarnings)
+                    {
+                        writer.WriteValue(warning);
+                    }
+
+                    writer.WriteEndArray();
+
                     writer.WritePropertyName("dd_trace_methods");
                     writer.WriteValue(instanceSettings.TraceMethods);
 

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/TracerManager.cs
@@ -405,6 +405,16 @@ namespace Datadog.Trace
                     writer.WritePropertyName("direct_logs_submission_error");
                     writer.WriteValue(string.Join(", ", instanceSettings.LogSubmissionSettings.ValidationErrors));
 
+                    writer.WritePropertyName("exporter_settings_warning");
+                    writer.WriteStartArray();
+
+                    foreach (var warning in instanceSettings.ExporterSettings.ValidationWarnings)
+                    {
+                        writer.WriteValue(warning);
+                    }
+
+                    writer.WriteEndArray();
+
                     writer.WritePropertyName("dd_trace_methods");
                     writer.WriteValue(instanceSettings.TraceMethods);
 