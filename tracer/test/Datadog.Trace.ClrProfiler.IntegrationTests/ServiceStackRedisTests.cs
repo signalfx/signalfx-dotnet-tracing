2932f8c0b9fbb223295aed50a807ed287c9f60d1
Zach Montoya <zach.montoya@datadoghq.com>
2022-07-06 07:48:59 -0700

    [Tracer] Convert ServiceStackRedis integration tests to snapshots (#2927)
    * Convert ServiceStackRedis integration tests to snapshots
    
    * We know that there are INFO/ROLE/QUIT commands being sent so let's assert against them. The snapshot tests will order them by command name so the snapshot should be stable

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/ServiceStackRedisTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/ServiceStackRedisTests.cs
@@ -8,15 +8,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Datadog.Trace.Configuration;
 using Datadog.Trace.ExtensionMethods;
 using Datadog.Trace.TestHelpers;
+using FluentAssertions;
+using VerifyXunit;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Datadog.Trace.ClrProfiler.IntegrationTests
 {
     [Trait("RequiresDockerDependency", "true")]
+    [UsesVerify]
     public class ServiceStackRedisTests : TestHelper
     {
         public ServiceStackRedisTests(ITestOutputHelper output)
@@ -28,23 +32,38 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
         [SkippableTheory]
         [MemberData(nameof(PackageVersions.ServiceStackRedis), MemberType = typeof(PackageVersions))]
         [Trait("Category", "EndToEnd")]
-        public void SubmitsTraces(string packageVersion)
+        public async Task SubmitsTraces(string packageVersion)
         {
             using var telemetry = this.ConfigureTelemetry();
             using (var agent = EnvironmentHelper.GetMockAgent())
             using (RunSampleAndWaitForExit(agent, arguments: $"{TestPrefix}", packageVersion: packageVersion))
             {
-                // note: ignore the INFO command because it's timing is unpredictable (on Linux?)
-                var spans = agent.WaitForSpans(11)
-                                 .Where(s => s.Type == "redis" && s.Resource != "INFO" && s.Resource != "ROLE" && s.Resource != "QUIT")
+#if NETCOREAPP3_1_OR_GREATER
+                var numberOfRuns = 3;
+#else
+                var numberOfRuns = 2;
+#endif
+
+                var expectedSpansPerRun = 12;
+                var spans = agent.WaitForSpans(numberOfRuns * expectedSpansPerRun)
                                  .OrderBy(s => s.Start)
                                  .ToList();
+                spans.Count.Should().Be(numberOfRuns * expectedSpansPerRun);
 
                 var host = Environment.GetEnvironmentVariable("SERVICESTACK_REDIS_HOST") ?? "localhost:6379";
                 var port = host.Substring(host.IndexOf(':') + 1);
                 host = host.Substring(0, host.IndexOf(':'));
 
-                foreach (var span in spans)
+                var settings = VerifyHelper.GetSpanVerifierSettings();
+                settings.UseFileName($"{nameof(ServiceStackRedisTests)}.RunServiceStack");
+                settings.DisableRequireUniquePrefix();
+                settings.AddSimpleScrubber($" {TestPrefix}ServiceStack.Redis.", " ServiceStack.Redis.");
+                settings.AddSimpleScrubber($"out.host: {host}", "out.host: servicestackredis");
+                settings.AddSimpleScrubber($"out.port: {port}", "out.port: 6379");
+
+                // The test application runs the same RunServiceStack method X number of times
+                // Use the snapshot to verify each run of RunServiceStack, instead of testing all of the application spans
+                for (int i = 0; i < numberOfRuns; i++)
                 {
                     Assert.Equal("redis.command", span.LogicScope);
                     Assert.Equal("Samples.ServiceStack.Redis", span.Service);
@@ -90,6 +109,16 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 
                     Assert.True(e1 == a1, $@"invalid resource name for span #{i}, expected ""{e1}"", actual ""{a1}""");
                     Assert.True(e2 == a2, $@"invalid raw command for span #{i}, expected ""{e2}"" != ""{a2}""");
+                    var routineSpans = spans.GetRange(i * expectedSpansPerRun, expectedSpansPerRun).AsReadOnly();
+                    await VerifyHelper.VerifySpans(
+                        routineSpans,
+                        settings,
+                        o => o
+                            .OrderBy(x => VerifyHelper.GetRootSpanName(x, o))
+                            .ThenBy(x => VerifyHelper.GetSpanDepth(x, o))
+                            .ThenBy(x => x.Tags.TryGetValue("redis.raw_command", out var value) ? value.Replace(TestPrefix, string.Empty) : null)
+                            .ThenBy(x => x.Start)
+                            .ThenBy(x => x.Duration));
                 }
 
                 telemetry.AssertIntegrationEnabled(IntegrationId.ServiceStackRedis);
