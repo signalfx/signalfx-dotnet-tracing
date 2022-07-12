DataDog/dd-trace-dotnet

Add support for computation of stats in the tracer (#2591)
* Vendor sketches-dotnet

* Initial implementation of tracer stats

* Update public API

* Add integration test

* Fix compilation of benchmarks

* Move the lock inside of StatsAggregator and add new tests

* Fix MessagePack serialization

* Introduce interface IStatsAggregator, and flush stats on shutdown

* Remove stats property from ImmutableExporterSettings

* Update API

* Apply suggestions from code review

Co-authored-by: Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>

* Update tracer/src/Datadog.Trace/Agent/Api.cs

Co-authored-by: Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>

* Update tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs

Co-authored-by: Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>

* Address some PR feedback

* Rename "isPartialFlushEnabled" => "partialFlushEnabled" and "areTracerStatsEnabled" => "tracerStatsEnabled"

* Rename "duration" => "bucketDuration" to make its usage clearer

* Remove Service from the information submitted as part of the ClientStatsPayload. This information is currently not created by the Python tracer or the Agent so its inclusion would only lead to confusion. The Service used for aggregation keys will come from the service name observed by the spans whose metrics are being calculated

* Add synthetics to the stats aggregation key

* Because it's a really small work item, improve the Stats by skipping snapshot spans which are spans marked with the "_dd.partial_version" metric (containing a positive integer)

* Fix synthetics test case

* Add test case for skipping snapshot spans

* Renamed StatsAggregationKey.Synthetics field to StatsAggregationKey.IsSyntheticsRequest

* Rename constructor argument as well

* Since we have established across teams that the environment variable name will be `DD_TRACE_STATS_COMPUTATION_ENABLED`, rename everything in the Tracer from `TracerStatsEna

tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -282,6 +282,11 @@ namespace Datadog.Trace.Configuration
         public const string ExpandRouteTemplatesEnabled = "DD_TRACE_EXPAND_ROUTE_TEMPLATES_ENABLED";
 
         /// <summary>
+        /// Configuration key for enabling computation of stats (aka trace metrics) on the tracer side
+        /// </summary>
+        public const string StatsComputationEnabled = "DD_TRACE_STATS_COMPUTATION_ENABLED";
+
+        /// <summary>
         /// Configuration key for setting the propagation style injection.
         /// </summary>
         public const string PropagationStyleInject = "DD_PROPAGATION_STYLE_INJECT";

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -384,6 +384,23 @@ namespace Datadog.Trace.Configuration
         /// <seealso cref="TracerSettings.ExpandRouteTemplatesEnabled"/>
         public const string ExpandRouteTemplatesEnabled = "SIGNALFX_TRACE_EXPAND_ROUTE_TEMPLATES_ENABLED";
 
+        public const string ExpandRouteTemplatesEnabled = "SIGNALFX_TRACE_EXPAND_ROUTE_TEMPLATES_ENABLED";
+
+        /// <summary>
+        /// Configuration key for enabling computation of stats (aka trace metrics) on the tracer side
+        /// </summary>
+        public const string StatsComputationEnabled = "SIGNALFX_TRACE_STATS_COMPUTATION_ENABLED";
+
+        /// <summary>
+        /// Configuration key for setting the propagation style injection.
+        /// </summary>
+        public const string PropagationStyleInject = "SIGNALFX_PROPAGATION_STYLE_INJECT";
+
+        /// <summary>
+        /// Configuration key for setting the propagation style extraction.
+        /// </summary>
+        public const string PropagationStyleExtract = "SIGNALFX_PROPAGATION_STYLE_EXTRACT";
+
         /// <summary>
         /// Configuration key for enabling automatic instrumentation on specified methods.
         /// Default value is "" (disabled).