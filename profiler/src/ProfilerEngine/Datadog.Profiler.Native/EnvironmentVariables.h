DataDog/dd-trace-dotnet

[Profiler] Disable walltime profiler during exception tests (#2935)

profiler/src/ProfilerEngine/Datadog.Profiler.Native/EnvironmentVariables.h
@@ -26,6 +26,7 @@ public:
     inline static const shared::WSTRING Tags                        = WStr("DD_TAGS");
     inline static const shared::WSTRING NativeFramesEnabled         = WStr("DD_PROFILING_FRAMES_NATIVE_ENABLED");
     inline static const shared::WSTRING CpuProfilingEnabled         = WStr("DD_PROFILING_CPU_ENABLED");
+    inline static const shared::WSTRING WallTimeProfilingEnabled    = WStr("DD_PROFILING_WALLTIME_ENABLED");
     inline static const shared::WSTRING ExceptionProfilingEnabled   = WStr("DD_PROFILING_EXCEPTION_ENABLED");
     inline static const shared::WSTRING ExceptionSampleLimit        = WStr("DD_PROFILING_EXCEPTION_SAMPLE_LIMIT");
     inline static const shared::WSTRING ProfilesOutputDir           = WStr("DD_INTERNAL_PROFILING_OUTPUT_DIR");

profiler/test/Datadog.Profiler.IntegrationTests/Helpers/EnvironmentVariables.cs
@@ -12,6 +12,7 @@ namespace Datadog.Profiler.IntegrationTests.Helpers
         public const string CodeHotSpotsEnable = "DD_PROFILING_CODEHOTSPOTS_ENABLED";
         public const string UseNativeLoader = "USE_NATIVE_LOADER";
         public const string CpuProfilerEnabled = "DD_PROFILING_CPU_ENABLED";
+        public const string WallTimeProfilerEnabled = "DD_PROFILING_WALLTIME_ENABLED";
         public const string ExceptionProfilerEnabled = "DD_PROFILING_EXCEPTION_ENABLED";
         public const string ExceptionSampleLimit = "DD_PROFILING_EXCEPTION_SAMPLE_LIMIT";
     }

signalfx/signalfx-dotnet-tracing

profiler/src/ProfilerEngine/Datadog.Profiler.Native/EnvironmentVariables.h
@@ -26,6 +26,7 @@ public:
     inline static const shared::WSTRING Tags                        = WStr("SIGNALFX_TAGS");
     inline static const shared::WSTRING NativeFramesEnabled         = WStr("SIGNALFX_PROFILING_FRAMES_NATIVE_ENABLED");
     inline static const shared::WSTRING CpuProfilingEnabled         = WStr("SIGNALFX_PROFILING_CPU_ENABLED");
+    inline static const shared::WSTRING WallTimeProfilingEnabled    = WStr("SIGNALFX_PROFILING_WALLTIME_ENABLED");
     inline static const shared::WSTRING ExceptionProfilingEnabled   = WStr("SIGNALFX_PROFILING_EXCEPTION_ENABLED");
     inline static const shared::WSTRING ExceptionSampleLimit        = WStr("SIGNALFX_PROFILING_EXCEPTION_SAMPLE_LIMIT");
     inline static const shared::WSTRING ProfilesOutputDir           = WStr("SIGNALFX_PROFILING_OUTPUT_DIR");

profiler/test/Datadog.Profiler.IntegrationTests/Helpers/EnvironmentVariables.cs
@@ -12,6 +12,7 @@ namespace Datadog.Profiler.IntegrationTests.Helpers
         public const string CodeHotSpotsEnable = "SIGNALFX_PROFILING_CODEHOTSPOTS_ENABLED";
         public const string UseNativeLoader = "SIGNALFX_USE_NATIVE_LOADER";
         public const string CpuProfilerEnabled = "SIGNALFX_PROFILING_CPU_ENABLED";
+        public const string WallTimeProfilerEnabled = "SIGNALFX_PROFILING_WALLTIME_ENABLED";
         public const string ExceptionProfilerEnabled = "SIGNALFX_PROFILING_EXCEPTION_ENABLED";
         public const string ExceptionSampleLimit = "SIGNALFX_PROFILING_EXCEPTION_SAMPLE_LIMIT";
     }