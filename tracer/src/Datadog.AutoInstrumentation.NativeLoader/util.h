DataDog/dd-trace-dotnet

Add instrumented IL verification (#2829)
Add instrumented IL verification

tracer/src/Datadog.AutoInstrumentation.NativeLoader/util.h
@@ -14,10 +14,11 @@ EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 #include "../../../shared/src/native-src/pal.h"
 #include "../../../shared/src/native-src/string.h"
 
-using namespace datadog::shared::nativeloader;
-
 const std::string conf_filename = "loader.conf";
 const ::shared::WSTRING cfg_filepath_env = WStr("DD_NATIVELOADER_CONFIGFILE");
+const ::shared::WSTRING cfg_instrumentation_verification_env = WStr("DD_WRITE_INSTRUMENTATION_TO_DISK");
+const ::shared::WSTRING cfg_log_directory_env = WStr("DD_TRACE_LOG_DIRECTORY");
+inline static const ::shared::WSTRING datadog_logs_folder_path = WStr(R"(Datadog .NET Tracer\logs)");
 
 static fs::path GetCurrentModuleFolderPath()
 {
@@ -35,6 +36,30 @@ static fs::path GetCurrentModuleFolderPath()
     }
 #endif
     return {};
+}
+
+static ::shared::WSTRING GetDatadogLogsDirectoryPath()
+{
+    ::shared::WSTRING directory = shared::GetEnvironmentValue(cfg_log_directory_env);
+
+    if (directory.length() > 0)
+    {
+        return directory;
+    }
+
+#ifdef _WIN32
+    fs::path program_data_path = shared::GetEnvironmentValue(WStr("PROGRAMDATA"));
+
+    if (program_data_path.empty())
+    {
+        program_data_path = WStr(R"(C:\ProgramData)");
+    }
+
+    // on Windows WSTRING == wstring
+    return (program_data_path / datadog_logs_folder_path).wstring();
+#else
+    return shared::ToWSTRING("/var/log/datadog/dotnet/");
+#endif
 }
 
 // Gets the configuration file path

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.AutoInstrumentation.NativeLoader/util.h
@@ -14,10 +14,11 @@ EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 #include "../../../shared/src/native-src/pal.h"
 #include "../../../shared/src/native-src/string.h"
 
-using namespace datadog::shared::nativeloader;
-
 const std::string conf_filename = "loader.conf";
 const ::shared::WSTRING cfg_filepath_env = WStr("SIGNALFX_NATIVELOADER_CONFIGFILE");
+const ::shared::WSTRING cfg_instrumentation_verification_env = WStr("SIGNALFX_WRITE_INSTRUMENTATION_TO_DISK");
+const ::shared::WSTRING cfg_log_directory_env = WStr("SIGNALFX_TRACE_LOG_DIRECTORY");
+inline static const ::shared::WSTRING signalfx_logs_folder_path = WStr(R"(SignalFx .NET Tracer\logs)");
 
 static fs::path GetCurrentModuleFolderPath()
 {
@@ -35,6 +36,30 @@ static fs::path GetCurrentModuleFolderPath()
     }
 #endif
     return {};
+}
+
+static ::shared::WSTRING GetDatadogLogsDirectoryPath()
+{
+    ::shared::WSTRING directory = shared::GetEnvironmentValue(cfg_log_directory_env);
+
+    if (directory.length() > 0)
+    {
+        return directory;
+    }
+
+#ifdef _WIN32
+    fs::path program_data_path = shared::GetEnvironmentValue(WStr("PROGRAMDATA"));
+
+    if (program_data_path.empty())
+    {
+        program_data_path = WStr(R"(C:\ProgramData)");
+    }
+
+    // on Windows WSTRING == wstring
+    return (program_data_path / signalfx_logs_folder_path).wstring();
+#else
+    return shared::ToWSTRING("/var/log/datadog/dotnet/");
+#endif
 }
 
 // Gets the configuration file path