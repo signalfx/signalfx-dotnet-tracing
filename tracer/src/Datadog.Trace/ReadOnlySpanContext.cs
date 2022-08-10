DataDog/dd-trace-dotnet

Finalize Live Debugger codebase for public repo and address all .NET Team feedback

tracer/src/Datadog.Trace/ReadOnlySpanContext.cs
@@ -3,20 +3,21 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-namespace Datadog.Trace;
-
-internal class ReadOnlySpanContext : ISpanContext
+namespace Datadog.Trace
 {
-    public ReadOnlySpanContext(ulong traceId, ulong spanId, string serviceName)
+    internal class ReadOnlySpanContext : ISpanContext
     {
-        TraceId = traceId;
-        SpanId = spanId;
-        ServiceName = serviceName;
+        public ReadOnlySpanContext(ulong traceId, ulong spanId, string serviceName)
+        {
+            TraceId = traceId;
+            SpanId = spanId;
+            ServiceName = serviceName;
+        }
+
+        public ulong TraceId { get; }
+
+        public ulong SpanId { get; }
+
+        public string ServiceName { get; }
     }
-
-    public ulong TraceId { get; }
-
-    public ulong SpanId { get; }
-
-    public string ServiceName { get; }
 }

signalfx/signalfx-dotnet-tracing

tracer/src/Datadog.Trace/ReadOnlySpanContext.cs
@@ -5,20 +5,21 @@
 
 // Modified by Splunk Inc.
 
-namespace Datadog.Trace;
-
-internal class ReadOnlySpanContext : ISpanContext
+namespace Datadog.Trace
 {
-    public ReadOnlySpanContext(TraceId traceId, ulong spanId, string serviceName)
+    internal class ReadOnlySpanContext : ISpanContext
     {
-        TraceId = traceId;
-        SpanId = spanId;
-        ServiceName = serviceName;
+        public ReadOnlySpanContext(TraceId traceId, ulong spanId, string serviceName)
+        {
+            TraceId = traceId;
+            SpanId = spanId;
+            ServiceName = serviceName;
+        }
+
+        public TraceId TraceId { get; }
+
+        public ulong SpanId { get; }
+
+        public string ServiceName { get; }
     }
-
-    public TraceId TraceId { get; }
-
-    public ulong SpanId { get; }
-
-    public string ServiceName { get; }
 }