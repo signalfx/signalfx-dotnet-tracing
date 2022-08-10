DataDog/dd-trace-dotnet

Implemented the instrument-all mode for the Debugger
* Added the Instrument All capability where we the Debugger instrumentation is being applied to every jit-compiled method (except predefined known set).

* Added an environment variable named `DD_INTERNAL_DEBUGGER_INSTRUMENT_ALL` to control the instrumentation all mode.

* Dropped non-ByRef support usages.

* Fixed the signature of `LogException` - it is now accepting `DebuggerState`.

tracer/build/_build/BuildVariables.cs
@@ -8,6 +8,7 @@ public static class BuildVariables
     {
         envVars.AddTracerEnvironmentVariables(tracerHomeDirectory);
         envVars.Add("DD_DEBUGGER_ENABLED", "1");
+        envVars.Add("DD_INTERNAL_DEBUGGER_INSTRUMENT_ALL", "1");
     }
 
     public static void AddContinuousProfilerEnvironmentVariables(this Dictionary<string, string> envVars, AbsolutePath tracerHomeDirectory)

signalfx/signalfx-dotnet-tracing

tracer/build/_build/BuildVariables.cs
@@ -10,6 +10,7 @@ public static class BuildVariables
     {
         envVars.AddTracerEnvironmentVariables(tracerHomeDirectory);
         envVars.Add("SINGNALFX_DEBUGGER_ENABLED", "1");
+        envVars.Add("SINGNALFX_INTERNAL_DEBUGGER_INSTRUMENT_ALL", "1");
     }
 
     public static void AddContinuousProfilerEnvironmentVariables(this Dictionary<string, string> envVars, AbsolutePath tracerHomeDirectory)