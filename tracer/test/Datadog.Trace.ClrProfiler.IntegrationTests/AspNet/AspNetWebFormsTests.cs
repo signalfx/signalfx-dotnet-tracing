DataDog/dd-trace-dotnet

[Tracer] Add Fluent API to validate integration spans and produce documentation of tracing integrations -- Redo (#3011)
I created C# APIs to easily and clearly chain together assertions for span properties and tag values. By limiting the API surface area, I was also able to write a build task to parse this file and generate a markdown file so we can have an easily linked file in our repo to show what tracing information we generate.

All of the integrations to date are being tested with this readable testing framework.

tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetWebFormsTests.cs
@@ -80,6 +80,20 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 
             Assert.True(allSpans.Count > 0, "Expected there to be spans.");
 
+            var aspnetSpans = allSpans.Where(s => s.Name == "aspnet.request");
+            foreach (var aspnetSpan in aspnetSpans)
+            {
+                var result = aspnetSpan.IsAspNet();
+                Assert.True(result.Success, result.ToString());
+            }
+
+            var aspnetMvcSpans = allSpans.Where(s => s.Name == "aspnet-mvc.request");
+            foreach (var aspnetMvcSpan in aspnetMvcSpans)
+            {
+                var result = aspnetMvcSpan.IsAspNetMvc();
+                Assert.True(result.Success, result.ToString());
+            }
+
             var elasticSpans = allSpans
                              .Where(s => s.Type == "elasticsearch")
                              .ToList();

tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNetCore/AspNetCoreIisMinimalApisTests.cs
@@ -6,6 +6,7 @@
 #if NET6_0_OR_GREATER
 #pragma warning disable SA1402 // File may only contain a single class
 #pragma warning disable SA1649 // File name must match first type name
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Datadog.Trace.TestHelpers;
@@ -73,6 +74,20 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AspNetCore
         {
             // We actually sometimes expect 2, but waiting for 1 is good enough
             var spans = await GetWebServerSpans(path, _iisFixture.Agent, _iisFixture.HttpPort, statusCode, expectedSpanCount: 1);
+
+            var aspnetCoreSpans = spans.Where(s => s.Name == "aspnet_core.request");
+            foreach (var aspnetCoreSpan in aspnetCoreSpans)
+            {
+                var result = aspnetCoreSpan.IsAspNetCore();
+                Assert.True(result.Success, result.ToString());
+            }
+
+            var aspnetCoreMvcSpans = spans.Where(s => s.Name == "aspnet_core_mvc.request");
+            foreach (var aspnetCoreMvcSpan in aspnetCoreMvcSpans)
+            {
+                var result = aspnetCoreMvcSpan.IsAspNetCoreMvc();
+                Assert.True(result.Success, result.ToString());
+            }
 
             var sanitisedPath = VerifyHelper.SanitisePathsForVerify(path);
 

tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNetCore/AspNetCoreMvcWrongMethodTestBase.cs
@@ -6,6 +6,7 @@
 #if NETCOREAPP
 #pragma warning disable SA1402 // File may only contain a single class
 #pragma warning disable SA1649 // File name must match first type name
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Datadog.Trace.TestHelpers;
@@ -30,13 +31,26 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AspNetCore
 
         public async Task TestIncorrectMethod(string path)
         {
-            var sanitisedPath = VerifyHelper.SanitisePathsForVerify(path);
-
-            var settings = VerifyHelper.GetSpanVerifierSettings(sanitisedPath);
-
             await fixture.TryStartApp(this);
 
             var spans = await fixture.WaitForSpans(path, true);
+
+            var aspnetCoreSpans = spans.Where(s => s.Name == "aspnet_core.request");
+            foreach (var aspnetCoreSpan in aspnetCoreSpans)
+            {
+                var result = aspnetCoreSpan.IsAspNetCore();
+                Assert.True(result.Success, result.ToString());
+            }
+
+            var aspnetCoreMvcSpans = spans.Where(s => s.Name == "aspnet_core_mvc.request");
+            foreach (var aspnetCoreMvcSpan in aspnetCoreMvcSpans)
+            {
+                var result = aspnetCoreMvcSpan.IsAspNetCoreMvc();
+                Assert.True(result.Success, result.ToString());
+            }
+
+            var sanitisedPath = VerifyHelper.SanitisePathsForVerify(path);
+            var settings = VerifyHelper.GetSpanVerifierSettings(sanitisedPath);
 
             // Overriding the type name here as we have multiple test classes in the file
             // Ensures that we get nice file nesting in Solution Explorer

signalfx/signalfx-dotnet-tracing

tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetWebFormsTests.cs
@@ -83,6 +83,20 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 
             Assert.True(allSpans.Count > 0, "Expected there to be spans.");
 
+            var aspnetSpans = allSpans.Where(s => s.LogicScope == "aspnet.request");
+            foreach (var aspnetSpan in aspnetSpans)
+            {
+                var result = aspnetSpan.IsAspNet();
+                Assert.True(result.Success, result.ToString());
+            }
+
+            var aspnetMvcSpans = allSpans.Where(s => s.LogicScope == "aspnet-mvc.request");
+            foreach (var aspnetMvcSpan in aspnetMvcSpans)
+            {
+                var result = aspnetMvcSpan.IsAspNetMvc();
+                Assert.True(result.Success, result.ToString());
+            }
+
             var elasticSpans = allSpans
                              .Where(s => s.Type == "elasticsearch")
                              .ToList();

tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNetCore/AspNetCoreIisMinimalApisTests.cs
@@ -6,6 +6,7 @@
 #if NET6_0_OR_GREATER
 #pragma warning disable SA1402 // File may only contain a single class
 #pragma warning disable SA1649 // File name must match first type name
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Datadog.Trace.TestHelpers;
@@ -73,6 +74,20 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AspNetCore
         {
             // We actually sometimes expect 2, but waiting for 1 is good enough
             var spans = await GetWebServerSpans(path, _iisFixture.Agent, _iisFixture.HttpPort, statusCode, expectedSpanCount: 1);
+
+            var aspnetCoreSpans = spans.Where(s => s.LogicScope == "aspnet_core.request");
+            foreach (var aspnetCoreSpan in aspnetCoreSpans)
+            {
+                var result = aspnetCoreSpan.IsAspNetCore();
+                Assert.True(result.Success, result.ToString());
+            }
+
+            var aspnetCoreMvcSpans = spans.Where(s => s.LogicScope == "aspnet_core_mvc.request");
+            foreach (var aspnetCoreMvcSpan in aspnetCoreMvcSpans)
+            {
+                var result = aspnetCoreMvcSpan.IsAspNetCoreMvc();
+                Assert.True(result.Success, result.ToString());
+            }
 
             var sanitisedPath = VerifyHelper.SanitisePathsForVerify(path);
 

tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNetCore/AspNetCoreMvcWrongMethodTestBase.cs
@@ -6,6 +6,7 @@
 #if NETCOREAPP
 #pragma warning disable SA1402 // File may only contain a single class
 #pragma warning disable SA1649 // File name must match first type name
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Datadog.Trace.TestHelpers;
@@ -30,13 +31,26 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AspNetCore
 
         public async Task TestIncorrectMethod(string path)
         {
-            var sanitisedPath = VerifyHelper.SanitisePathsForVerify(path);
-
-            var settings = VerifyHelper.GetSpanVerifierSettings(sanitisedPath);
-
             await fixture.TryStartApp(this);
 
             var spans = await fixture.WaitForSpans(path, true);
+
+            var aspnetCoreSpans = spans.Where(s => s.LogicScope == "aspnet_core.request");
+            foreach (var aspnetCoreSpan in aspnetCoreSpans)
+            {
+                var result = aspnetCoreSpan.IsAspNetCore();
+                Assert.True(result.Success, result.ToString());
+            }
+
+            var aspnetCoreMvcSpans = spans.Where(s => s.LogicScope == "aspnet_core_mvc.request");
+            foreach (var aspnetCoreMvcSpan in aspnetCoreMvcSpans)
+            {
+                var result = aspnetCoreMvcSpan.IsAspNetCoreMvc();
+                Assert.True(result.Success, result.ToString());
+            }
+
+            var sanitisedPath = VerifyHelper.SanitisePathsForVerify(path);
+            var settings = VerifyHelper.GetSpanVerifierSettings(sanitisedPath);
 
             // Overriding the type name here as we have multiple test classes in the file
             // Ensures that we get nice file nesting in Solution Explorer