DataDog/dd-trace-dotnet

Try disabling telemetry in ci (#3010)
* Disable telemetry in build by default

* Update build_in_docker.ps1

* Update build_in_docker.sh

* Update run-in-docker.yml

* Update tracer/build_in_docker.sh

Co-authored-by: Zach Montoya <zach.montoya@datadoghq.com>

Co-authored-by: Zach Montoya <zach.montoya@datadoghq.com>

tracer/build_in_docker.sh
@@ -19,6 +19,7 @@ docker run -it --rm \
     --env NugetPackageDirectory=/project/packages \
     --env tracerHome=/project/shared/bin/monitoring-home/tracer \
     --env artifacts=/project/tracer/bin/artifacts \
+    --env DD_INSTRUMENTATION_TELEMETRY_ENABLED=0 \
     -p 5003:5003 \
     -v /ddlogs:/var/log/datadog/dotnet \
     $IMAGE_NAME \

signalfx/signalfx-dotnet-tracing

tracer/build_in_docker.sh
@@ -19,6 +19,7 @@ docker run -it --rm \
     --env NugetPackageDirectory=/project/packages \
     --env tracerHome=/project/shared/bin/monitoring-home/tracer \
     --env artifacts=/project/tracer/bin/artifacts \
+    --env SIGNALFX_INSTRUMENTATION_TELEMETRY_ENABLED=0 \
     -p 5003:5003 \
     -v /ddlogs:/var/log/signalfx/dotnet \
     $IMAGE_NAME \