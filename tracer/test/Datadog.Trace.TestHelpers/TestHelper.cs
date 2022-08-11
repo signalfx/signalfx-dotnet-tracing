DataDog/dd-trace-dotnet

[ASM] Tags standardization, http.route, http.url, http.useragent, http.client_ip (#2915)
* Collecting tags, http.url with query string and not raw, http.client_ip, http.route, http.useragent

Collecting tags, http.url with query string and not raw, http.client_ip, http.route, http.useragent

* factorize ip collection

* Collecting and testing route

* fix unit tests

* fix conflicts remove error Id regex, scrubinlineguids should do it

* integration with aspnet web api 2

* Ip extraction and tests

* peer ip in network client ip

* add possibility to disable query string collection

* fix url collection and add framework and snapshots

Fix snapshots

fix snapshots

fix test name

Fix name snapshots

* Query string test fix for framework

* change regex to take into account encoded characters

* add ability of disabling collecting ip header

fix public api and fix attempts for mock agent

try fix agent port in tests

fix snapshots

fix snapshots 2.1

fix error contains

fix snapshots since rebase and verify helper has the scrubs guids

* add throughput tests for query string

throughput

* add regex pattern to framework and test throuput

* obfuscator change timeout system for better perf

Fix return method

Fix unit test

query string unit tests

fix timeout

* timeout can be a setting for regex

* add log

* Increase timeout

* add timeout for integration tests security

longer timeout for security tests

remove key duplicate in integration test

log timeout

add log

* fix unit tests params

* logs for instantiation

* add timeout logs

* task run instead of factory

* improve logs

* change way of starting tasks

change way of starting tasks

change the way task is initialized with real timeout

change way initializing task

catch the right exception

catch exception better

adjust token and crop query string

fix substring

* fix tests

* back to timeout o

tracer/test/Datadog.Trace.TestHelpers/TestHelper.cs
@@ -520,6 +520,7 @@ namespace Datadog.Trace.TestHelpers
 
             // disable tracing for this HttpClient request
             httpClient.DefaultRequestHeaders.Add(HttpHeaderNames.TracingEnabled, "false");
+            httpClient.DefaultRequestHeaders.Add(HttpHeaderNames.UserAgent, "testhelper");
             var testStart = DateTime.UtcNow;
             var response = await httpClient.GetAsync($"http://localhost:{httpPort}" + path);
             var content = await response.Content.ReadAsStringAsync();

signalfx/signalfx-dotnet-tracing

tracer/test/Datadog.Trace.TestHelpers/TestHelper.cs
@@ -520,6 +520,7 @@ namespace Datadog.Trace.TestHelpers
 
             // disable tracing for this HttpClient request
             httpClient.DefaultRequestHeaders.Add(CommonHttpHeaderNames.TracingEnabled, "false");
+            httpClient.DefaultRequestHeaders.Add(CommonHttpHeaderNames.UserAgent, "testhelper");
             var testStart = DateTime.UtcNow;
             var response = await httpClient.GetAsync($"http://localhost:{httpPort}" + path);
             var content = await response.Content.ReadAsStringAsync();