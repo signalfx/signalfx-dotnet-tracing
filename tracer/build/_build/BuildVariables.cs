DataDog/dd-trace-dotnet

Temporarily disable Live Debugger (#3012)
* Temporarily disable Live Debugger by renaming DD_DEBUGGER_ENABLED to DD_INTERNAL_DEBUGGER_ENABLED.

* Address PR feedback

tracer/build/_build/BuildVariables.cs
@@ -7,7 +7,7 @@ public static class BuildVariables
     public static void AddDebuggerEnvironmentVariables(this Dictionary<string, string> envVars, AbsolutePath tracerHomeDirectory)
     {
         envVars.AddTracerEnvironmentVariables(tracerHomeDirectory);
-        envVars.Add("DD_DEBUGGER_ENABLED", "1");
+        envVars.Add("DD_INTERNAL_DEBUGGER_ENABLED", "1");
         envVars.Add("DD_INTERNAL_DEBUGGER_INSTRUMENT_ALL", "1");
     }
 

tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.Debugger.cs
@@ -37,7 +37,7 @@ namespace Datadog.Trace.Configuration
             /// Default value is false (disabled).
             /// </summary>
             /// <seealso cref="DebuggerSettings.Enabled"/>
-            public const string Enabled = "DD_DEBUGGER_ENABLED";
+            public const string Enabled = "DD_INTERNAL_DEBUGGER_ENABLED";
 
             /// <summary>
             /// Configuration key for the max object depth to serialize for probe snapshots.

signalfx/signalfx-dotnet-tracing

tracer/build/_build/BuildVariables.cs
@@ -9,7 +9,7 @@ public static class BuildVariables
     public static void AddDebuggerEnvironmentVariables(this Dictionary<string, string> envVars, AbsolutePath tracerHomeDirectory)
     {
         envVars.AddTracerEnvironmentVariables(tracerHomeDirectory);
-        envVars.Add("SINGNALFX_DEBUGGER_ENABLED", "1");
+        envVars.Add("SINGNALFX_INTERNAL_DEBUGGER_ENABLED", "1");
         envVars.Add("SINGNALFX_INTERNAL_DEBUGGER_INSTRUMENT_ALL", "1");
     }
 

tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.Debugger.cs
@@ -37,7 +37,7 @@ namespace Datadog.Trace.Configuration
             /// Default value is false (disabled).
             /// </summary>
             /// <seealso cref="DebuggerSettings.Enabled"/>
-            public const string Enabled = "SIGNALFX_DEBUGGER_ENABLED";
+            public const string Enabled = "SIGNALFX_INTERNAL_DEBUGGER_ENABLED";
 
             /// <summary>
             /// Configuration key for the max object depth to serialize for probe snapshots.