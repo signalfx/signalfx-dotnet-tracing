36d9076fac8ebf5f9a5ae390781d0785cebdffbb
RassK <rkuusmann@splunk.com>
2022-10-10 13:05:49 +0300

    add install script from docs
    Signed-off-by: RassK <rkuusmann@splunk.com>

---
+++ install.ps1
@@ -0,0 +1,22 @@
+# signalfx-dotnet-tracing github repository API
+$api = "https://api.github.com/repos/signalfx/signalfx-dotnet-tracing/releases/latest"
+
+# determine OS architecture
+$os_bits = (Get-CimInstance Win32_OperatingSystem).OSArchitecture
+$os_arch = (&{If($os_bits -eq "64-bit") {"x64"} Else {"x86"}})
+
+# File pattern to search for
+$pattern = "signalfx-dotnet-tracing-*-$os_arch.msi"
+
+# Find latest MSI to download
+$download = (Invoke-WebRequest $api | ConvertFrom-Json).assets | Where-Object { $_.name -like $pattern } | Select-Object -Property browser_download_url,name
+
+# Download installer MSI to Temp
+$msi = Join-Path $env:temp $download.name
+Invoke-WebRequest -Uri $download.browser_download_url -OutFile $msi
+
+# Install downloaded MSI
+Start-Process msiexec.exe -Wait -ArgumentList "/I $msi /quiet"
+
+# Cleanup
+Remove-Item $msi
