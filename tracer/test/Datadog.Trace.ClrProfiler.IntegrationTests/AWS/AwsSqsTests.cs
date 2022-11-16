0e7fff2c9988a9f8e59ecdccfe408efb090c7a73
Robert Pająk <pellared@hotmail.com>
2022-11-16 22:47:39 +0100

    AwsSqsTests are also fixed

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AWS/AwsSqsTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AWS/AwsSqsTests.cs
@@ -29,11 +29,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AWS
             SetEnvironmentVariable("SIGNALFX_PROPAGATORS", "B3");
         }
 
-#if NET7_0
-        [SkippableTheory(Skip = "APMI-3564 - fix .NET 7 tests")]
-#else
         [SkippableTheory]
-#endif
         [MemberData(nameof(PackageVersions.AwsSqs), MemberType = typeof(PackageVersions))]
         [Trait("Category", "EndToEnd")]
         public async Task SubmitsTraces(string packageVersion)
